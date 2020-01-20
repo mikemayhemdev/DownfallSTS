fa38a7a45a31ef3f5ac3cae15e42e013ef459d2e
Vex Darkon <edanvirgo@gmail.com>
2020-01-20 15:50:23 -0700

    ghostwalk and ethereal expedition

---
+++ src/main/java/theHexaghost/cards/EtherealExpedition.java
@@ -0,0 +1,58 @@
+package theHexaghost.cards;
+
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInHandAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import java.util.ArrayList;
+
+import static com.megacrit.cardcrawl.dungeons.AbstractDungeon.cardRandomRng;
+
+public class EtherealExpedition extends AbstractHexaCard {
+
+    public final static String ID = makeID("EtherealExpedition");
+
+    //stupid intellij stuff SKILL, SELF, UNCOMMON
+
+    public EtherealExpedition() {
+        super(ID, 1, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.SELF);
+        isEthereal = true;
+        exhaust = true;
+    }
+
+    private static AbstractCard returnTrulyRandomEtherealCardInCombat() {
+        ArrayList<AbstractCard> list = new ArrayList<>();// 1201
+        for (AbstractCard c : AbstractDungeon.srcCommonCardPool.group) {
+            if (c.isEthereal && !c.hasTag(CardTags.HEALING)) {// 1203
+                list.add(c);// 1204
+            }
+        }
+
+        for (AbstractCard c : AbstractDungeon.srcUncommonCardPool.group) {
+            if (c.isEthereal && !c.hasTag(CardTags.HEALING)) {// 1203
+                list.add(c);// 1204
+            }
+        }
+        for (AbstractCard c : AbstractDungeon.srcRareCardPool.group) {
+            if (c.isEthereal && !c.hasTag(CardTags.HEALING)) {// 1203
+                list.add(c);// 1204
+            }
+        }
+        return (AbstractCard) list.get(cardRandomRng.random(list.size() - 1));// 1217
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        AbstractCard q = returnTrulyRandomEtherealCardInCombat().makeCopy();
+        q.setCostForTurn(0);
+        this.addToBot(new MakeTempCardInHandAction(q, true));// 34
+    }
+
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeBaseCost(0);
+        }
+    }
+}
---
+++ src/main/java/theHexaghost/powers/EtherealRefundPower.java
@@ -0,0 +1,66 @@
+package theHexaghost.powers;
+
+import basemod.interfaces.CloneablePowerInterface;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
+import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import theHexaghost.HexaMod;
+import theHexaghost.util.TextureLoader;
+
+public class EtherealRefundPower extends AbstractPower implements CloneablePowerInterface {
+
+    public static final String POWER_ID = HexaMod.makeID("EnhancePower");
+
+    private static final Texture tex84 = TextureLoader.getTexture(HexaMod.getModID() + "Resources/images/powers/Key_power84.png");
+    private static final Texture tex32 = TextureLoader.getTexture(HexaMod.getModID() + "Resources/images/powers/Key_power32.png");
+
+    public EtherealRefundPower(final int amount) {
+        this.name = "Enhance";
+        this.ID = POWER_ID;
+        this.owner = AbstractDungeon.player;
+        this.amount = amount;
+        this.type = PowerType.BUFF;
+        this.isTurnBased = true;
+
+        this.region128 = new TextureAtlas.AtlasRegion(tex84, 0, 0, 84, 84);
+        this.region48 = new TextureAtlas.AtlasRegion(tex32, 0, 0, 32, 32);
+
+        this.updateDescription();
+    }
+
+    @Override
+    public void onPlayCard(AbstractCard card, AbstractMonster m) {
+        super.onPlayCard(card, m);
+        if (card.isEthereal) {
+            addToBot(new GainEnergyAction(1));
+        }
+    }
+
+    @Override
+    public void updateDescription() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Whenever you play an #yEthereal card this turn, gain ");
+
+        for (int i = 0; i < this.amount; ++i) {
+            sb.append("[E] ");
+        }
+
+        sb.append(" .");
+        this.description = sb.toString();
+    }
+
+    public void atEndOfTurn(boolean isPlayer) {
+        this.flash();// 30
+        this.addToBot(new RemoveSpecificPowerAction(this.owner, this.owner, this.ID));// 32
+    }
+
+    @Override
+    public AbstractPower makeCopy() {
+        return new EtherealRefundPower(amount);
+    }
+}
