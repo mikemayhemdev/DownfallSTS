f9d805c07af7506688f6b94657310b7eb6f3d3dc
Vex Darkon <edanvirgo@gmail.com>
2020-01-20 11:12:23 -0700

    Skip a Beat

---
+++ src/main/java/theHexaghost/cards/SkipABeat.java
@@ -0,0 +1,32 @@
+package theHexaghost.cards;
+
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import theHexaghost.actions.AdvanceAction;
+import theHexaghost.actions.ChargeCurrentFlameAction;
+
+public class SkipABeat extends AbstractHexaCard {
+
+    public final static String ID = makeID("SkipABeat");
+
+    //stupid intellij stuff SKILL, SELF, UNCOMMON
+
+    public SkipABeat() {
+        super(ID, 1, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.SELF);
+        exhaust = true;
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        atb(new ChargeCurrentFlameAction());
+        atb(new AdvanceAction());
+    }
+
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            exhaust = false;
+            rawDescription = UPGRADE_DESCRIPTION;
+            initializeDescription();
+        }
+    }
+}
