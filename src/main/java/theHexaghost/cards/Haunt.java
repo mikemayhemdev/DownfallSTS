a44bb27861b4b1425435367121bc12a54c568550
Vex Darkon <edanvirgo@gmail.com>
2020-01-20 14:53:21 -0700

    Haunt!

---
+++ src/main/java/theHexaghost/cards/Haunt.java
@@ -0,0 +1,49 @@
+package theHexaghost.cards;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import theHexaghost.actions.AdvanceAction;
+
+public class Haunt extends AbstractHexaCard {
+
+    public final static String ID = makeID("Haunt");
+
+    //stupid intellij stuff SKILL, SELF, COMMON
+
+    private static final int MAGIC = 2;
+    private static final int UPG_MAGIC = 1;
+
+    public Haunt() {
+        super(ID, 0, CardType.SKILL, CardRarity.COMMON, CardTarget.SELF);
+        baseMagicNumber = magicNumber = MAGIC;
+        isEthereal = true;
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        atb(new AdvanceAction());
+        atb(new DrawCardAction(p, magicNumber));
+        atb(new AbstractGameAction() {
+            @Override
+            public void update() {
+                isDone = true;
+                for (AbstractCard c : p.hand.group) {
+                    if (!c.isEthereal) {
+                        c.isEthereal = true;
+                        c.rawDescription = "Ethereal. NL " + c.rawDescription;
+                        c.initializeDescription();
+                    }
+                }
+            }
+        });
+    }
+
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeMagicNumber(UPG_MAGIC);
+        }
+    }
+}
