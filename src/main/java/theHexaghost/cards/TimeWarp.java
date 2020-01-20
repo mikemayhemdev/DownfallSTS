3ef7cce576c36e7692125ef677480f179a9162c0
Vex Darkon <edanvirgo@gmail.com>
2020-01-20 11:22:25 -0700

    time warp!

---
+++ src/main/java/theHexaghost/cards/TimeWarp.java
@@ -0,0 +1,31 @@
+package theHexaghost.cards;
+
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import theHexaghost.actions.TimeWarpAction;
+
+public class TimeWarp extends AbstractHexaCard {
+
+    public final static String ID = makeID("TimeWarp");
+
+    //stupid intellij stuff ATTACK, ALL_ENEMY, COMMON
+
+    private static final int DAMAGE = 6;
+    private static final int UPG_DAMAGE = 3;
+
+    public TimeWarp() {
+        super(ID, 2, CardType.ATTACK, CardRarity.COMMON, CardTarget.ALL_ENEMY);
+        baseDamage = DAMAGE;
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        atb(new TimeWarpAction(makeInfo()));
+    }
+
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeDamage(UPG_DAMAGE);
+        }
+    }
+}
