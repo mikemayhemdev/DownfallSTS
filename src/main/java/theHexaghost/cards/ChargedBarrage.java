53e0247463749ebedf73375659ccd05b14a325b3
Vex Darkon <edanvirgo@gmail.com>
2020-01-20 20:18:27 -0700

    Charged Barrage!

---
+++ src/main/java/theHexaghost/cards/ChargedBarrage.java
@@ -0,0 +1,44 @@
+package theHexaghost.cards;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import theHexaghost.GhostflameHelper;
+import theHexaghost.actions.BurnAction;
+import theHexaghost.ghostflames.AbstractGhostflame;
+
+public class ChargedBarrage extends AbstractHexaCard {
+
+    public final static String ID = makeID("ChargedBarrage");
+
+    //stupid intellij stuff ATTACK, ENEMY, UNCOMMON
+
+    private static final int DAMAGE = 2;
+    private static final int UPG_DAMAGE = 1;
+
+    private static final int MAGIC = 2;
+    private static final int UPG_MAGIC = 1;
+
+    public ChargedBarrage() {
+        super(ID, 1, CardType.ATTACK, CardRarity.UNCOMMON, CardTarget.ENEMY);
+        baseDamage = DAMAGE;
+        baseMagicNumber = magicNumber = MAGIC;
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        dmg(m, makeInfo(), AbstractGameAction.AttackEffect.BLUNT_LIGHT);
+        atb(new BurnAction(m, magicNumber));
+        for (AbstractGhostflame gf : GhostflameHelper.hexaGhostFlames) {
+            dmg(m, makeInfo(), AbstractGameAction.AttackEffect.BLUNT_LIGHT);
+            atb(new BurnAction(m, magicNumber));
+        }
+    }
+
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeDamage(UPG_DAMAGE);
+            upgradeMagicNumber(UPG_MAGIC);
+        }
+    }
+}
