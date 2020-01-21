72938a39e9f6b7a004bd534a66f0c7374655ee4e
Vex Darkon <edanvirgo@gmail.com>
2020-01-20 20:11:06 -0700

    spectral spark!

---
+++ src/main/java/theHexaghost/cards/SpectralSpark.java
@@ -0,0 +1,56 @@
+package theHexaghost.cards;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import theHexaghost.GhostflameHelper;
+import theHexaghost.actions.BurnAction;
+import theHexaghost.actions.ExtinguishCurrentFlameAction;
+import theHexaghost.actions.SpecificNonChosenDiscardPileToHandAction;
+
+public class SpectralSpark extends AbstractHexaCard {
+
+    public final static String ID = makeID("SpectralSpark");
+
+    //stupid intellij stuff SKILL, ENEMY, UNCOMMON
+
+    private static final int MAGIC = 3;
+
+    public SpectralSpark() {
+        super(ID, 0, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.ENEMY);
+        baseMagicNumber = magicNumber = MAGIC;
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        atb(new BurnAction(m, magicNumber));
+        AbstractCard c = this;
+        atb(new AbstractGameAction() {
+            @Override
+            public void update() {
+                isDone = true;
+                if (GhostflameHelper.activeGhostFlame.charged) {
+                    addToTop(new AbstractGameAction() {
+                        @Override
+                        public void update() {
+                            isDone = true;
+                            c.baseMagicNumber++;
+                            if (c.upgraded) c.baseMagicNumber++;
+                            c.applyPowers();
+                        }
+                    });
+                    addToTop(new SpecificNonChosenDiscardPileToHandAction(c));
+                    addToTop(new ExtinguishCurrentFlameAction());
+                }
+            }
+        });
+    }
+
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            rawDescription = UPGRADE_DESCRIPTION;
+            initializeDescription();
+        }
+    }
+}
