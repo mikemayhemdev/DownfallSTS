307e94f1637e8c61544a1d1a4b05673d4be47a75
Vex Darkon <edanvirgo@gmail.com>
2020-01-20 20:33:08 -0700

    Dynamic blow!

---
+++ src/main/java/theHexaghost/cards/DynamicBlow.java
@@ -0,0 +1,43 @@
+package theHexaghost.cards;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import theHexaghost.GhostflameHelper;
+import theHexaghost.actions.BurnAction;
+
+public class DynamicBlow extends AbstractHexaCard {
+
+    public final static String ID = makeID("DynamicBlow");
+
+    //stupid intellij stuff ATTACK, ENEMY, UNCOMMON
+
+    private static final int DAMAGE = 9;
+    private static final int UPG_DAMAGE = 3;
+
+    private static final int MAGIC = 9;
+    private static final int UPG_MAGIC = 3;
+
+    public DynamicBlow() {
+        super(ID, 1, CardType.ATTACK, CardRarity.UNCOMMON, CardTarget.ENEMY);
+        baseDamage = DAMAGE;
+        baseMagicNumber = magicNumber = MAGIC;
+        isEthereal = true;
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        if (GhostflameHelper.activeGhostFlame.charged) {
+            atb(new BurnAction(m, magicNumber));
+        } else {
+            dmg(m, makeInfo(), AbstractGameAction.AttackEffect.BLUNT_HEAVY);
+        }
+    }
+
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeDamage(UPG_DAMAGE);
+            upgradeMagicNumber(UPG_MAGIC);
+        }
+    }
+}
