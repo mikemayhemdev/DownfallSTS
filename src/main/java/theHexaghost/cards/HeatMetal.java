8e010775b59aae3bbd0f376e948965f65934f41b
Vex Darkon <edanvirgo@gmail.com>
2020-01-20 17:54:19 -0700

    Heat Metal

---
+++ src/main/java/theHexaghost/cards/HeatMetal.java
@@ -0,0 +1,38 @@
+package theHexaghost.cards;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import theHexaghost.powers.BurnPerTurnPower;
+
+public class HeatMetal extends AbstractHexaCard {
+
+    public final static String ID = makeID("HeatMetal");
+
+    //stupid intellij stuff ATTACK, ENEMY, COMMON
+
+    private static final int DAMAGE = 6;
+    private static final int UPG_DAMAGE = 2;
+
+    private static final int MAGIC = 2;
+    private static final int UPG_MAGIC = 1;
+
+    public HeatMetal() {
+        super(ID, 1, CardType.ATTACK, CardRarity.COMMON, CardTarget.ENEMY);
+        baseDamage = DAMAGE;
+        baseMagicNumber = magicNumber = MAGIC;
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        dmg(m, makeInfo(), AbstractGameAction.AttackEffect.SLASH_HORIZONTAL);
+        applyToEnemy(m, new BurnPerTurnPower(m, magicNumber));
+    }
+
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeDamage(UPG_DAMAGE);
+            upgradeMagicNumber(UPG_MAGIC);
+        }
+    }
+}
---
+++ src/main/java/theHexaghost/powers/BurnPerTurnPower.java
@@ -0,0 +1,51 @@
+package theHexaghost.powers;
+
+import basemod.interfaces.CloneablePowerInterface;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import theHexaghost.HexaMod;
+import theHexaghost.actions.BurnAction;
+import theHexaghost.util.TextureLoader;
+
+public class BurnPerTurnPower extends AbstractPower implements CloneablePowerInterface {
+
+    public static final String POWER_ID = HexaMod.makeID("BurnPerTurnPower");
+
+    private static final Texture tex84 = TextureLoader.getTexture(HexaMod.getModID() + "Resources/images/powers/Key_power84.png");
+    private static final Texture tex32 = TextureLoader.getTexture(HexaMod.getModID() + "Resources/images/powers/Key_power32.png");
+
+    public BurnPerTurnPower(final AbstractCreature owner, final int amount) {
+        this.name = "Melting Metal";
+        this.ID = POWER_ID;
+        this.owner = owner;
+        this.amount = amount;
+        this.type = PowerType.DEBUFF;
+        this.isTurnBased = true;
+
+        this.region128 = new TextureAtlas.AtlasRegion(tex84, 0, 0, 84, 84);
+        this.region48 = new TextureAtlas.AtlasRegion(tex32, 0, 0, 32, 32);
+
+        this.updateDescription();
+    }
+
+    @Override
+    public void atStartOfTurn() {
+        if (owner instanceof AbstractMonster) {
+            flash();
+            addToBot(new BurnAction(((AbstractMonster) owner), amount));
+        }
+    }
+
+    @Override
+    public void updateDescription() {
+        description = "Gains #b" + amount + " #yBurn at the start of its turn.";
+    }
+
+    @Override
+    public AbstractPower makeCopy() {
+        return new BurnPerTurnPower(owner, amount);
+    }
+}
