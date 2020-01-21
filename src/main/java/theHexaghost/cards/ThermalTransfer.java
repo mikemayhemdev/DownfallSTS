9beb25567cb73f02d00683d04cbe06597610e5a6
Vex Darkon <edanvirgo@gmail.com>
2020-01-20 17:29:44 -0700

    thermal transfer

---
+++ src/main/java/theHexaghost/cards/ThermalTransfer.java
@@ -0,0 +1,46 @@
+package theHexaghost.cards;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import theHexaghost.powers.BurnPower;
+
+public class ThermalTransfer extends AbstractHexaCard {
+
+    public final static String ID = makeID("ThermalTransfer");
+
+    //stupid intellij stuff ATTACK, ENEMY, UNCOMMON
+
+    private static final int DAMAGE = 12;
+    private static final int UPG_DAMAGE = 3;
+
+    private static final int BLOCK = 12;
+    private static final int UPG_BLOCK = 3;
+
+    public ThermalTransfer() {
+        super(ID, 2, CardType.ATTACK, CardRarity.UNCOMMON, CardTarget.ENEMY);
+        baseDamage = DAMAGE;
+        baseBlock = BLOCK;
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        dmg(m, makeInfo(), AbstractGameAction.AttackEffect.FIRE);
+        atb(new AbstractGameAction() {
+            @Override
+            public void update() {
+                if (m.hasPower(BurnPower.POWER_ID)) {
+                    addToTop(new GainBlockAction(p, block));
+                }
+            }
+        });
+    }
+
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeDamage(UPG_DAMAGE);
+            upgradeBlock(UPG_BLOCK);
+        }
+    }
+}
