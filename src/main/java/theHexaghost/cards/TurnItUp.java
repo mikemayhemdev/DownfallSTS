ca5d19d796c950f6c1df29aef4dc321d3d1abaf4
Vex Darkon <edanvirgo@gmail.com>
2020-01-20 17:48:20 -0700

    Turn It Up

---
+++ src/main/java/theHexaghost/cards/TurnItUp.java
@@ -0,0 +1,42 @@
+package theHexaghost.cards;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import theHexaghost.actions.BurnAction;
+import theHexaghost.powers.BurnPower;
+
+public class TurnItUp extends AbstractHexaCard {
+
+    public final static String ID = makeID("TurnItUp");
+
+    //stupid intellij stuff SKILL, ENEMY, RARE
+
+    public TurnItUp() {
+        super(ID, 1, CardType.SKILL, CardRarity.RARE, CardTarget.ENEMY);
+        baseMagicNumber = magicNumber = 1;
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new AbstractGameAction() {
+            @Override
+            public void update() {
+                if (m.hasPower(BurnPower.POWER_ID)) {
+                    AbstractPower p = m.getPower(BurnPower.POWER_ID);
+                    int flame = p.amount * magicNumber;
+                    addToTop(new BurnAction(m, flame));
+                }
+            }
+        });
+    }
+
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeMagicNumber(1);
+            rawDescription = UPGRADE_DESCRIPTION;
+            initializeDescription();
+        }
+    }
+}
