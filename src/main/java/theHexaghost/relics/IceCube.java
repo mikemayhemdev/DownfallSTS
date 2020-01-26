1e2e33e1b39f49f710634d10f2c6714417126dea
Vex Darkon <edanvirgo@gmail.com>
2020-01-26 16:31:44 -0700

    New Relic: Ice Cube

---
+++ src/main/java/theHexaghost/relics/IceCube.java
@@ -0,0 +1,43 @@
+package theHexaghost.relics;
+
+import basemod.abstracts.CustomRelic;
+import com.badlogic.gdx.graphics.Texture;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import theHexaghost.GhostflameHelper;
+import theHexaghost.HexaMod;
+import theHexaghost.ghostflames.AbstractGhostflame;
+import theHexaghost.ghostflames.BolsteringGhostflame;
+import theHexaghost.util.OnChargeSubscriber;
+import theHexaghost.util.TextureLoader;
+
+import static theHexaghost.HexaMod.makeRelicOutlinePath;
+import static theHexaghost.HexaMod.makeRelicPath;
+
+public class IceCube extends CustomRelic {
+
+    public static final String ID = HexaMod.makeID("IceCube");
+    private static final Texture IMG = TextureLoader.getTexture(makeRelicPath("SpiritBrand.png"));
+    private static final Texture OUTLINE = TextureLoader.getTexture(makeRelicOutlinePath("SpiritBrand.png"));
+
+    public IceCube() {
+        super(ID, IMG, OUTLINE, RelicTier.STARTER, LandingSound.MAGICAL);
+    }
+
+    @Override
+    public void atBattleStart() {
+        GhostflameHelper.hexaGhostFlames.removeIf(gf -> gf instanceof BolsteringGhostflame);
+    }
+
+    public void onEquip() {
+        ++AbstractDungeon.player.energy.energyMaster;// 37
+    }// 38
+
+    public void onUnequip() {
+        --AbstractDungeon.player.energy.energyMaster;// 42
+    }// 43
+
+    public String getUpdatedDescription() {
+        return DESCRIPTIONS[0];
+    }
+}
