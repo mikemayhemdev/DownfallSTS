578eccdea35953f39856779cc1077059c9b511ac
Vex Darkon <edanvirgo@gmail.com>
2020-01-28 17:05:32 -0700

    Fixed Unknown Class?

--- src/main/java/sneckomod/cards/unknowns/UnknownClass.java
+++ src/main/java/sneckomod/cards/unknowns/UnknownClass.java
@@ -2,7 +2,9 @@ package sneckomod.cards.unknowns;
 
 import basemod.BaseMod;
 import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import sneckomod.TheSnecko;
 
 import java.util.ArrayList;
 import java.util.function.Predicate;
@@ -11,13 +13,17 @@ public class UnknownClass extends AbstractUnknownCard {
     public final static String ID = makeID("UnknownClass");
 
     public UnknownClass() {
-        super(ID, CardType.SKILL, CardRarity.UNCOMMON, getRandomCardColor());
+        super(ID, CardType.SKILL, CardRarity.UNCOMMON);
+        if (CardCrawlGame.isInARun()) myColor = getRandomCardColor();
+        else myColor = TheSnecko.Enums.SNECKO_CYAN;
     }
+
+    private CardColor myColor;
 
     @Override
     public void update() {
         super.update();
-        this.rawDescription = "sneckomod:Unknown" + color.name() + ".";
+        this.rawDescription = "sneckomod:Unknown" + myColor.name() + ".";
     }
 
     public static CardColor getRandomCardColor() {
@@ -27,6 +33,6 @@ public class UnknownClass extends AbstractUnknownCard {
 
     @Override
     public Predicate<AbstractCard> myNeeds() {
-        return c -> c.color == this.color;
+        return c -> c.color == myColor;
     }
 }
