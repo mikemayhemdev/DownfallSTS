5de08ff56209d967d30c6bced52a93c89bf43626
Vex Darkon <edanvirgo@gmail.com>
2020-01-28 17:57:47 -0700

    unknown class working, other minor tweaks + fixes

--- src/main/java/sneckomod/TheSnecko.java
+++ src/main/java/sneckomod/TheSnecko.java
@@ -1,6 +1,5 @@
 package sneckomod;
 
-import basemod.abstracts.CustomEnergyOrb;
 import basemod.abstracts.CustomPlayer;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
@@ -18,15 +17,11 @@ import com.megacrit.cardcrawl.helpers.CardLibrary;
 import com.megacrit.cardcrawl.helpers.FontHelper;
 import com.megacrit.cardcrawl.helpers.ScreenShake;
 import com.megacrit.cardcrawl.localization.CharacterStrings;
-import com.megacrit.cardcrawl.relics.SneckoEye;
 import com.megacrit.cardcrawl.screens.CharSelectInfo;
 import sneckomod.cards.Defend;
 import sneckomod.cards.SnekBite;
 import sneckomod.cards.Strike;
 import sneckomod.cards.TailWhip;
-import sneckomod.cards.unknowns.UnknownCommonAttack;
-import sneckomod.cards.unknowns.UnknownCommonSkill;
-import sneckomod.relics.SneckoLibrary;
 import sneckomod.relics.SneckoSoul;
 
 import java.util.ArrayList;
@@ -59,7 +54,7 @@ public class TheSnecko extends CustomPlayer {
                 SHOULDER1,
                 SHOULDER2,
                 CORPSE,
-                getLoadout(), 10.0F, 200.0F, 150.0F, 150.0F, new EnergyManager(3));
+                getLoadout(), 10.0F, -20.0F, 300.0F, 300.0F, new EnergyManager(3));
         dialogX = (drawX + 0.0F * Settings.scale);
         dialogY = (drawY + 240.0F * Settings.scale);
         this.reloadAnimation();
@@ -104,13 +99,12 @@ public class TheSnecko extends CustomPlayer {
     public ArrayList<String> getStartingRelics() {
         ArrayList<String> retVal = new ArrayList<>();
         retVal.add(SneckoSoul.ID);
-        retVal.add(SneckoLibrary.ID);
         return retVal;
     }
 
     @Override
     public void doCharSelectScreenSelectEffect() {
-        CardCrawlGame.sound.playA("UNLOCK_PING", MathUtils.random(-0.2F, 0.2F));
+        CardCrawlGame.sound.playA("MONSTER_SNECKO_GLARE", MathUtils.random(-0.2F, 0.2F));
         CardCrawlGame.screenShake.shake(ScreenShake.ShakeIntensity.LOW, ScreenShake.ShakeDur.SHORT,
                 false);
     }
--- src/main/java/sneckomod/cards/unknowns/UnknownClass.java
+++ src/main/java/sneckomod/cards/unknowns/UnknownClass.java
@@ -2,28 +2,48 @@ package sneckomod.cards.unknowns;
 
 import basemod.BaseMod;
 import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
-import sneckomod.TheSnecko;
+import sneckomod.CardIgnore;
 
 import java.util.ArrayList;
 import java.util.function.Predicate;
 
+@CardIgnore
 public class UnknownClass extends AbstractUnknownCard {
     public final static String ID = makeID("UnknownClass");
 
-    public UnknownClass() {
+    public UnknownClass(CardColor cardColor) {
         super(ID, CardType.SKILL, CardRarity.UNCOMMON);
-        if (CardCrawlGame.isInARun()) myColor = getRandomCardColor();
-        else myColor = TheSnecko.Enums.SNECKO_CYAN;
+        myColor = cardColor;
     }
+
+
+    public String getCharName(CardColor myColor) {
+        ArrayList<AbstractPlayer> theDudes = new ArrayList<AbstractPlayer>(CardCrawlGame.characterManager.getAllCharacters());
+        for (AbstractPlayer p : theDudes) {
+            if (p.getCardColor() == myColor)
+                return p.getLocalizedCharacterName();
+        }
+        return "You should never see this. Report to Vex";
+    }
+
 
     private CardColor myColor;
 
     @Override
+    public AbstractCard makeCopy() {
+        return new UnknownClass(myColor);
+    }
+
+    @Override
     public void update() {
         super.update();
-        this.rawDescription = "sneckomod:Unknown" + myColor.name() + ".";
+        if (!this.rawDescription.equals("sneckomod:Unknown" + getCharName(myColor) + ".")) {
+            this.rawDescription = "sneckomod:Unknown" + getCharName(myColor) + ".";
+            initializeDescription();
+        }
     }
 
     public static CardColor getRandomCardColor() {
