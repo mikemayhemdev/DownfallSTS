9e0a8e8084c1280f909cf18362c123fc09a80916
Vex Darkon <edanvirgo@gmail.com>
2020-01-30 22:08:38 -0700

    Cheap Stock

---
+++ src/main/java/sneckomod/cards/CheapStock.java
@@ -0,0 +1,29 @@
+package sneckomod.cards;
+
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import sneckomod.powers.CheapStockPower;
+
+public class CheapStock extends AbstractSneckoCard {
+
+    public final static String ID = makeID("CheapStock");
+
+    //stupid intellij stuff POWER, SELF, UNCOMMON
+
+    public CheapStock() {
+        super(ID, 1, CardType.POWER, CardRarity.UNCOMMON, CardTarget.SELF);
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        applyToSelf(new CheapStockPower(1));
+    }
+
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            isInnate = true;
+            rawDescription = UPGRADE_DESCRIPTION;
+            initializeDescription();
+        }
+    }
+}
---
+++ src/main/java/sneckomod/powers/CheapStockPower.java
@@ -0,0 +1,68 @@
+package sneckomod.powers;
+
+import basemod.interfaces.CloneablePowerInterface;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import sneckomod.TheSnecko;
+import theHexaghost.HexaMod;
+import theHexaghost.util.TextureLoader;
+
+import java.util.ArrayList;
+
+public class CheapStockPower extends AbstractPower implements CloneablePowerInterface {
+
+    public static final String POWER_ID = HexaMod.makeID("CheapStockPower");
+
+    private static final Texture tex84 = TextureLoader.getTexture(HexaMod.getModID() + "Resources/images/powers/Enhance84.png");
+    private static final Texture tex32 = TextureLoader.getTexture(HexaMod.getModID() + "Resources/images/powers/Enhance32.png");
+
+    public CheapStockPower(final int amount) {
+        this.name = "Cheap Stock";
+        this.ID = POWER_ID;
+        this.owner = AbstractDungeon.player;
+        this.amount = amount;
+        this.type = PowerType.BUFF;
+        this.isTurnBased = true;
+
+        this.region128 = new TextureAtlas.AtlasRegion(tex84, 0, 0, 84, 84);
+        this.region48 = new TextureAtlas.AtlasRegion(tex32, 0, 0, 32, 32);
+
+        this.updateDescription();
+    }
+
+    public void atStartOfTurnPostDraw() {
+        for (int i = 0; i < amount; i++)
+            addToBot(new AbstractGameAction() {
+                @Override
+                public void update() {
+                    int x = 999;
+                    for (AbstractCard q : AbstractDungeon.player.hand.group) {
+                        if (q.color != TheSnecko.Enums.SNECKO_CYAN && q.cost < x)
+                            x = q.cost;
+                    }
+                    ArrayList<AbstractCard> possCardsList = new ArrayList<>();
+                    for (AbstractCard q : AbstractDungeon.player.hand.group) {
+                        if (q.cost == x)
+                            possCardsList.add(q);
+                    }
+                    AbstractCard q = possCardsList.get(AbstractDungeon.cardRandomRng.random(possCardsList.size() - 1));
+                    q.modifyCostForCombat(-1);
+                    q.superFlash();
+                }
+            });
+    }
+
+    @Override
+    public void updateDescription() {
+        description = "At the start of your turn, reduce the cost of the highest cost #yOffclass card in your hand by #b1 #b" + amount+ " times.";
+    }
+
+    @Override
+    public AbstractPower makeCopy() {
+        return new CheapStockPower(amount);
+    }
+}
