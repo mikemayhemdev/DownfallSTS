e6078360a78259ba6e5c8abad692f288c52903f6
Vex Darkon <edanvirgo@gmail.com>
2020-01-31 18:41:48 -0700

    Restock

---
+++ src/main/java/sneckomod/cards/Restock.java
@@ -0,0 +1,31 @@
+package sneckomod.cards;
+
+import com.megacrit.cardcrawl.actions.common.DiscardAction;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import sneckomod.SneckoMod;
+
+public class Restock extends AbstractSneckoCard {
+
+    public final static String ID = makeID("Restock");
+
+    //stupid intellij stuff SKILL, SELF, RARE
+
+    public Restock() {
+        super(ID, "Beta", 1, CardType.SKILL, CardRarity.RARE, CardTarget.SELF);
+        tags.add(SneckoMod.SNEKPROOF);
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        atb(new DiscardAction(p, p, p.hand.size(), true));
+        atb(new DrawCardAction(getRandomNum(5, 10)));
+    }
+
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeBaseCost(0);
+        }
+    }
+}
