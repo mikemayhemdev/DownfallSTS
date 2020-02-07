9db65836cbf8a18cf788be16fc1232cf8a22475c
Kiooeht <kiooeht@gmail.com>
2020-02-06 23:10:45 -0800

    Refactor room flip without fbo

--- src/main/java/evilWithin/patches/UnflipUI.java
+++ src/main/java/evilWithin/patches/UnflipUI.java
@@ -1,5 +1,6 @@
 package evilWithin.patches;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.megacrit.cardcrawl.core.AbstractCreature;
@@ -8,6 +9,15 @@ import evilWithin.EvilWithinMod;
 
 public class UnflipUI
 {
+    public static float flipPos(SpriteBatch sb, float x)
+    {
+        if (FlipRoom.isFlipped()) {
+            FlipRoom.pauseFlip(sb);
+            x = Gdx.graphics.getWidth() - x;
+        }
+        return x;
+    }
+
     @SpirePatch(
             clz = AbstractCreature.class,
             method = "renderHealth"
@@ -18,7 +28,7 @@ public class UnflipUI
         {
             if (!EvilWithinMod.EXPERIMENTAL_FLIP) return;
 
-            FlipRoom.endFBO(sb);
+            FlipRoom.endFlip(sb);
             __instance.hb.cX = Settings.WIDTH - __instance.hb.cX;
         }
 
@@ -27,7 +37,7 @@ public class UnflipUI
             if (!EvilWithinMod.EXPERIMENTAL_FLIP) return;
 
             __instance.hb.cX = Settings.WIDTH - __instance.hb.cX;
-            FlipRoom.startFBO(sb);
+            FlipRoom.beginFlip(sb);
         }
     }
 }
