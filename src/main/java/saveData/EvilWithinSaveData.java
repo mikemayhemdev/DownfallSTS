983a22c9c9e5cfe073bc50857efcd250dcb6da9a
Alchyr <isithalchyr@gmail.com>
2020-02-08 22:18:13 -0800

    Save stuff

---
+++ src/main/java/saveData/EvilWithinSaveData.java
@@ -0,0 +1,10 @@
+package saveData;
+
+//gotta put anything you wanna save in here
+public class EvilWithinSaveData {
+    public boolean EVIL_MODE = false;
+
+    public boolean RED_KEY_CONSUMED = false;
+    public boolean GREEN_KEY_CONSUMED = false;
+    public boolean BLUE_KEY_CONSUMED = false;
+}
