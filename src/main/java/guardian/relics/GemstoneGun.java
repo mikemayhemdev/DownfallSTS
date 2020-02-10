f64d870aef058254bd0ef3e6aca0abea1ad8e8ae
Vex Darkon <edanvirgo@gmail.com>
2020-02-09 19:45:19 -0700

    Gemstone Gun is gemstone FUNCTIONAL!

--- src/main/java/guardian/relics/GemstoneGun.java
+++ src/main/java/guardian/relics/GemstoneGun.java
@@ -1,18 +1,20 @@
 package guardian.relics;
 
 import basemod.abstracts.CustomRelic;
+import basemod.abstracts.CustomSavable;
 import com.badlogic.gdx.graphics.Texture;
 import com.megacrit.cardcrawl.actions.common.RelicAboveCreatureAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.CardGroup;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.helpers.PowerTip;
 import com.megacrit.cardcrawl.rooms.AbstractRoom;
 import expansioncontent.patches.CenterGridCardSelectScreen;
 import guardian.GuardianMod;
 
 import java.util.ArrayList;
 
-public class GemstoneGun extends CustomRelic {
+public class GemstoneGun extends CustomRelic implements CustomSavable<ArrayList<AbstractCard>> {
 
 
     public static final String ID = GuardianMod.makeID("GemstoneGun");
@@ -31,7 +33,7 @@ public class GemstoneGun extends CustomRelic {
     int numPicked = 0;
 
     public GemstoneGun() {
-        super(ID, new Texture(GuardianMod.getResourcePath(IMG_PATH)), new Texture(GuardianMod.getResourcePath(OUTLINE_IMG_PATH)), RelicTier.BOSS, LandingSound.CLINK);
+        super(ID, new Texture(GuardianMod.getResourcePath(IMG_PATH)), new Texture(GuardianMod.getResourcePath(OUTLINE_IMG_PATH)), RelicTier.RARE, LandingSound.CLINK);
         allDone = false;
     }
 
@@ -50,6 +52,23 @@ public class GemstoneGun extends CustomRelic {
         myGemThree.use(AbstractDungeon.player, null);
     }
 
+    @Override
+    public ArrayList<AbstractCard> onSave() {
+        ArrayList<AbstractCard> myList = new ArrayList<>();
+        myList.add(myGemOne);
+        myList.add(myGemTwo);
+        myList.add(myGemThree);
+        return myList;
+    }
+
+    @Override
+    public void onLoad(ArrayList<AbstractCard> abstractCards) {
+        myGemOne = abstractCards.get(0);
+        myGemTwo = abstractCards.get(1);
+        myGemThree = abstractCards.get(2);
+        setDescriptionAfterLoading();
+    }
+
     private void openScreen() {
         this.cardSelected = false;
         if (AbstractDungeon.isScreenUp) {
@@ -65,7 +84,7 @@ public class GemstoneGun extends CustomRelic {
         for (AbstractCard q : gems) {
             group.addToTop(q);
         }
-        AbstractDungeon.gridSelectScreen.open(group, 1, this.DESCRIPTIONS[1], false, false, false, false);
+        AbstractDungeon.gridSelectScreen.open(group, 1, "Choose a Gem to load the Gem Gun.", false, false, false, false);
     }
 
     @Override
@@ -99,10 +118,17 @@ public class GemstoneGun extends CustomRelic {
         }
     }
 
+    private void setDescriptionAfterLoading() {
+        this.description = DESCRIPTIONS[1] + myGemOne.rawDescription.replaceAll("Gem. NL ", "") + " NL " + myGemTwo.rawDescription.replaceAll("Gem. NL ", "") + " NL " + myGemThree.rawDescription.replaceAll("Gem. NL ", "");
+        tips.clear();
+        tips.add(new PowerTip(name, description));
+        initializeTips();
+    }
+
     @Override
     public String getUpdatedDescription() {
         if (allDone) {
-            return DESCRIPTIONS[1] + myGemOne.rawDescription + " NL " + myGemTwo.rawDescription + " NL " + myGemThree.rawDescription;
+            return DESCRIPTIONS[1] + myGemOne.rawDescription.replaceAll("Gem. NL ", "") + " NL " + myGemTwo.rawDescription.replaceAll("Gem. NL ", "") + " NL " + myGemThree.rawDescription.replaceAll("Gem. NL ", "");
         }
         return DESCRIPTIONS[0];
     }
