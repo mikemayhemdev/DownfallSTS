d5dacda0011a977514726a21a402bf874382eec5
mikemayhemdev <mikemayhemdev@gmail.com>
2020-05-03 17:33:22 -0400

    Anticard art and bug fixing.

--- src/main/java/charbosses/cards/anticards/PeaceOut.java
+++ src/main/java/charbosses/cards/anticards/PeaceOut.java
@@ -3,6 +3,7 @@ package charbosses.cards.anticards;
 import charbosses.actions.unique.EnemyChangeStanceAction;
 import charbosses.bosses.AbstractCharBoss;
 import charbosses.bosses.Watcher.CharBossWatcher;
+import charbosses.powers.cardpowers.EnemyMantraPower;
 import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
@@ -13,16 +14,16 @@ public class PeaceOut extends AbstractExpansionCard {
     public final static String ID = makeID("PeaceOut");
 
     public PeaceOut() {
-        super(ID, 1, CardType.SKILL, CardRarity.SPECIAL, CardTarget.SELF);
+        super(ID, 1, CardType.SKILL, CardRarity.SPECIAL, CardTarget.ENEMY);
         exhaust = true;
         selfRetain = true;
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
-        if (AbstractCharBoss.boss instanceof CharBossWatcher) {
+        if (m instanceof CharBossWatcher) {
             atb(new EnemyChangeStanceAction("Neutral"));
         }
-        atb(new RemoveSpecificPowerAction(m, p, MantraPower.POWER_ID));
+        atb(new RemoveSpecificPowerAction(m, p, EnemyMantraPower.POWER_ID));
     }
 
     public void upgrade() {
