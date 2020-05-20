57a722e45c14eebbadcac6ef65f7eee0e6aebc93
Vex Darkon <edanvirgo@gmail.com>
2020-05-19 18:12:03 -0700

    Council's Justice

--- src/main/java/theHexaghost/cards/CouncilsJustice.java
+++ src/main/java/theHexaghost/cards/CouncilsJustice.java
@@ -1,7 +1,10 @@
 package theHexaghost.cards;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.colorless.Apparition;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 public class CouncilsJustice extends AbstractHexaCard {
@@ -10,14 +13,47 @@ public class CouncilsJustice extends AbstractHexaCard {
 
     //stupid intellij stuff ATTACK, ENEMY, SPECIAL
 
-    private static final int DAMAGE = 16;
+    private static final int DAMAGE = 6;
     private static final int UPG_DAMAGE = 4;
 
     public CouncilsJustice() {
         super(ID, 1, CardType.ATTACK, CardRarity.SPECIAL, CardTarget.ENEMY, CardColor.COLORLESS);
         baseDamage = DAMAGE;
+        baseMagicNumber = 3;
         exhaust = true;
+        isEthereal = true;
         tags.add(CardTags.STRIKE);
+        cardsToPreview = new Apparition();
+    }
+
+    @Override
+    public void applyPowers() {
+        int oldBase = baseDamage;
+        for (AbstractCard q : AbstractDungeon.player.exhaustPile.group) {
+            if (q.cardID.equals(Apparition.ID)) {
+                baseDamage += magicNumber;
+            }
+        }
+        super.applyPowers();
+        baseDamage = oldBase;
+        if (baseDamage != damage) {
+            isDamageModified = true;
+        }
+    }
+
+    @Override
+    public void calculateCardDamage(AbstractMonster mo) {
+        int oldBase = baseDamage;
+        for (AbstractCard q : AbstractDungeon.player.exhaustPile.group) {
+            if (q.cardID.equals(Apparition.ID)) {
+                baseDamage += magicNumber;
+            }
+        }
+        super.calculateCardDamage(mo);
+        baseDamage = oldBase;
+        if (baseDamage != damage) {
+            isDamageModified = true;
+        }
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
@@ -28,6 +64,7 @@ public class CouncilsJustice extends AbstractHexaCard {
         if (!upgraded) {
             upgradeName();
             upgradeDamage(UPG_DAMAGE);
+            upgradeMagicNumber(1);
         }
     }
 }
