a77e7634897612bcc5e55dc05e9a18d3c85d41d4
Rita <13536709069@163.com>
2020-05-26 15:38:50 +0800

    fixed Snecko cards crash in zhs and Change Gold Action wont give golds

--- src/main/java/sneckomod/actions/ChangeGoldAction.java
+++ src/main/java/sneckomod/actions/ChangeGoldAction.java
@@ -1,6 +1,7 @@
 package sneckomod.actions;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.vfx.GainGoldTextEffect;
 import com.megacrit.cardcrawl.vfx.RainingGoldEffect;
@@ -17,6 +18,7 @@ public class ChangeGoldAction extends AbstractGameAction {
         if (amount > 0) {
             AbstractDungeon.effectList.add(new RainingGoldEffect(amount));
             AbstractDungeon.effectList.add(new GainGoldTextEffect(amount));
+            AbstractDungeon.player.gainGold(amount);
         } else if (amount < 0) {
             AbstractDungeon.player.loseGold(-amount);
             AbstractDungeon.effectList.add(new LoseGoldTextEffect(-amount));
