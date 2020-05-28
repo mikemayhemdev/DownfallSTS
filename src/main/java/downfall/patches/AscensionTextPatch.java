47ce78561b57abd1863582aeab3a4818dd5e9ea1
Rita <13536709069@163.com>
2020-05-28 11:13:08 +0800

    Ascension text localize

--- src/main/java/downfall/patches/AscensionTextPatch.java
+++ src/main/java/downfall/patches/AscensionTextPatch.java
@@ -2,19 +2,25 @@ package downfall.patches;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.UIStrings;
 import com.megacrit.cardcrawl.screens.charSelect.CharacterSelectScreen;
+import downfall.downfallMod;
 
 @SpirePatch(
         clz = CharacterSelectScreen.class,
         method = "render"
 )
 public class AscensionTextPatch {
+
+      private static UIStrings uiStrings = CardCrawlGame.languagePack.getUIString(downfallMod.makeID("AscensionText"));
+
     public static void Prefix(CharacterSelectScreen __instance, SpriteBatch sb) {
         if (EvilModeCharacterSelect.evilMode) {
             if (__instance.ascensionLevel == 20) {
-                __instance.ascLevelInfoString = "20. Act 3 Boss has Lizard Tail and Magic Flower.";
+                __instance.ascLevelInfoString = uiStrings.TEXT[1];
             } else if (__instance.ascensionLevel == 19) {
-                __instance.ascLevelInfoString = "19. Bosses have an extra Relic.";
+                __instance.ascLevelInfoString = uiStrings.TEXT[0];
             }
         }
     }
