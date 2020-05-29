b11cade5104e1d0c6f905cc74cbd06beec1e45f2
Rita <13536709069@163.com>
2020-05-29 11:16:00 +0800

    compatibility for Guardian chan mod

--- src/main/java/TheGuardianChan/TheGuardianChan.java
+++ src/main/java/TheGuardianChan/TheGuardianChan.java
@@ -45,15 +45,29 @@ public class TheGuardianChan
     public static final String MODNAME = "TheGuardianChan";
     public static final String AUTHOR = "Rita";
     public static final String DESCRIPTION = "";
-//设置用变量
+    //设置用变量
     public static boolean GuardianOriginalAnimation = true;
     public static boolean SlimeOriginalAnimation = true;
     public static boolean HexaghostOriginalAnimation = true;
     public static boolean SneckoOriginalAnimation = true;
 
+    public static boolean hexaghostMask = false;
+    public static boolean hexaghostSurroundDisplay = false;
+
     public static boolean disablePortraitAnimation = false;
 
-//
+    public static boolean displaySkin_Snecko = false;
+    public static boolean displaySkin_Guardian = false;
+    public static boolean displaySkin_Hexaghost = false;
+
+    public static boolean displaySkin_SlimeBoss = false;
+    public static boolean displaySkin_SlimeAcid_L = false;
+    public static boolean displaySkin_SlimeAcid_M = false;
+    public static boolean displaySkin_SlimeAcid_S = false;
+    public static boolean displaySkin_SlimeSpike_L = false;
+    public static boolean displaySkin_SlimeSpike_M = false;
+    public static boolean displaySkin_SlimeSpike_S = false;
+    //
     private static final float configRow = 50.0f;
     private static final float configColumn = 300.0f;
 
@@ -67,6 +81,9 @@ public class TheGuardianChan
 
     @SuppressWarnings("unused")
     public static void initialize() {   new TheGuardianChan();
+
+        foundmod_GuardianMod = Loader.isModLoaded("Guardian");
+        if(foundmod_GuardianMod){logger.info("==========================守护者mod存在=============================");}
 
     }
 
@@ -113,12 +130,56 @@ public class TheGuardianChan
         ModPanel settingsPanel = new ModPanel();
         BaseMod.registerModBadge(badgeTexture, MODNAME, AUTHOR, DESCRIPTION, settingsPanel);
 
+        ModLabeledToggleButton hexaghostMaskSwitch = new ModLabeledToggleButton(CardCrawlGame.languagePack.getUIString(makeID("modSettings")).TEXT[0],400.0f, 720.0f, Settings.CREAM_COLOR, FontHelper.charDescFont,hexaghostMask, settingsPanel,
+                (label) -> {}, (button) -> {hexaghostMask = button.enabled;saveSettings();});
+        ModLabeledToggleButton hexaghostSurroundDisplaySwitch = new ModLabeledToggleButton(CardCrawlGame.languagePack.getUIString(makeID("modSettings")).TEXT[1],400.0f, 720.0f-configRow, Settings.CREAM_COLOR, FontHelper.charDescFont,hexaghostSurroundDisplay, settingsPanel,
+                (label) -> {}, (button) -> {hexaghostSurroundDisplay = button.enabled;saveSettings();});
         ModLabeledToggleButton disablePortraitAnimationSwitch = new ModLabeledToggleButton(CardCrawlGame.languagePack.getUIString(makeID("modSettings")).TEXT[2],400.0f, 720.0f-2*configRow, Settings.CREAM_COLOR, FontHelper.charDescFont,disablePortraitAnimation, settingsPanel,
                 (label) -> {}, (button) -> {disablePortraitAnimation = button.enabled;saveSettings();});
 
+        ModLabeledToggleButton displaySkin_SneckoSwitch = new ModLabeledToggleButton(CardCrawlGame.languagePack.getMonsterStrings(Snecko.ID).NAME,400.0f, 720.0f-4*configRow, Settings.CREAM_COLOR, FontHelper.charDescFont,displaySkin_Snecko, settingsPanel,
+                (label) -> {}, (button) -> {displaySkin_Snecko = button.enabled;saveSettings();});
+        ModLabeledToggleButton displaySkin_GuardianSwitch = new ModLabeledToggleButton(CardCrawlGame.languagePack.getMonsterStrings(TheGuardian.ID).NAME,400.0f+configColumn, 720.0f-4*configRow, Settings.CREAM_COLOR, FontHelper.charDescFont,displaySkin_Guardian, settingsPanel,
+                (label) -> {}, (button) -> {displaySkin_Guardian = button.enabled;saveSettings();});
+        ModLabeledToggleButton displaySkin_HexaghostSwitch = new ModLabeledToggleButton(CardCrawlGame.languagePack.getMonsterStrings(Hexaghost.ID).NAME,400.0f+2*configColumn, 720.0f-4*configRow, Settings.CREAM_COLOR, FontHelper.charDescFont,displaySkin_Hexaghost, settingsPanel,
+                (label) -> {}, (button) -> {displaySkin_Hexaghost = button.enabled;saveSettings();});
+
+
+        ModLabeledToggleButton displaySkin_SlimeBossSwitch = new ModLabeledToggleButton(CardCrawlGame.languagePack.getMonsterStrings(SlimeBoss.ID).NAME,400.0f+3*configColumn, 720.0f-4*configRow, Settings.CREAM_COLOR, FontHelper.charDescFont,displaySkin_SlimeBoss, settingsPanel,
+                (label) -> {}, (button) -> {displaySkin_SlimeBoss = button.enabled;saveSettings();});
+        ModLabeledToggleButton displaySkin_SlimeAcid_LSwitch = new ModLabeledToggleButton(CardCrawlGame.languagePack.getMonsterStrings(AcidSlime_L.ID).NAME,400.0f, 720.0f-5*configRow, Settings.CREAM_COLOR, FontHelper.charDescFont,displaySkin_SlimeAcid_L, settingsPanel,
+                (label) -> {}, (button) -> {displaySkin_SlimeAcid_L = button.enabled;saveSettings();});
+        ModLabeledToggleButton displaySkin_SlimeAcid_MSwitch = new ModLabeledToggleButton(CardCrawlGame.languagePack.getMonsterStrings(AcidSlime_M.ID).NAME,400.0f+configColumn, 720.0f-5*configRow, Settings.CREAM_COLOR, FontHelper.charDescFont,displaySkin_SlimeAcid_M, settingsPanel,
+                (label) -> {}, (button) -> {displaySkin_SlimeAcid_M = button.enabled;saveSettings();});
+        ModLabeledToggleButton displaySkin_SlimeAcid_SSwitch = new ModLabeledToggleButton(CardCrawlGame.languagePack.getMonsterStrings(AcidSlime_S.ID).NAME,400.0f+2*configColumn, 720.0f-5*configRow, Settings.CREAM_COLOR, FontHelper.charDescFont,displaySkin_SlimeAcid_S, settingsPanel,
+                (label) -> {}, (button) -> {displaySkin_SlimeAcid_S = button.enabled;saveSettings();});
+        ModLabeledToggleButton displaySkin_SlimeSpike_LSwitch = new ModLabeledToggleButton(CardCrawlGame.languagePack.getMonsterStrings(SpikeSlime_L.ID).NAME,400.0f, 720.0f-6*configRow, Settings.CREAM_COLOR, FontHelper.charDescFont,displaySkin_SlimeSpike_L, settingsPanel,
+                (label) -> {}, (button) -> {displaySkin_SlimeSpike_L = button.enabled;saveSettings();});
+        ModLabeledToggleButton displaySkin_SlimeSpike_MSwitch = new ModLabeledToggleButton(CardCrawlGame.languagePack.getMonsterStrings(SpikeSlime_M.ID).NAME,400.0f+1*configColumn, 720.0f-6*configRow, Settings.CREAM_COLOR, FontHelper.charDescFont,displaySkin_SlimeSpike_M, settingsPanel,
+                (label) -> {}, (button) -> {displaySkin_SlimeSpike_M = button.enabled;saveSettings();});
+        ModLabeledToggleButton displaySkin_SlimeSpike_SSwitch = new ModLabeledToggleButton(CardCrawlGame.languagePack.getMonsterStrings(SpikeSlime_S.ID).NAME,400.0f+2*configColumn, 720.0f-6*configRow, Settings.CREAM_COLOR, FontHelper.charDescFont,displaySkin_SlimeSpike_S, settingsPanel,
+                (label) -> {}, (button) -> {displaySkin_SlimeSpike_S = button.enabled;saveSettings();});
+
         ModLabel displaySkinText = new ModLabel(CardCrawlGame.languagePack.getUIString(makeID("modSettings")).TEXT[3],400.0f, 720.0f-3*configRow, Settings.GOLD_COLOR, FontHelper.charDescFont,settingsPanel,(label) -> {});
 
+
+
+        settingsPanel.addUIElement(hexaghostMaskSwitch);
+        settingsPanel.addUIElement(hexaghostSurroundDisplaySwitch);
         settingsPanel.addUIElement(disablePortraitAnimationSwitch);
+
+
+        settingsPanel.addUIElement(displaySkin_SneckoSwitch);
+        settingsPanel.addUIElement(displaySkin_GuardianSwitch);
+        settingsPanel.addUIElement(displaySkin_HexaghostSwitch);
+
+        settingsPanel.addUIElement(displaySkin_SlimeBossSwitch);
+        settingsPanel.addUIElement(displaySkin_SlimeAcid_LSwitch);
+        settingsPanel.addUIElement(displaySkin_SlimeAcid_MSwitch);
+        settingsPanel.addUIElement(displaySkin_SlimeAcid_SSwitch);
+        settingsPanel.addUIElement(displaySkin_SlimeSpike_LSwitch);
+        settingsPanel.addUIElement(displaySkin_SlimeSpike_MSwitch);
+        settingsPanel.addUIElement(displaySkin_SlimeSpike_SSwitch);
 
         settingsPanel.addUIElement(displaySkinText);
 
@@ -131,8 +192,20 @@ public class TheGuardianChan
             config.setBool("SlimeOriginalAnimation", SlimeOriginalAnimation);
             config.setBool("HexaghostOriginalAnimation", HexaghostOriginalAnimation);
             config.setBool("SneckoOriginalAnimation", SneckoOriginalAnimation);
+            config.setBool("hexaghostMask", hexaghostMask);
+            config.setBool("hexaghostSurroundDisplay", hexaghostSurroundDisplay);
             config.setBool("disablePortraitAnimation", disablePortraitAnimation);
 
+            config.setBool("displaySkin_Snecko", displaySkin_Snecko);
+            config.setBool("displaySkin_Guardian", displaySkin_Guardian);
+            config.setBool("displaySkin_Hexaghost", displaySkin_Hexaghost);
+            config.setBool("displaySkin_SlimeBoss", displaySkin_SlimeBoss);
+            config.setBool("displaySkin_SlimeAcid_L", displaySkin_SlimeAcid_L);
+            config.setBool("displaySkin_SlimeAcid_M", displaySkin_SlimeAcid_M);
+            config.setBool("displaySkin_SlimeAcid_S", displaySkin_SlimeAcid_S);
+            config.setBool("displaySkin_SlimeSpike_L", displaySkin_SlimeSpike_L);
+            config.setBool("displaySkin_SlimeSpike_M", displaySkin_SlimeSpike_M);
+            config.setBool("displaySkin_SlimeSpike_S", displaySkin_SlimeSpike_S);
             config.save();
         } catch (Exception e) {
             e.printStackTrace();
@@ -147,9 +220,21 @@ public class TheGuardianChan
             SlimeOriginalAnimation = config.getBool("SlimeOriginalAnimation");
             HexaghostOriginalAnimation = config.getBool("HexaghostOriginalAnimation");
             SneckoOriginalAnimation = config.getBool("SneckoOriginalAnimation");
+            hexaghostMask = config.getBool("hexaghostMask");
+            hexaghostSurroundDisplay = config.getBool("hexaghostSurroundDisplay");
             disablePortraitAnimation = config.getBool("disablePortraitAnimation");
 
+            displaySkin_Snecko = config.getBool("displaySkin_Snecko");
+            displaySkin_Guardian = config.getBool("displaySkin_Guardian");
+            displaySkin_Hexaghost = config.getBool("displaySkin_Hexaghost");
 
+            displaySkin_SlimeBoss = config.getBool("displaySkin_SlimeBoss");
+            displaySkin_SlimeAcid_L = config.getBool("displaySkin_SlimeAcid_L");
+            displaySkin_SlimeAcid_M = config.getBool("displaySkin_SlimeAcid_M");
+            displaySkin_SlimeAcid_S = config.getBool("displaySkin_SlimeAcid_S");
+            displaySkin_SlimeSpike_L = config.getBool("displaySkin_SlimeSpike_L");
+            displaySkin_SlimeSpike_M = config.getBool("displaySkin_SlimeSpike_M");
+            displaySkin_SlimeSpike_S = config.getBool("displaySkin_SlimeSpike_S");
 
         } catch (Exception e) {
             e.printStackTrace();
