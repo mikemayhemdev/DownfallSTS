927fb43963bbddaefe87e114101cb8481fcae26b
Rita <13536709069@163.com>
2020-06-20 08:51:25 +0800

    zhs localization

--- src/main/java/downfall/powers/NeowInvulnerablePower.java
+++ src/main/java/downfall/powers/NeowInvulnerablePower.java
@@ -12,7 +12,9 @@ import downfall.monsters.NeowBoss;
 import theHexaghost.util.TextureLoader;
 
 public class NeowInvulnerablePower extends AbstractPower {
-    public static final String POWER_ID = "downfall:NeowInvulnerable";
+    public static final String POWER_ID = downfallMod.makeID("NeowInvulnerable");
+    public static final String NAME = CardCrawlGame.languagePack.getPowerStrings(POWER_ID).NAME;
+    public static final String DESCRIPTIONS[] = CardCrawlGame.languagePack.getPowerStrings(POWER_ID).DESCRIPTIONS;
 
     private static final Texture tex84 = TextureLoader.getTexture(downfallMod.assetPath("images/powers/NeowRez84.png"));
     private static final Texture tex32 = TextureLoader.getTexture(downfallMod.assetPath("images/powers/NeowRez32.png"));
@@ -27,8 +29,7 @@ public class NeowInvulnerablePower extends AbstractPower {
         this.region128 = new TextureAtlas.AtlasRegion(tex84, 0, 0, 84, 84);
         this.region48 = new TextureAtlas.AtlasRegion(tex32, 0, 0, 32, 32);
 
-        DESCRIPTIONS = CardCrawlGame.languagePack.getPowerStrings(this.ID).DESCRIPTIONS;
-        this.name = CardCrawlGame.languagePack.getPowerStrings(this.ID).NAME;
+        this.name = NAME;
 
         this.updateDescription();
 
