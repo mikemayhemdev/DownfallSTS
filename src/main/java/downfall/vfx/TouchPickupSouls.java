c4b85c6155024568c5107ed853890fb875b5b1ba
Alchyr <isithalchyr@gmail.com>
2020-06-21 01:48:55 -0700

    make soul effect prettier?
    The way they instantly vanish currently is a bit eh
    so, I did some stuff and removed the unused leftover bounce code from gold effect.

--- src/main/java/downfall/vfx/TouchPickupSouls.java
+++ src/main/java/downfall/vfx/TouchPickupSouls.java
@@ -29,73 +29,71 @@ public class TouchPickupSouls extends AbstractGameEffect {
     private float x;
     private float y;
     private float landingY;
-    private boolean willBounce;
-    private boolean hasBounced;
     private boolean flip;
-    private float bounceY;
-    private float bounceX;
     private float vY;
     private float vX;
-    private float gravity;
     private Hitbox hitbox;
 
     public TouchPickupSouls() {
         this.isPickupable = false;
         this.pickedup = false;
-        this.hasBounced = true;
-        this.vY = -0.2F;
-        this.vX = 0.0F;
-        this.gravity = -0.3F;
         this.img = ImageMaster.loadImage(downfallMod.assetPath("images/ui/Souls.png"));
 
-        this.willBounce = false;
-
         this.y = MathUtils.random(0F, Settings.HEIGHT * -0.15F);
-        this.x = MathUtils.random((float)Settings.WIDTH * 0.3F, (float)Settings.WIDTH * 0.95F) - (float)this.img.getWidth() / 2.0F;
+        this.x = MathUtils.random((float)Settings.WIDTH * 0.05F, (float)Settings.WIDTH * 0.95F) - (float)this.img.getWidth() / 2.0F;
         this.landingY = MathUtils.random(AbstractDungeon.floorY + (float)Settings.HEIGHT * 0.25F, AbstractDungeon.floorY + (float)Settings.HEIGHT * 0.38F);
         this.rotation = 180F + MathUtils.random(-30F, 30F);
         this.flip = MathUtils.randomBoolean();
         this.scale = Settings.scale;
+
+        this.vY = MathUtils.random(-1.0f, 1.0f) + (this.y - this.landingY) / 40.0f;
+        this.vX = MathUtils.random(-0.5f, 0.5f);
+
+        this.color = Color.WHITE.cpy();
+        this.color.a = 0.8f;
     }
 
     public TouchPickupSouls(boolean centerOnPlayer) {
         this();
         if (centerOnPlayer) {
             this.x = MathUtils.random(AbstractDungeon.player.drawX - AbstractDungeon.player.hb_w, AbstractDungeon.player.drawX + AbstractDungeon.player.hb_w);
-            this.gravity = -0.7F;
-        }
 
+            this.vY *= 0.5f;
+        }
     }
 
     public void update() {
-        if (!this.isPickupable) {
-            this.x += this.vX * Gdx.graphics.getDeltaTime() * 60.0F;
-            this.y -= this.vY * Gdx.graphics.getDeltaTime() * 60.0F;
-            this.vY += this.gravity;
-            if (this.y > this.landingY) {
-                if (this.hasBounced) {
-                    this.y = this.landingY;
-                    this.isPickupable = true;
-                    this.hitbox = new Hitbox(this.x - IMG_WIDTH * 2.0F, this.y - IMG_WIDTH * 2.0F, IMG_WIDTH * 4.0F, IMG_WIDTH * 4.0F);
-                } else {
-                    if (MathUtils.random(1) == 0) {
-                        this.hasBounced = true;
-                    }
+        this.x += this.vX * Gdx.graphics.getDeltaTime() * 60.0F;
+        this.y -= this.vY * Gdx.graphics.getDeltaTime() * 60.0F;
 
-                    this.y = this.landingY;
-                    this.vY = this.bounceY;
-                    this.vX = this.bounceX;
-                    this.bounceY *= 0.5F;
-                    this.bounceX *= 0.3F;
-                }
+        if (this.vY < -25f)
+        {
+            this.vY *= Math.max(0.5f, 1 - (2.5f * Gdx.graphics.getDeltaTime()));
+        }
+        else if (this.vY < -10f)
+        {
+            this.vY *= Math.max(0.5f, 1 - (Gdx.graphics.getDeltaTime()));
+        }
+
+
+        if (!this.isPickupable) {
+            if (this.y > this.landingY) {
+                this.isPickupable = true;
+                this.hitbox = new Hitbox(this.x - IMG_WIDTH * 2.0F, this.y - IMG_WIDTH * 2.0F, IMG_WIDTH * 4.0F, IMG_WIDTH * 4.0F);
             }
         } else if (!this.pickedup) {
+            this.color.a -= Gdx.graphics.getDeltaTime() * 2.0f;
             this.pickedup = true;
-            this.isDone = true;
             this.playGainGoldSFX();
             AbstractDungeon.effectsQueue.add(new ShineLinesEffect(this.x, this.y));
         }
+        else
+        {
+            this.color.a -= Gdx.graphics.getDeltaTime() * 2.0f;
 
+            if (this.color.a <= 0)
+                this.isDone = true;
+        }
     }
 
     private void playGainGoldSFX() {
@@ -114,7 +112,7 @@ public class TouchPickupSouls extends AbstractGameEffect {
     }
 
     public void render(SpriteBatch sb) {
-        sb.setColor(Color.WHITE);
+        sb.setColor(this.color);
         sb.draw(this.img, this.x, this.y, (float)this.img.getWidth() / 2.0F, (float)this.img.getHeight() / 2.0F, this.img.getWidth(), this.img.getHeight(), Settings.scale, Settings.scale, this.rotation, 0, 0, this.img.getWidth(), this.img.getHeight(), this.flip, false);
         if (this.hitbox != null) {
             this.hitbox.render(sb);
