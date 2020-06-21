308972487bb1a450c99fc1d4db79f6e22d173ced
Rita <13536709069@163.com>
2020-06-21 09:25:46 +0800

    localization fixes

--- src/main/java/expansioncontent/cards/CaCaw.java
+++ src/main/java/expansioncontent/cards/CaCaw.java
@@ -6,6 +6,7 @@ import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.monsters.city.Byrd;
 import com.megacrit.cardcrawl.powers.RitualPower;
 import com.megacrit.cardcrawl.vfx.MegaSpeechBubble;
 import expansioncontent.expansionContentMod;
@@ -28,7 +29,7 @@ public class CaCaw extends AbstractExpansionCard {
     public void use(AbstractPlayer p, AbstractMonster m) {
         CardCrawlGame.sound.playA("VO_CULTIST_1A", -.1f);
 
-        AbstractDungeon.effectList.add(new MegaSpeechBubble(p.hb.cX, p.hb.cY, 1.0F, "Caw-CAW!", true));
+        AbstractDungeon.effectList.add(new MegaSpeechBubble(p.hb.cX, p.hb.cY, 1.0F, Byrd.DIALOG[0], true));
 
         atb(new ApplyPowerAction(p, p, new RitualPower(p, 1, true), 1));
 
