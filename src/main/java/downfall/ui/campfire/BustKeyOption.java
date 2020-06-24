b56c1998defd91ee912c240fda49f820ec6ce83a
mikemayhemdev <mikemayhemdev@gmail.com>
2020-06-24 02:30:33 -0400

    Bust Key error texts when disabled

--- src/main/java/downfall/ui/campfire/BustKeyOption.java
+++ src/main/java/downfall/ui/campfire/BustKeyOption.java
@@ -36,7 +36,7 @@ public class BustKeyOption extends AbstractCampfireOption {
 
     public BustKeyOption(Keys key) {
         this.key = key;
-        if (AbstractDungeon.player.gold < 50) {
+        if (AbstractDungeon.player.gold < 75) {
             this.usable = false;
             updateImage(key);
         } else {
@@ -57,9 +57,13 @@ public class BustKeyOption extends AbstractCampfireOption {
                     this.img = TextureLoader.getTexture(downfallMod.assetPath("images/ui/campfire/sapphireDisabled.png"));
                 }
                 if (!this.used) {
-                    this.description += TEXT[5];
+                    if (this.usable) {
+                        this.description += TEXT[5];
+                    }else {
+                        this.description = TEXT[8];
+                    }
                 } else {
-                    this.description = "";
+                    this.description = TEXT[7];
                 }
                 break;
             case EMERALD:
@@ -70,9 +74,13 @@ public class BustKeyOption extends AbstractCampfireOption {
                     this.img = TextureLoader.getTexture(downfallMod.assetPath("images/ui/campfire/emeraldDisabled.png"));
                 }
                 if (!this.used) {
-                    this.description += TEXT[6];
+                    if (this.usable) {
+                        this.description += TEXT[6];
+                    }else {
+                        this.description = TEXT[8];
+                    }
                 } else {
-                    this.description = "";
+                    this.description = TEXT[7];
                 }
                 break;
             default:
@@ -83,9 +91,13 @@ public class BustKeyOption extends AbstractCampfireOption {
                     this.img = TextureLoader.getTexture(downfallMod.assetPath("images/ui/campfire/rubyDisabled.png"));
                 }
                 if (!this.used) {
-                    this.description += TEXT[4];
+                    if (this.usable) {
+                        this.description += TEXT[4];
+                    } else {
+                        this.description = TEXT[8];
+                    }
                 } else {
-                    this.description = "";
+                    this.description = TEXT[7];
                 }
         }
     }
@@ -94,11 +106,11 @@ public class BustKeyOption extends AbstractCampfireOption {
         super.update();
 
         if (!this.used) {
-            if (AbstractDungeon.player.gold < 50 && this.usable) {
+            if (AbstractDungeon.player.gold < 75 && this.usable) {
                 this.usable = false;
                 updateImage(key);
             }
-            if (AbstractDungeon.player.gold >= 50 && !this.usable) {
+            if (AbstractDungeon.player.gold >= 75 && !this.usable) {
                 this.usable = true;
                 updateImage(key);
             }
@@ -128,7 +140,7 @@ public class BustKeyOption extends AbstractCampfireOption {
         if (this.usable) {
             AbstractDungeon.effectList.add(new BustKeyEffect());
             AbstractPlayer p = AbstractDungeon.player;
-            p.loseGold(50);
+            p.loseGold(75);
             this.used = true;
             this.usable = false;
             switch (key) {
