0a7de795af8b99382185e4b3a91ce0ad0fd75a7b
mikemayhemdev <mikemayhemdev@gmail.com>
2020-06-26 04:15:32 -0400

    Defensive Flair nerfed a tad

--- src/main/java/sneckomod/cards/DefensiveFlair.java
+++ src/main/java/sneckomod/cards/DefensiveFlair.java
@@ -12,9 +12,9 @@ public class DefensiveFlair extends AbstractSneckoCard {
     //stupid intellij stuff SKILL, SELF, UNCOMMON
 
     private static final int BLOCK = 6;
-    private static final int UPG_BLOCK = 2;
+    private static final int UPG_BLOCK = 1;
 
-    private static final int MAGIC = 2;
+    private static final int MAGIC = 1;
     private static final int UPG_MAGIC = 1;
 
     public DefensiveFlair() {
