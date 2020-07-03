16a0cc57b76f3c00cf623a1254fcb6aa0d1d5d0b
mikemayhemdev <mikemayhemdev@gmail.com>
2020-07-02 20:09:28 -0400

    Fix Neow not applying start of turn powers

--- src/main/java/charbosses/actions/util/CharBossMonsterGroup.java
+++ src/main/java/charbosses/actions/util/CharBossMonsterGroup.java
@@ -40,6 +40,8 @@ public class CharBossMonsterGroup extends MonsterGroup {
                         m.applyStartOfTurnPowers();
                     }
                 }
+            } else {
+                m.applyStartOfTurnPowers();
             }
         }
 
