cf86e0a3b3f38944f89cb8f1a635c4ddcb201e6f
Vex Darkon <edanvirgo@gmail.com>
2020-09-20 14:34:24 -0700

    Lariat

---
+++ src/main/java/champ/cards/Lariat.java
@@ -0,0 +1,39 @@
+package champ.cards;
+
+import champ.actions.LariatAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.ui.panels.EnergyPanel;
+import downfall.actions.PerformXAction;
+
+public class Lariat extends AbstractChampCard {
+
+    public final static String ID = makeID("Lariat");
+
+    //stupid intellij stuff skill, self, uncommon
+
+    private static final int BLOCK = 5;
+    private static final int UPG_BLOCK = 1;
+
+    private static final int MAGIC = 5;
+    private static final int UPG_MAGIC = 1;
+
+    public Lariat() {
+        super(ID, -1, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.SELF);
+        baseBlock = BLOCK;
+        baseMagicNumber = magicNumber = MAGIC;
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        if (energyOnUse < EnergyPanel.totalCount) {
+            energyOnUse = EnergyPanel.totalCount;
+        }
+        LariatAction r = new LariatAction(magicNumber, block, dcombo() ? 1 : 0);
+        atb(new PerformXAction(r, p, energyOnUse, freeToPlayOnce));
+    }
+
+    public void upp() {
+        upgradeBlock(UPG_BLOCK);
+        upgradeMagicNumber(UPG_MAGIC);
+    }
+}
