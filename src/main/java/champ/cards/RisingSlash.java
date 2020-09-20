e5a663d0c95095ecd7f9a4553dff1d00a4a138d5
Vex Darkon <edanvirgo@gmail.com>
2020-09-20 15:48:38 -0700

    Rising Slash and fun with uuids

---
+++ src/main/java/champ/cards/RisingSlash.java
@@ -0,0 +1,49 @@
+package champ.cards;
+
+import champ.ChampMod;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.GameActionManager;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+public class RisingSlash extends AbstractChampCard {
+
+    public final static String ID = makeID("RisingSlash");
+
+    //stupid intellij stuff attack, enemy, uncommon
+
+    private static final int DAMAGE = 7;
+    private static final int UPG_DAMAGE = 3;
+
+    public RisingSlash() {
+        super(ID, 1, CardType.ATTACK, CardRarity.UNCOMMON, CardTarget.ENEMY);
+        baseDamage = DAMAGE;
+        tags.add(ChampMod.TECHNIQUE);
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        techique();
+        dmg(m, AbstractGameAction.AttackEffect.SLASH_DIAGONAL);
+        if (AbstractDungeon.actionManager.cardsPlayedThisTurn.get(AbstractDungeon.actionManager.cardsPlayedThisTurn.size() - 1).hasTag(ChampMod.TECHNIQUE) && AbstractDungeon.actionManager.cardsPlayedThisTurn.get(AbstractDungeon.actionManager.cardsPlayedThisTurn.size() - 1).uuid != this.uuid) {
+            AbstractCard r = this;
+            atb(new AbstractGameAction() {
+                @Override
+                public void update() {
+                    isDone = true;
+                    GameActionManager.queueExtraCard(r, m);
+                }
+            });
+        }
+    }
+
+    @Override
+    public void triggerOnGlowCheck() {
+        glowColor = (AbstractDungeon.actionManager.cardsPlayedThisTurn.get(AbstractDungeon.actionManager.cardsPlayedThisTurn.size() - 1).hasTag(ChampMod.TECHNIQUE) && AbstractDungeon.actionManager.cardsPlayedThisTurn.get(AbstractDungeon.actionManager.cardsPlayedThisTurn.size() - 1).uuid != this.uuid) ? AbstractCard.GOLD_BORDER_GLOW_COLOR : BLUE_BORDER_GLOW_COLOR;
+    }
+
+    public void upp() {
+        upgradeDamage(UPG_DAMAGE);
+    }
+}
