dc1be5e4fcdb6f0e203f9d2f55effb6042b49b09
Vex Darkon <edanvirgo@gmail.com>
2020-09-20 11:39:08 -0700

    Tornado Punch, also loc for Skillful Dodge because I forgot that before commmitting the card

---
+++ src/main/java/champ/cards/TornadoPunch.java
@@ -0,0 +1,52 @@
+package champ.cards;
+
+import champ.powers.CounterPower;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.animations.VFXAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.utility.SFXAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.vfx.combat.WhirlwindEffect;
+
+public class TornadoPunch extends AbstractChampCard {
+
+    public final static String ID = makeID("TornadoPunch");
+
+    //stupid intellij stuff attack, all_enemy, common
+
+    private static final int DAMAGE = 10;
+    private static final int UPG_DAMAGE = 2;
+
+    private static final int MAGIC = 5;
+    private static final int UPG_MAGIC = 3;
+
+    public TornadoPunch() {
+        super(ID, 2, CardType.ATTACK, CardRarity.COMMON, CardTarget.ENEMY);
+        baseDamage = DAMAGE;
+        baseMagicNumber = magicNumber = MAGIC;
+        isMultiDamage = true;
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        atb(new SFXAction("ATTACK_WHIRLWIND"));
+        atb(new VFXAction(new WhirlwindEffect(), 0.0F));
+        allDmg(AbstractGameAction.AttackEffect.BLUNT_HEAVY);
+        if (dcombo()) atb(new AbstractGameAction() {
+            @Override
+            public void update() {
+                isDone = true;
+                for (AbstractMonster q : monsterList()) {
+                    if (q.getIntentBaseDmg() > -1) {
+                        att(new ApplyPowerAction(p, p, new CounterPower(magicNumber), magicNumber));
+                    }
+                }
+            }
+        });
+    }
+
+    public void upp() {
+        upgradeDamage(UPG_DAMAGE);
+        upgradeMagicNumber(UPG_MAGIC);
+    }
+}
