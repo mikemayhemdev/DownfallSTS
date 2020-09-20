90832e0681a65f1a033757a04bd80295d75f9115
Vex Darkon <edanvirgo@gmail.com>
2020-09-20 10:30:11 -0700

    Flash Cut

---
+++ src/main/java/champ/cards/FlashCut.java
@@ -0,0 +1,36 @@
+package champ.cards;
+
+import champ.powers.CounterPower;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+public class FlashCut extends AbstractChampCard {
+
+    public final static String ID = makeID("FlashCut");
+
+    //stupid intellij stuff attack, enemy, common
+
+    private static final int DAMAGE = 9;
+    private static final int UPG_DAMAGE = 3;
+
+    private static final int MAGIC = 9;
+    private static final int UPG_MAGIC = 3;
+
+    public FlashCut() {
+        super(ID, 1, CardType.ATTACK, CardRarity.COMMON, CardTarget.ENEMY);
+        baseDamage = DAMAGE;
+        baseMagicNumber = magicNumber = MAGIC;
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        gladOpen();
+        dmg(m, AbstractGameAction.AttackEffect.SLASH_VERTICAL);
+        if (dcombo()) applyToSelf(new CounterPower(9));
+    }
+
+    public void upp() {
+        upgradeDamage(UPG_DAMAGE);
+        upgradeMagicNumber(UPG_MAGIC);
+    }
+}
