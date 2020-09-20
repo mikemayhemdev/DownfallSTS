2f2140549610ec1821c260224c708ec569993cca
Vex Darkon <edanvirgo@gmail.com>
2020-09-20 14:42:05 -0700

    Haymaker

---
+++ src/main/java/champ/cards/Haymaker.java
@@ -0,0 +1,32 @@
+package champ.cards;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+public class Haymaker extends AbstractChampCard {
+
+    public final static String ID = makeID("Haymaker");
+
+    //stupid intellij stuff attack, enemy, common
+
+    private static final int DAMAGE = 12;
+    private static final int UPG_DAMAGE = 4;
+
+    public Haymaker() {
+        super(ID, 2, CardType.ATTACK, CardRarity.COMMON, CardTarget.ENEMY);
+        baseDamage = DAMAGE;
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        techique();
+        dmg(m, AbstractGameAction.AttackEffect.BLUNT_HEAVY);
+        if (gcombo()) {
+            for (AbstractMonster q : monsterList()) applyToEnemy(q, autoWeak(q, 1));
+        } else applyToEnemy(m, autoWeak(m, 1));
+    }
+
+    public void upp() {
+        upgradeDamage(UPG_DAMAGE);
+    }
+}
