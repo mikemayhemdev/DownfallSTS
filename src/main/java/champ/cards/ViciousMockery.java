e5689f3757d50c8c93cedfe6ef6f372a0b4c8cc7
Vex Darkon <edanvirgo@gmail.com>
2020-09-20 14:44:56 -0700

    Vicious Mockery - very champy card

---
+++ src/main/java/champ/cards/ViciousMockery.java
@@ -0,0 +1,30 @@
+package champ.cards;
+
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.DexterityPower;
+
+public class ViciousMockery extends AbstractChampCard {
+
+    public final static String ID = makeID("ViciousMockery");
+
+    //stupid intellij stuff skill, enemy, uncommon
+
+    public ViciousMockery() {
+        super(ID, 1, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.ENEMY);
+        exhaust = true;
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        techique();
+        if (gcombo()) {
+            if (m.getIntentBaseDmg() > -1) applyToSelf(new DexterityPower(p, 1));
+        }
+    }
+
+    public void upp() {
+        exhaust = false;
+        rawDescription = UPGRADE_DESCRIPTION;
+        initializeDescription();
+    }
+}
