79f6ae7f3b866f8e24798eda5cc49938697d6ede
Vex Darkon <edanvirgo@gmail.com>
2020-09-20 14:50:29 -0700

    Crooked Strike

---
+++ src/main/java/champ/cards/CrookedStrike.java
@@ -0,0 +1,44 @@
+package champ.cards;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.GainStrengthPower;
+import com.megacrit.cardcrawl.powers.StrengthPower;
+
+public class CrookedStrike extends AbstractChampCard {
+
+    public final static String ID = makeID("CrookedStrike");
+
+    //stupid intellij stuff attack, enemy, uncommon
+
+    private static final int DAMAGE = 8;
+    private static final int UPG_DAMAGE = 4;
+
+    public CrookedStrike() {
+        super(ID, 1, CardType.ATTACK, CardRarity.UNCOMMON, CardTarget.ENEMY);
+        baseDamage = DAMAGE;
+        tags.add(CardTags.STRIKE);
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        techique();
+        dmg(m, AbstractGameAction.AttackEffect.SLASH_DIAGONAL);
+        if (gcombo()) {
+            atb(new AbstractGameAction() {
+                @Override
+                public void update() {
+                    isDone = true;
+                    int x = p.hand.size();
+                    att(new ApplyPowerAction(m, p, new GainStrengthPower(m, x), x));
+                    att(new ApplyPowerAction(m, p, new StrengthPower(m, -x), -x));
+                }
+            });
+        }
+    }
+
+    public void upp() {
+        upgradeDamage(UPG_DAMAGE);
+    }
+}
