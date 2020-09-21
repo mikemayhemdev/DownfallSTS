8487b089acc5632f32880d62606c3bfc974a23bf
Vex Darkon <edanvirgo@gmail.com>
2020-09-20 19:06:46 -0700

    Berserker Style

---
+++ src/main/java/champ/cards/BerserkerStyle.java
@@ -0,0 +1,26 @@
+package champ.cards;
+
+import champ.powers.BerserkerStylePower;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+public class BerserkerStyle extends AbstractChampCard {
+
+    public final static String ID = makeID("DefensiveStyle");
+
+    //stupid intellij stuff power, self, uncommon
+
+    public BerserkerStyle() {
+        super(ID, 1, CardType.POWER, CardRarity.UNCOMMON, CardTarget.SELF);
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        applyToSelf(new BerserkerStylePower(2));
+    }
+
+    public void upp() {
+        isInnate = true;
+        rawDescription = UPGRADE_DESCRIPTION;
+        initializeDescription();
+    }
+}
