fd1e671a2f4a0e4a84200ae45fd3b97054107db1
Vex Darkon <edanvirgo@gmail.com>
2020-09-20 17:34:38 -0700

    Crown Throw

---
+++ src/main/java/champ/powers/DrawLessNextTurnPower.java
@@ -0,0 +1,62 @@
+package champ.powers;
+
+import basemod.interfaces.CloneablePowerInterface;
+import champ.ChampMod;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import theHexaghost.HexaMod;
+import theHexaghost.util.TextureLoader;
+
+public class DrawLessNextTurnPower extends AbstractPower implements CloneablePowerInterface {
+
+    public static final String POWER_ID = ChampMod.makeID("DrawLessNextTurnPower");
+
+    private static final Texture tex84 = TextureLoader.getTexture(HexaMod.getModID() + "Resources/images/powers/Again84.png");
+    private static final Texture tex32 = TextureLoader.getTexture(HexaMod.getModID() + "Resources/images/powers/Again32.png");
+    private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    public static final String NAME = powerStrings.NAME;
+    public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+
+    public DrawLessNextTurnPower(final int amount) {
+        this.name = NAME;
+        this.ID = POWER_ID;
+        this.owner = AbstractDungeon.player;
+        this.amount = amount;
+        this.type = PowerType.BUFF;
+        this.isTurnBased = true;
+
+        this.region128 = new TextureAtlas.AtlasRegion(tex84, 0, 0, 84, 84);
+        this.region48 = new TextureAtlas.AtlasRegion(tex32, 0, 0, 32, 32);
+
+        this.updateDescription();
+    }
+
+    @Override
+    public void onInitialApplication() {
+        AbstractDungeon.player.gameHandSize -= amount;
+    }
+
+    @Override
+    public void onRemove() {
+        AbstractDungeon.player.gameHandSize += amount;
+    }
+
+    public void atEndOfRound() {
+        AbstractDungeon.actionManager.addToBottom(new RemoveSpecificPowerAction(owner, owner, ID));
+    }
+
+    @Override
+    public void updateDescription() {
+        description = DESCRIPTIONS[0] + amount + (amount == 1 ? DESCRIPTIONS[1] : DESCRIPTIONS[2]);
+    }
+
+    @Override
+    public AbstractPower makeCopy() {
+        return new DrawLessNextTurnPower(amount);
+    }
+}
