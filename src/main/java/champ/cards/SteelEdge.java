4bb68abed5a7792d8080eb3e43ca8c6ab68aa552
Vex Darkon <edanvirgo@gmail.com>
2020-09-20 18:20:08 -0700

    Steel edge. It's neat making sure things are designed correctly for Ultimate Form later on

---
+++ src/main/java/champ/cards/SteelEdge.java
@@ -0,0 +1,38 @@
+package champ.cards;
+
+import champ.actions.SteelEdgeAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.ui.panels.EnergyPanel;
+import downfall.actions.PerformXAction;
+
+public class SteelEdge extends AbstractChampCard {
+
+    public final static String ID = makeID("SteelEdge");
+
+    //stupid intellij stuff attack, enemy, uncommon
+
+    private static final int DAMAGE = 10;
+    private static final int BLOCK = 8;
+
+    public SteelEdge() {
+        super(ID, -1, CardType.ATTACK, CardRarity.UNCOMMON, CardTarget.ENEMY);
+        baseDamage = DAMAGE;
+        baseBlock = BLOCK;
+        baseMagicNumber = magicNumber = 0;
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        if (energyOnUse < EnergyPanel.totalCount) {
+            energyOnUse = EnergyPanel.totalCount;
+        }
+        SteelEdgeAction r = new SteelEdgeAction(damage, block, magicNumber, m);
+        atb(new PerformXAction(r, p, energyOnUse, freeToPlayOnce));
+    }
+
+    public void upp() {
+        upgradeMagicNumber(1);
+        rawDescription = UPGRADE_DESCRIPTION;
+        initializeDescription();
+    }
+}
