f1c8fb639a776bbf7890fbec2455af9af6fd596f
Vex Darkon <edanvirgo@gmail.com>
2020-09-25 11:11:05 -0700

    Tech potion + fixes + text

--- src/main/java/champ/potions/TechPotion.java
+++ src/main/java/champ/potions/TechPotion.java
@@ -4,48 +4,50 @@ package champ.potions;
 import basemod.abstracts.CustomPotion;
 import champ.ChampMod;
 import champ.actions.DiscoverOpenerAction;
+import champ.stances.AbstractChampStance;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.helpers.PowerTip;
 import com.megacrit.cardcrawl.localization.PotionStrings;
+import com.megacrit.cardcrawl.relics.SacredBark;
 
 
-public class OpenerPotion extends CustomPotion {
+public class TechPotion extends CustomPotion {
     public static final String POTION_ID = "champ:OpenerPotion";
     private static final PotionStrings potionStrings = CardCrawlGame.languagePack.getPotionString(POTION_ID);
     public static final String NAME = potionStrings.NAME;
     public static final String[] DESCRIPTIONS = potionStrings.DESCRIPTIONS;
 
-    public OpenerPotion() {
-        super(NAME, POTION_ID, PotionRarity.COMMON, PotionSize.H, PotionColor.SMOKE);
+    public TechPotion() {
+        super(NAME, POTION_ID, PotionRarity.UNCOMMON, PotionSize.H, PotionColor.SMOKE);
         this.isThrown = false;
         this.targetRequired = false;
         this.labOutlineColor = ChampMod.placeholderColor;
     }
 
-
     public void initializeData() {
         this.potency = getPotency();
-        if (AbstractDungeon.player != null && AbstractDungeon.player.hasRelic("SacredBark")) {
-            this.description = potionStrings.DESCRIPTIONS[1];
-        } else {
-            this.description = potionStrings.DESCRIPTIONS[0];
-        }
+        this.description = potionStrings.DESCRIPTIONS[0] + this.potency + potionStrings.DESCRIPTIONS[1];
         this.tips.clear();
         this.tips.add(new PowerTip(this.name, this.description));
     }
 
     public void use(AbstractCreature target) {
-        addToBot(new DiscoverOpenerAction(AbstractDungeon.player.hasRelic("SacredBark")));
+        for (int i = 0; i < potency; i++) {
+            if (AbstractDungeon.player.stance instanceof AbstractChampStance)
+                ((AbstractChampStance) AbstractDungeon.player.stance).techique();
+        }
     }
 
     public CustomPotion makeCopy() {
-        return new OpenerPotion();
+        return new TechPotion();
     }
 
     public int getPotency(int ascensionLevel) {
-        return 1;
+        if (AbstractDungeon.player.hasRelic(SacredBark.ID))
+            return 6;
+        return 3;
     }
 }
 
