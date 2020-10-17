e39714863319c894ad5cedad246a704068b03081
mikemayhemdev <mikemayhemdev@gmail.com>
2020-10-17 12:44:04 -0400

    Merchant got its groove back

--- src/main/java/charbosses/bosses/Merchant/ArchetypeAct3MerchantBoss.java
+++ src/main/java/charbosses/bosses/Merchant/ArchetypeAct3MerchantBoss.java
@@ -29,17 +29,17 @@ public class ArchetypeAct3MerchantBoss extends ArchetypeBaseMerchant {
 
         addRelic(new CBR_NeowsBlessing());
         addRelic(new CBR_MercuryHourglass());
-        //addRelic(new CBR_Torii());
+        addRelic(new CBR_Torii());
         //addRelic(new CBR_SelfFormingClay());
-        //addRelic(new CBR_IncenseBurner());
+        addRelic(new CBR_IncenseBurner());
         addRelic(new CBR_Calipers());
-        //addRelic(new CBR_Girya(3));
-        //addRelic(new CBR_Vajra());
+        addRelic(new CBR_Girya(3));
+        addRelic(new CBR_Vajra());
         addRelic(new CBR_SmoothStone());
         addRelic(new CBR_FossilizedHelix());
         //addRelic(new CBR_BagOfPreparation());
-        //addRelic(new CBR_ClockworkSouvenir());
-        //addRelic(new CBR_TungstenRod());
+        addRelic(new CBR_ClockworkSouvenir());
+        addRelic(new CBR_TungstenRod());
         addRelic(new CBR_IceCream());
         //addRelic(new CBR_FusionHammer());
 
