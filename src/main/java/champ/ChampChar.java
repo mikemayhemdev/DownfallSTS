85b3b798ce9fbea40ee3498f9488c54eaa5e37b9
mikemayhemdev <mikemayhemdev@gmail.com>
2020-12-08 10:37:40 -0500

    Champ positioning fixed

--- src/main/java/champ/ChampChar.java
+++ src/main/java/champ/ChampChar.java
@@ -90,10 +90,11 @@ public class ChampChar extends CustomPlayer {
                 SHOULDER1,
                 SHOULDER2,
                 CORPSE,
-                getLoadout(), -15.0F, 0.0F, 450.0F, 450.0F, new EnergyManager(3));
+                getLoadout(), -15.0F, -30.0F, 450.0F, 450.0F, new EnergyManager(3));
 
         this.reloadAnimation();
 
+        this.drawY = this.drawY + 25F * Settings.scale;
         dialogX = (drawX + 0.0F * Settings.scale);
         dialogY = (drawY + 240.0F * Settings.scale);
 
