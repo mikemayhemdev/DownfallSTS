6f0520799bd5f09af8c381aa1405ceebfb04aa98
mikemayhemdev <mikemayhemdev@gmail.com>
2020-12-08 13:00:15 -0500

    Incinerate nerfed, Rain of Embers buffed

--- src/main/java/theHexaghost/cards/RainOfEmbers.java
+++ src/main/java/theHexaghost/cards/RainOfEmbers.java
@@ -1,7 +1,10 @@
 package theHexaghost.cards;
 
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.WeakPower;
 import com.megacrit.cardcrawl.ui.panels.EnergyPanel;
 import theHexaghost.actions.EmbersAction;
 import downfall.actions.PerformXAction;
@@ -26,14 +29,30 @@ public class RainOfEmbers extends AbstractHexaCard {
         if (energyOnUse < EnergyPanel.totalCount) {
             energyOnUse = EnergyPanel.totalCount;
         }
+
         EmbersAction r = new EmbersAction(magicNumber, p, m, damage, damageTypeForTurn, burn);
         atb(new PerformXAction(r, p, energyOnUse, freeToPlayOnce));
+
+        if (energyOnUse >= 3){
+
+            for (AbstractMonster q : monsterList()) {
+                if (upgraded){
+                    applyToEnemy(q, autoWeak(q, 2));
+                    applyToEnemy(q, autoVuln(q, 2));
+                } else {
+                    applyToEnemy(q, autoWeak(q, 1));
+                    applyToEnemy(q, autoVuln(q, 1));
+                }
+            }
+
+        }
     }
 
     public void upgrade() {
         if (!upgraded) {
             upgradeName();
-            upgradeMagicNumber(1);
+            upgradeDamage(1);
+            upgradeBurn(1);
             rawDescription = UPGRADE_DESCRIPTION;
             initializeDescription();
         }
