073bf9b132c524df8da41a802074b3060fd6494c
Vex Darkon <edanvirgo@gmail.com>
2020-12-14 16:40:42 -0700

    Core actions in

---
+++ src/main/java/automaton/actions/FireFromPileAction.java
@@ -0,0 +1,31 @@
+package automaton.actions;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.CardGroup;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+
+public class FireFromPileAction extends AbstractGameAction {
+    private CardGroup g;
+
+    public FireFromPileAction(CardGroup t, int amount) {
+        this.g = t;
+        this.amount = amount;
+    }
+
+    @Override
+    public void update() {
+        if (duration == startDuration) {
+            AbstractDungeon.gridSelectScreen.open(g, amount, false, "Choose."); //TODO: Localize
+            tickDuration();
+        }
+        else {
+            if (!AbstractDungeon.gridSelectScreen.selectedCards.isEmpty()) {
+                for (AbstractCard c : AbstractDungeon.gridSelectScreen.selectedCards) {
+                    addToTop(new EasyAutoplayAction(c));
+                }
+                isDone = true;
+            }
+        }
+    }
+}
