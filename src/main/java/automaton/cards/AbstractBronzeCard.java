ac51bcaab26c7b4b9e2c5a1c7394bf5ffa18517a
Vex Darkon <edanvirgo@gmail.com>
2020-12-14 16:10:25 -0700

    AbstractBronzeCard (more to come) and some other stuff. I think I foudn a bug

---
+++ src/main/java/automaton/cards/AbstractBronzeCard.java
@@ -0,0 +1,168 @@
+package automaton.cards;
+
+import basemod.abstracts.CustomCard;
+import basemod.helpers.TooltipInfo;
+import champ.ChampChar;
+import champ.ChampMod;
+import champ.actions.FatigueHpLossAction;
+import champ.powers.CalledShotPower;
+import champ.powers.FocusedBerPower;
+import champ.powers.ResolvePower;
+import champ.relics.SignatureFinisher;
+import champ.stances.*;
+import champ.util.OnOpenerSubscriber;
+import com.badlogic.gdx.Gdx;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.*;
+import com.megacrit.cardcrawl.actions.watcher.ChangeStanceAction;
+import com.megacrit.cardcrawl.actions.watcher.PressEndTurnButtonAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.powers.VulnerablePower;
+import com.megacrit.cardcrawl.powers.WeakPower;
+import com.megacrit.cardcrawl.relics.AbstractRelic;
+import com.megacrit.cardcrawl.stances.NeutralStance;
+import slimebound.SlimeboundMod;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static champ.ChampMod.*;
+import static com.badlogic.gdx.graphics.Color.RED;
+
+
+public abstract class AbstractBronzeCard extends CustomCard {
+
+    protected final CardStrings cardStrings;
+    protected final String NAME;
+    protected String DESCRIPTION;
+    protected String UPGRADE_DESCRIPTION;
+    protected String[] EXTENDED_DESCRIPTION;
+
+    public AbstractBronzeCard(final String id, final int cost, final CardType type, final CardRarity rarity, final CardTarget target) {
+        super(id, "ERROR", getCorrectPlaceholderImage(type, id),
+                cost, "ERROR", type, ChampChar.Enums.CHAMP_GRAY, rarity, target);
+        cardStrings = CardCrawlGame.languagePack.getCardStrings(id);
+        name = NAME = cardStrings.NAME;
+        originalName = NAME;
+        rawDescription = DESCRIPTION = cardStrings.DESCRIPTION;
+        UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
+        EXTENDED_DESCRIPTION = cardStrings.EXTENDED_DESCRIPTION;
+        initializeTitle();
+        initializeDescription();
+    }
+
+    public AbstractBronzeCard(final String id, final int cost, final CardType type, final CardRarity rarity, final CardTarget target, final CardColor color) {
+        super(id, "ERROR", getCorrectPlaceholderImage(type, id),
+                cost, "ERROR", type, color, rarity, target);
+        cardStrings = CardCrawlGame.languagePack.getCardStrings(id);
+        name = NAME = cardStrings.NAME;
+        originalName = NAME;
+        rawDescription = DESCRIPTION = cardStrings.DESCRIPTION;
+        UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
+        EXTENDED_DESCRIPTION = cardStrings.EXTENDED_DESCRIPTION;
+        initializeTitle();
+        initializeDescription();
+    }
+
+    public static String getCorrectPlaceholderImage(CardType type, String id) {
+        String img = makeCardPath(id.replaceAll((getModID() + ":"), "") + ".png");
+        if ((!Gdx.files.internal(img).exists()))
+            switch (type) {
+                case ATTACK:
+                    return makeCardPath("Attack.png");
+                case SKILL:
+                    return makeCardPath("Skill.png");
+                case POWER:
+                    return makeCardPath("Power.png");
+            }
+        return img;
+    }
+
+    public static String makeID(String blah) {
+        return getModID() + ":" + blah;
+    }
+
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upp();
+        }
+    }
+
+    public abstract void upp();
+
+    protected void atb(AbstractGameAction action) {
+        addToBot(action);
+    }
+
+    protected void att(AbstractGameAction action) {
+        addToTop(action);
+    }
+
+    protected DamageInfo makeInfo() {
+        return makeInfo(damageTypeForTurn);
+    }
+
+    private DamageInfo makeInfo(DamageInfo.DamageType type) {
+        return new DamageInfo(AbstractDungeon.player, damage, type);
+    }
+
+    public void dmg(AbstractMonster m, AbstractGameAction.AttackEffect fx) {
+        atb(new DamageAction(m, makeInfo(), fx));
+    }
+
+    public void allDmg(AbstractGameAction.AttackEffect fx) {
+        atb(new DamageAllEnemiesAction(AbstractDungeon.player, multiDamage, damageTypeForTurn, fx));
+    }
+
+    public void blck() {
+        atb(new GainBlockAction(AbstractDungeon.player, AbstractDungeon.player, block));
+    }
+
+    private void makeInHand(AbstractCard c, int i) {
+        atb(new MakeTempCardInHandAction(c, i));
+    }
+
+    public void makeInHand(AbstractCard c) {
+        makeInHand(c, 1);
+    }
+
+    void shuffleIn(AbstractCard c, int i) {
+        atb(new MakeTempCardInDrawPileAction(c, i, true, true)); // This is suspect
+    }
+
+    public void shuffleIn(AbstractCard c) {
+        shuffleIn(c, 1);
+    }
+
+    public ArrayList<AbstractMonster> monsterList() {
+        ArrayList<AbstractMonster> q = new ArrayList<>();
+        for (AbstractMonster m : AbstractDungeon.getMonsters().monsters) {
+            if (!m.isDying && !m.isDead) q.add(m);
+        }
+        return q;
+    }
+
+    public void applyToEnemy(AbstractMonster m, AbstractPower po) {
+        atb(new ApplyPowerAction(m, AbstractDungeon.player, po, po.amount));
+    }
+
+    public void applyToSelf(AbstractPower po) {
+        atb(new ApplyPowerAction(AbstractDungeon.player, AbstractDungeon.player, po, po.amount));
+    }
+
+    WeakPower autoWeak(AbstractMonster m, int i) {
+        return new WeakPower(m, i, false);
+    }
+
+    VulnerablePower autoVuln(AbstractMonster m, int i) {
+        return new VulnerablePower(m, i, false);
+    }
+}
