07d2e7c364bd19a5069abbd5c6d896eb1e870f00
Vex Darkon <edanvirgo@gmail.com>
2020-12-14 16:43:52 -0700

    Peashooter

---
+++ src/main/java/automaton/cards/Peashooter.java
@@ -0,0 +1,30 @@
+package automaton.cards;
+
+import automaton.AutomatonMod;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+public class Peashooter extends AbstractBronzeCard {
+
+    public final static String ID = makeID("Peashooter");
+
+    //stupid intellij stuff attack, enemy, basic
+
+    private static final int DAMAGE = 4;
+    private static final int UPG_DAMAGE = 2;
+
+    public Peashooter() {
+        super(ID, 0, CardType.ATTACK, CardRarity.BASIC, CardTarget.ENEMY);
+        baseDamage = DAMAGE;
+        tags.add(AutomatonMod.BLASTER);
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        dmg(m, AbstractGameAction.AttackEffect.BLUNT_LIGHT);
+    }
+
+    public void upp() {
+        upgradeDamage(UPG_DAMAGE);
+    }
+}
