541c4cdf079771fec9d5aca25fe719928d4240fe
Vex Darkon <edanvirgo@gmail.com>
2020-12-14 16:49:54 -0700

    Open Fire!

---
+++ src/main/java/automaton/cards/OpenFire.java
@@ -0,0 +1,25 @@
+package automaton.cards;
+
+import automaton.MechaHelper;
+import automaton.actions.FireFromPileAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+public class OpenFire extends AbstractBronzeCard {
+
+    public final static String ID = makeID("OpenFire");
+
+    //stupid intellij stuff skill, self, basic
+
+    public OpenFire() {
+        super(ID, 1, CardType.SKILL, CardRarity.BASIC, CardTarget.SELF);
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        atb(new FireFromPileAction(MechaHelper.blasters, 1));
+    }
+
+    public void upp() {
+        upgradeBaseCost(0);
+    }
+}
