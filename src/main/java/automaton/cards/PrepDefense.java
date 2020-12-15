90bace50b3be7f0c8a902f6d9fcde34e01623515
Vex Darkon <edanvirgo@gmail.com>
2020-12-15 16:13:42 -0700

    Prep Defense missing shield tag fix

--- src/main/java/automaton/cards/PrepDefense.java
+++ src/main/java/automaton/cards/PrepDefense.java
@@ -1,5 +1,6 @@
 package automaton.cards;
 
+import automaton.AutomatonMod;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
@@ -15,6 +16,7 @@ public class PrepDefense extends AbstractBronzeCard {
         super(ID, 1, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.SELF);
         baseBlock = BLOCK;
         cardsToPreview = new ReserveGuard();
+        tags.add(AutomatonMod.SHIELD);
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
