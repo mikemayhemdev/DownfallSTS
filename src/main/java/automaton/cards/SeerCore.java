23c7426be1607d5051df79cfd942205f726d2dfe
Vex Darkon <edanvirgo@gmail.com>
2020-12-14 17:59:36 -0700

    Seer Core

---
+++ src/main/java/automaton/cards/SeerCore.java
@@ -0,0 +1,31 @@
+package automaton.cards;
+
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
+import com.megacrit.cardcrawl.actions.utility.ScryAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+public class SeerCore extends AbstractBronzeCard {
+
+    public final static String ID = makeID("SeerCore");
+
+    //stupid intellij stuff skill, self, uncommon
+
+    private static final int MAGIC = 1;
+
+    public SeerCore() {
+        super(ID, 1, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.SELF);
+        baseMagicNumber = magicNumber = MAGIC;
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        atb(new ScryAction(magicNumber));
+        atb(new GainEnergyAction(1));
+        atb(new DrawCardAction(1));
+    }
+
+    public void upp() {
+        upgradeMagicNumber(2);
+    }
+}
