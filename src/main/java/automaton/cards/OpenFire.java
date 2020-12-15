58b0f5df3f3cdff568486397d58a5c593868ee24
Vex Darkon <edanvirgo@gmail.com>
2020-12-14 18:04:46 -0700

    Buff Open Fire

--- src/main/java/automaton/cards/OpenFire.java
+++ src/main/java/automaton/cards/OpenFire.java
@@ -1,5 +1,6 @@
 package automaton.cards;
 
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
@@ -10,10 +11,12 @@ public class OpenFire extends AbstractBronzeCard {
     //stupid intellij stuff skill, self, basic
 
     public OpenFire() {
-        super(ID, 1, CardType.SKILL, CardRarity.BASIC, CardTarget.SELF);
+        super(ID, 1, CardType.ATTACK, CardRarity.BASIC, CardTarget.ENEMY);
+        baseDamage = 4;
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
+        dmg(m, AbstractGameAction.AttackEffect.FIRE);
         fireBlaster(1);
     }
 
