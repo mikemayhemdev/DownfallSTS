b792cc50a70fdb99295b6430c808298b9af8f433
Vex Darkon <edanvirgo@gmail.com>
2020-12-14 18:07:46 -0700

    Defer Process

---
+++ src/main/java/automaton/cards/DeferProcess.java
@@ -0,0 +1,29 @@
+package automaton.cards;
+
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.EnergizedPower;
+
+public class DeferProcess extends AbstractBronzeCard {
+
+    public final static String ID = makeID("DeferProcess");
+
+    //stupid intellij stuff skill, self, common
+
+    private static final int MAGIC = 1;
+    private static final int UPG_MAGIC = 1;
+
+    public DeferProcess() {
+        super(ID, 1, CardType.SKILL, CardRarity.COMMON, CardTarget.SELF);
+        baseMagicNumber = magicNumber = MAGIC;
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        applyToEnemy(m, autoWeak(m, magicNumber));
+        applyToSelf(new EnergizedPower(p, 1));
+    }
+
+    public void upp() {
+        upgradeMagicNumber(UPG_MAGIC);
+    }
+}
