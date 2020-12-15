10d43a2e4deda8909b6bf1f2add20fbdff53ce80
Vex Darkon <edanvirgo@gmail.com>
2020-12-15 12:11:47 -0700

    Mental Map

---
+++ src/main/java/automaton/cards/MentalMap.java
@@ -0,0 +1,38 @@
+package automaton.cards;
+
+import automaton.AutomatonMod;
+import com.megacrit.cardcrawl.actions.common.BetterDiscardPileToHandAction;
+import com.megacrit.cardcrawl.actions.utility.ScryAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+public class MentalMap extends AbstractBronzeCard {
+
+    public final static String ID = makeID("MentalMap");
+
+    //stupid intellij stuff skill, self, uncommon
+
+    private static final int BLOCK = 5;
+    private static final int UPG_BLOCK = 2;
+
+    private static final int MAGIC = 2;
+    private static final int UPG_MAGIC = 1;
+
+    public MentalMap() {
+        super(ID, 1, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.SELF);
+        baseBlock = BLOCK;
+        baseMagicNumber = magicNumber = MAGIC;
+        tags.add(AutomatonMod.SHIELD);
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        blck();
+        atb(new ScryAction(magicNumber));
+        atb(new BetterDiscardPileToHandAction(1));
+    }
+
+    public void upp() {
+        upgradeBlock(UPG_BLOCK);
+        upgradeMagicNumber(UPG_MAGIC);
+    }
+}
