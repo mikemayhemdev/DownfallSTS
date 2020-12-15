65a773a09f1252139f8486421285ad37565ea540
Vex Darkon <edanvirgo@gmail.com>
2020-12-15 01:06:46 -0700

    Subroutine

---
+++ src/main/java/automaton/powers/FreeFirePower.java
@@ -0,0 +1,24 @@
+package automaton.powers;
+
+import com.megacrit.cardcrawl.actions.common.ReducePowerAction;
+import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+
+import static automaton.AutomatonMod.makeID;
+
+public class FreeFirePower extends AbstractAutomatonPower {
+    public static final String NAME = "FreeFire";
+    public static final String POWER_ID = makeID(NAME);
+    public static final PowerType TYPE = PowerType.BUFF;
+    public static final boolean TURN_BASED = false;
+
+    public FreeFirePower(int amount) {
+        super(NAME, TYPE, TURN_BASED, AbstractDungeon.player, null, amount);
+    }
+
+    @Override
+    public void onSpecificTrigger() {
+        flash();
+        addToTop(new ReducePowerAction(owner, owner, this, 1));
+    }
+}
