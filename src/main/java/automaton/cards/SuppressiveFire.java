e966219f51e148379b8ae90709076ca2c27f9ad0
Vex Darkon <edanvirgo@gmail.com>
2020-12-15 11:42:33 -0700

    Suppressive Fire

---
+++ src/main/java/automaton/cards/SuppressiveFire.java
@@ -0,0 +1,31 @@
+package automaton.cards;
+
+import automaton.AutomatonMod;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+public class SuppressiveFire extends AbstractBronzeCard {
+
+    public final static String ID = makeID("SuppressiveFire");
+
+    //stupid intellij stuff skill, all_enemy, uncommon
+
+    public SuppressiveFire() {
+        super(ID, 0, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.ENEMY);
+        tags.add(AutomatonMod.BLASTER);
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        if (inFire || upgraded) {
+            for (AbstractMonster q : monsterList()) {
+                applyToEnemy(q, autoWeak(q, 1));
+                applyToEnemy(q, autoVuln(q, 1));
+            }
+        }
+    }
+
+    public void upp() {
+        rawDescription = UPGRADE_DESCRIPTION;
+        initializeDescription();
+    }
+}
