8b87f2a13d81c603c003d53d143339c7f7f0caee
Vex Darkon <edanvirgo@gmail.com>
2020-12-16 11:06:32 -0700

    I'll be back - start of CardEffectsCardMod being good

--- src/main/java/automaton/cardmods/CardEffectsCardMod.java
+++ src/main/java/automaton/cardmods/CardEffectsCardMod.java
@@ -1,5 +1,6 @@
 package automaton.cardmods;
 
+import basemod.ReflectionHacks;
 import basemod.abstracts.AbstractCardModifier;
 import com.megacrit.cardcrawl.actions.utility.UseCardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
@@ -20,12 +21,28 @@ public class CardEffectsCardMod extends BronzeCardMod {
     }
 
     @Override
+    public void onApplyPowers(AbstractCard card) {
+        stored.applyPowers();
+    }
+
+    public static String getRealDesc(AbstractCard card) {
+        //You should apply powers first!
+        return card.rawDescription.replaceAll("!D!", String.valueOf(card.damage)) .replaceAll("!B!", String.valueOf(card.block)); // add !M! after
+    }
+
+    @Override
     public String modifyDescription(String rawDescription, AbstractCard card) {
         return rawDescription + stored.rawDescription;
     }
 
     @Override
     public void onUse(AbstractCard card, AbstractCreature target, UseCardAction action) {
+        card.baseDamage = stored.baseDamage;
+        ReflectionHacks.setPrivate(card, AbstractCard.class, "isMultiDamage", ReflectionHacks.getPrivate(stored, AbstractCard.class, "isMultiDamage"));
+        card.baseBlock = stored.baseBlock;
+        card.baseMagicNumber = card.magicNumber = stored.baseMagicNumber;
+        card.applyPowers();
+        card.calculateCardDamage(target instanceof AbstractMonster ? (AbstractMonster)target : null);
         stored.use(AbstractDungeon.player, target instanceof AbstractMonster ? (AbstractMonster) target : null);
     }
 
