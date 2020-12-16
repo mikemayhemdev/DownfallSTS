8fa0b2d8469a2ab6a590c1ff9c83e953b14348da
Vex Darkon <edanvirgo@gmail.com>
2020-12-15 18:11:50 -0700

    Held Protector nerf

--- src/main/java/automaton/cards/HeldProtector.java
+++ src/main/java/automaton/cards/HeldProtector.java
@@ -15,18 +15,17 @@ public class HeldProtector extends AbstractBronzeCard {
     public HeldProtector() {
         super(ID, 1, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.SELF);
         tags.add(AutomatonMod.SHIELD);
+        tags.add(AutomatonMod.BURNOUT);
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
         if (inFire) {
             applyToSelf(new ArtifactPower(p, 1));
         }
-        if (upgraded) {
-            atb(new ReduceCostAction(this));
-        }
     }
 
     public void upp() {
+        tags.remove(AutomatonMod.BURNOUT);
         rawDescription = UPGRADE_DESCRIPTION;
         initializeDescription();
     }
