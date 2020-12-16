e43ef8378d6d843c1710b22badc401f377b88e9b
Vex Darkon <edanvirgo@gmail.com>
2020-12-16 01:03:53 -0700

    CardEffectsCardMod

---
+++ src/main/java/automaton/cardmods/CardEffectsCardMod.java
@@ -0,0 +1,39 @@
+package automaton.cardmods;
+
+import automaton.actions.RepeatCardAction;
+import basemod.abstracts.AbstractCardModifier;
+import com.megacrit.cardcrawl.actions.utility.UseCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+public class CardEffectsCardMod extends AbstractAutomatonMod {
+    private AbstractCard stored;
+
+    public CardEffectsCardMod(AbstractCard q) {
+        stored = q;
+    }
+
+    @Override
+    public String modifyDescription(String rawDescription, AbstractCard card) {
+        if (rawDescription.equals("")) {
+            return "Activate " + stored.name;
+        }
+        return " NL Activate " + stored.name;
+    }
+
+    @Override
+    public void onUse(AbstractCard card, AbstractCreature target, UseCardAction action) {
+        if (target instanceof AbstractMonster) {
+            atb(new RepeatCardAction((AbstractMonster)target, stored));
+        }
+        else {
+            atb(new RepeatCardAction(stored));
+        }
+    }
+
+    @Override
+    public AbstractCardModifier makeCopy() {
+        return new CardEffectsCardMod(stored);
+    }
+}
