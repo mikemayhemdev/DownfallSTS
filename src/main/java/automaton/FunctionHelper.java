c66e06206f3efa3db6473dfff03990c0dff138a5
Vex Darkon <edanvirgo@gmail.com>
2020-12-16 00:32:42 -0700

    Thoughts

--- src/main/java/automaton/FunctionHelper.java
+++ src/main/java/automaton/FunctionHelper.java
@@ -12,4 +12,12 @@ public class FunctionHelper {
     public AbstractCard makeFunction() {
         return new Shiv(); //TODO: Make this do the thing
     }
+
+    //If everything is a cardmod, things can be done mostly dynamically.
+    //Damages-card-mod, blocks-card-mod, etc.
+    //The main issue would be being too dynamic.
+    //"Apply 1 Vulnerable. NL Deal 10 damage." isn't any good.
+    //The alternative is using similar tech to RepeatCardAction to make the FunctionCard silent-play held1, held2, held3, held4, utilizing their Compile bonuses correctly.
+    //That's probably cleaner, and makes stuff like Vuln -> Damage clearer, if it has to happen.
+    //Hmmm.
 }
