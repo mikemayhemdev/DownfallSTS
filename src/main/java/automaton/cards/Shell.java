9a9dabc1eadafea3b53e9d4a956713cc0c85c547
Vex Darkon <edanvirgo@gmail.com>
2020-12-16 17:55:36 -0700

    Shell

---
+++ src/main/java/automaton/cards/Shell.java
@@ -0,0 +1,31 @@
+package automaton.cards;
+
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.BlurPower;
+
+public class Shell extends AbstractBronzeCard {
+
+    public final static String ID = makeID("Shell");
+
+    //stupid intellij stuff skill, self, common
+
+    private static final int BLOCK = 10;
+    private static final int UPG_BLOCK = 5;
+
+    public Shell() {
+        super(ID, 0, CardType.SKILL, CardRarity.COMMON, CardTarget.SELF);
+        baseBlock = BLOCK;
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        applyToSelf(new BlurPower(p, 1));
+        if (inFunc) {
+            blck();
+        }
+    }
+
+    public void upp() {
+        upgradeBlock(UPG_BLOCK);
+    }
+}
