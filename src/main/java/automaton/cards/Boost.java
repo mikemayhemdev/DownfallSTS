020eb019bd580fbea2bae4af7f8dabe85d512a32
Vex Darkon <edanvirgo@gmail.com>
2020-12-17 14:51:11 -0700

    Boost

--- src/main/java/automaton/cards/Boost.java
+++ src/main/java/automaton/cards/Boost.java
@@ -1,40 +1,40 @@
 package automaton.cards;
 
-import basemod.helpers.CardModifierManager;
-import com.megacrit.cardcrawl.actions.common.HealAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import sneckomod.util.ExhaustMod;
+import com.megacrit.cardcrawl.powers.StrengthPower;
 
-public class Repair extends AbstractBronzeCard {
+public class Boost extends AbstractBronzeCard {
 
-    public final static String ID = makeID("Repair");
+    public final static String ID = makeID("Boost");
 
     //stupid intellij stuff skill, self, uncommon
 
-    private static final int MAGIC = 3;
-    private static final int UPG_MAGIC = 2;
+    private static final int BLOCK = 9;
+    private static final int MAGIC = 2;
 
-    public Repair() {
-        super(ID, 1, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.SELF);
+    public Boost() {
+        super(ID, 2, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.SELF);
+        baseBlock = BLOCK;
         baseMagicNumber = magicNumber = MAGIC;
         thisEncodes();
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
-        atb(new HealAction(p, p, magicNumber));
+        blck();
     }
 
     @Override
     public void onCompile(AbstractCard function, boolean forGameplay) {
         super.onCompile(function, forGameplay);
         if (forGameplay) {
-            CardModifierManager.addModifier(function, new ExhaustMod());
+            applyToSelf(new StrengthPower(AbstractDungeon.player, magicNumber));
         }
     }
 
     public void upp() {
-        upgradeMagicNumber(UPG_MAGIC);
+        upgradeBaseCost(1);
     }
 }
