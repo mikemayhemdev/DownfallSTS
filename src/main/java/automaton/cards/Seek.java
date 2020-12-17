27abb6e79710976f71b056e64efc73f2b32f129b
Vex Darkon <edanvirgo@gmail.com>
2020-12-17 16:29:52 -0700

    Seek

---
+++ src/main/java/automaton/cards/Seek.java
@@ -0,0 +1,29 @@
+package automaton.cards;
+
+import com.megacrit.cardcrawl.actions.defect.SeekAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+public class Seek extends AbstractBronzeCard {
+
+    public final static String ID = makeID("Seek");
+
+    //stupid intellij stuff skill, self, rare
+
+    private static final int MAGIC = 1;
+    private static final int UPG_MAGIC = 1;
+
+    public Seek() {
+        super(ID, 0, CardType.SKILL, CardRarity.RARE, CardTarget.SELF);
+        baseMagicNumber = magicNumber = MAGIC;
+        exhaust = true;
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        atb(new SeekAction(magicNumber));
+    }
+
+    public void upp() {
+        upgradeMagicNumber(UPG_MAGIC);
+    }
+}
