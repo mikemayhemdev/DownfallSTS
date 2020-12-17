5bc6586a9e08d0a66b788c0d1f67384a08702362
Vex Darkon <edanvirgo@gmail.com>
2020-12-16 21:28:52 -0700

    Null Pointer

---
+++ src/main/java/automaton/cards/NullPointer.java
@@ -0,0 +1,41 @@
+package automaton.cards;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+public class NullPointer extends AbstractBronzeCard {
+
+    public final static String ID = makeID("NullPointer");
+
+    //stupid intellij stuff attack, enemy, uncommon
+
+    private static final int DAMAGE = 15;
+    private static final int UPG_DAMAGE = 5;
+
+    private static final int BLOCK = 15;
+    private static final int UPG_BLOCK = 5;
+
+    public NullPointer() {
+        super(ID, 1, CardType.ATTACK, CardRarity.UNCOMMON, CardTarget.ENEMY);
+        baseDamage = DAMAGE;
+        baseBlock = BLOCK;
+        thisEncodes();
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        dmg(m, AbstractGameAction.AttackEffect.LIGHTNING);
+        blck();
+    }
+
+    @Override
+    public boolean onOutput() {
+        //POOF!
+        return false;
+    }
+
+    public void upp() {
+        upgradeDamage(UPG_DAMAGE);
+        upgradeBlock(UPG_BLOCK);
+    }
+}
