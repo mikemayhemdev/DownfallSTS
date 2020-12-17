6cd8ffca44ed0cc0bcc9b313791a5a0879f788a1
Vex Darkon <edanvirgo@gmail.com>
2020-12-16 21:25:46 -0700

    Digital Carnage, man my mouse is breaking

---
+++ src/main/java/automaton/cards/DigitalCarnage.java
@@ -0,0 +1,39 @@
+package automaton.cards;
+
+import basemod.helpers.CardModifierManager;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import downfall.util.EtherealMod;
+
+public class DigitalCarnage extends AbstractBronzeCard {
+
+    public final static String ID = makeID("DigitalCarnage");
+
+    //stupid intellij stuff attack, enemy, uncommon
+
+    private static final int DAMAGE = 20;
+    private static final int UPG_DAMAGE = 8;
+
+    public DigitalCarnage() {
+        super(ID, 2, CardType.ATTACK, CardRarity.UNCOMMON, CardTarget.ENEMY);
+        isEthereal = true;
+        baseDamage = DAMAGE;
+        thisEncodes();
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        dmg(m, AbstractGameAction.AttackEffect.FIRE);
+    }
+
+    @Override
+    public void onCompile(AbstractCard function, boolean forGameplay) {
+        super.onCompile(function, forGameplay);
+        CardModifierManager.addModifier(function, new EtherealMod());
+    }
+
+    public void upp() {
+        upgradeDamage(UPG_DAMAGE);
+    }
+}
