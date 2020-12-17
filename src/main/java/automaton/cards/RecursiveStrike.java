3853b3b31e56bd5eaf6ed4f313a95bfffe337869
Vex Darkon <edanvirgo@gmail.com>
2020-12-16 17:40:08 -0700

    Recursive Strike

---
+++ src/main/java/automaton/cards/RecursiveStrike.java
@@ -0,0 +1,38 @@
+package automaton.cards;
+
+import automaton.FunctionHelper;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+public class RecursiveStrike extends AbstractBronzeCard {
+
+    public final static String ID = makeID("RecursiveStrike");
+
+    //stupid intellij stuff attack, enemy, common
+
+    private static final int DAMAGE = 7;
+
+    public RecursiveStrike() {
+        super(ID, 2, CardType.ATTACK, CardRarity.COMMON, CardTarget.ENEMY);
+        baseDamage = DAMAGE;
+        thisEncodes();
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        dmg(m, AbstractGameAction.AttackEffect.SLASH_DIAGONAL);
+        dmg(m, AbstractGameAction.AttackEffect.SLASH_HEAVY);
+    }
+
+    @Override
+    public void atTurnStart() {
+        if (FunctionHelper.held.contains(this)) {
+            baseDamage += 1;
+            superFlash();
+        }
+    }
+
+    public void upp() {
+        upgradeDamage(1);
+    }
+}
