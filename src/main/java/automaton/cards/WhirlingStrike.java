d5dbc3745f7035ba7828862361a034abc43265ad
Vex Darkon <edanvirgo@gmail.com>
2020-12-16 17:27:18 -0700

    Whirling Strike

---
+++ src/main/java/automaton/cards/WhirlingStrike.java
@@ -0,0 +1,31 @@
+package automaton.cards;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+public class WhirlingStrike extends AbstractBronzeCard {
+
+    public final static String ID = makeID("WhirlingStrike");
+
+    //stupid intellij stuff attack, all_enemy, common
+
+    private static final int DAMAGE = 8;
+    private static final int UPG_DAMAGE = 3;
+
+    public WhirlingStrike() {
+        super(ID, 1, CardType.ATTACK, CardRarity.COMMON, CardTarget.ENEMY);
+        baseDamage = DAMAGE;
+        thisEncodes();
+        isMultiDamage = true;
+        tags.add(CardTags.STRIKE);
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        allDmg(AbstractGameAction.AttackEffect.SLASH_VERTICAL);
+    }
+
+    public void upp() {
+        upgradeDamage(UPG_DAMAGE);
+    }
+}
