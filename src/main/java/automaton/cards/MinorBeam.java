fe3377faf97611b1812a9d21d550d87228f8aae8
Vex Darkon <edanvirgo@gmail.com>
2020-12-17 11:43:54 -0700

    Minor Beam

---
+++ src/main/java/automaton/cards/MinorBeam.java
@@ -0,0 +1,29 @@
+package automaton.cards;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+public class MinorBeam extends AbstractBronzeCard {
+
+    public final static String ID = makeID("MinorBeam");
+
+    //stupid intellij stuff attack, enemy, special
+
+    private static final int DAMAGE = 3;
+    private static final int UPG_DAMAGE = 1;
+
+    public MinorBeam() {
+        super(ID, 0, CardType.ATTACK, CardRarity.SPECIAL, CardTarget.ENEMY, CardColor.COLORLESS);
+        baseDamage = DAMAGE;
+        thisEncodes();
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        dmg(m, AbstractGameAction.AttackEffect.BLUNT_LIGHT);
+    }
+
+    public void upp() {
+        upgradeDamage(UPG_DAMAGE);
+    }
+}
