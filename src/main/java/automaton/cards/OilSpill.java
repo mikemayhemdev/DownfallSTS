de5ba75173777adbdb59979ccc9f13432516fe0c
Vex Darkon <edanvirgo@gmail.com>
2020-12-16 17:46:21 -0700

    Oil Spill

---
+++ src/main/java/automaton/cards/OilSpill.java
@@ -0,0 +1,53 @@
+package automaton.cards;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInDiscardAction;
+import com.megacrit.cardcrawl.cards.status.Slimed;
+import com.megacrit.cardcrawl.cards.status.VoidCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.PoisonPower;
+
+public class OilSpill extends AbstractBronzeCard {
+
+    public final static String ID = makeID("OilSpill");
+
+    //stupid intellij stuff attack, all_enemy, common
+
+    private static final int DAMAGE = 4;
+    private static final int UPG_DAMAGE = 1;
+
+    private static final int MAGIC = 4;
+    private static final int UPG_MAGIC = 2;
+
+    public OilSpill() {
+        super(ID, 1, CardType.ATTACK, CardRarity.COMMON, CardTarget.ENEMY);
+        baseDamage = DAMAGE;
+        baseMagicNumber = magicNumber = MAGIC;
+        isMultiDamage = true;
+        thisEncodes();
+        cardsToPreview = new Slimed();
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        allDmg(AbstractGameAction.AttackEffect.FIRE);
+        for (AbstractMonster q : monsterList()) {
+            applyToEnemy(q, new PoisonPower(q, p, magicNumber));
+        }
+        if (inFunc) {
+            if (upgraded) {
+                atb(new MakeTempCardInDiscardAction(new Slimed(), 1));
+            }
+            else {
+                shuffleIn(new Slimed());
+            }
+        }
+    }
+
+    public void upp() {
+        upgradeDamage(UPG_DAMAGE);
+        upgradeMagicNumber(UPG_MAGIC);
+        rawDescription = UPGRADE_DESCRIPTION;
+        initializeDescription();
+    }
+}
