2ffb7b07e3266fd6619a9c371dd8b2055d11e883
Vex Darkon <edanvirgo@gmail.com>
2020-12-16 17:48:04 -0700

    Wild Beam'

---
+++ src/main/java/automaton/cards/WildBeam.java
@@ -0,0 +1,43 @@
+package automaton.cards;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInDiscardAction;
+import com.megacrit.cardcrawl.cards.status.Slimed;
+import com.megacrit.cardcrawl.cards.status.Wound;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+public class WildBeam extends AbstractBronzeCard {
+
+    public final static String ID = makeID("WildBeam");
+
+    //stupid intellij stuff attack, enemy, common
+
+    private static final int DAMAGE = 12;
+    private static final int UPG_DAMAGE = 5;
+
+    public WildBeam() {
+        super(ID, 1, CardType.ATTACK, CardRarity.COMMON, CardTarget.ENEMY);
+        baseDamage = DAMAGE;
+        cardsToPreview = new Wound();
+        thisEncodes();
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        dmg(m, AbstractGameAction.AttackEffect.LIGHTNING); //TODO: Beam effect
+        if (inFunc) {
+            if (upgraded) {
+                atb(new MakeTempCardInDiscardAction(new Wound(), 1));
+            }
+            else {
+                shuffleIn(new Wound());
+            }
+        }
+    }
+
+    public void upp() {
+        upgradeDamage(UPG_DAMAGE);
+        rawDescription = UPGRADE_DESCRIPTION;
+        initializeDescription();
+    }
+}
