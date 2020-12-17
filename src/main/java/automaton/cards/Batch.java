6e46d81f6ce5cf652010e1178d3a0e152fc57bd4
Vex Darkon <edanvirgo@gmail.com>
2020-12-16 18:00:14 -0700

    Batch

---
+++ src/main/java/automaton/cards/Batch.java
@@ -0,0 +1,36 @@
+package automaton.cards;
+
+import automaton.FunctionHelper;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInHandAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+public class Batch extends AbstractBronzeCard {
+
+    public final static String ID = makeID("Batch");
+
+    //stupid intellij stuff skill, self, common
+
+    public Batch() {
+        super(ID, 0, CardType.SKILL, CardRarity.COMMON, CardTarget.SELF);
+        exhaust = true;
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        atb(new MakeTempCardInHandAction(FunctionHelper.makeFunction()));
+        atb(new AbstractGameAction() {
+            @Override
+            public void update() {
+                isDone = true;
+                FunctionHelper.held.clear();
+            }
+        });
+    }
+
+    public void upp() {
+        selfRetain = true;
+        rawDescription = UPGRADE_DESCRIPTION;
+        initializeDescription();
+    }
+}
