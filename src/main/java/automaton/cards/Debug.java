1b123a8feecfc4ed109650317aeb5c62d2dfa100
Vex Darkon <edanvirgo@gmail.com>
2020-12-17 19:59:38 -0700

    Debug

---
+++ src/main/java/automaton/cards/Debug.java
@@ -0,0 +1,36 @@
+package automaton.cards;
+
+import automaton.FunctionHelper;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static automaton.FunctionHelper.WITH_DELIMITER;
+
+public class Debug extends AbstractBronzeCard {
+
+    public final static String ID = makeID("Debug");
+
+    //stupid intellij stuff skill, self, uncommon
+
+    public Debug() {
+        super(ID, 1, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.SELF);
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        for (AbstractCard q : FunctionHelper.held.group) {
+            if (q instanceof AbstractBronzeCard) {
+                ((AbstractBronzeCard) q).doSpecialCompileStuff = false;
+                if (q.rawDescription.contains(" NL bronze:Compile")) {
+                    String[] splitText = q.rawDescription.split(String.format(WITH_DELIMITER, " NL bronze:Compile"));
+                    String compileText = splitText[1] + splitText[2];
+                    q.rawDescription = q.rawDescription.replaceAll(compileText, "");
+                } //TODO: This entire thing is terrible and placeholder. Make it good eventually!
+            }
+        }
+    }
+
+    public void upp() {
+        upgradeBaseCost(0);
+    }
+}
