0192c9b23b1f91620779b9eb00f72551babd04c4
Vex Darkon <edanvirgo@gmail.com>
2020-12-19 01:49:29 -0700

    Heart Kill Bonus renamed to Whale Hunter

--- src/main/java/downfall/patches/ReplaceHeartKillBonusPatch.java
+++ src/main/java/downfall/patches/ReplaceHeartKillBonusPatch.java
@@ -1,22 +1,20 @@
-/*
 package downfall.patches;
 
 import basemod.ReflectionHacks;
-import com.evacipated.cardcrawl.modthespire.lib.*;
-import com.evacipated.cardcrawl.modthespire.patcher.PatchingException;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
-import com.megacrit.cardcrawl.screens.DeathScreen;
+import com.megacrit.cardcrawl.localization.UIStrings;
 import com.megacrit.cardcrawl.screens.GameOverScreen;
 import com.megacrit.cardcrawl.screens.GameOverStat;
 import com.megacrit.cardcrawl.screens.VictoryScreen;
-import javassist.CannotCompileException;
-import javassist.CtBehavior;
+import downfall.downfallMod;
 
 import java.util.ArrayList;
 
 public class ReplaceHeartKillBonusPatch {
 
     public static String CHECKAGAINST = CardCrawlGame.languagePack.getScoreString("Heartbreaker").NAME;
+    public static final UIStrings uiStrings = CardCrawlGame.languagePack.getUIString(downfallMod.makeID("WhaleHunter"));
 
     @SpirePatch(
             clz = VictoryScreen.class,
@@ -35,9 +33,8 @@ public class ReplaceHeartKillBonusPatch {
                     }
                 }
                 stats.remove(idx);
-                stats.add(idx, new GameOverStat("Whale Hunter", "Killed Neow.", "100,000,000,000")); //TODO: Make this legitimate
+                stats.add(idx, new GameOverStat(uiStrings.TEXT[0], uiStrings.TEXT[1], Integer.toString(250)));
             }
         }
     }
 }
-*/
