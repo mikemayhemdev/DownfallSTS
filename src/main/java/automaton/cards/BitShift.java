2b6fc07296b4b116e204da07d907ce60d01d2cba
Vex Darkon <edanvirgo@gmail.com>
2020-12-19 11:20:45 -0700

    Fix Bit Shift

--- src/main/java/automaton/cards/BitShift.java
+++ src/main/java/automaton/cards/BitShift.java
@@ -4,6 +4,7 @@ import automaton.FunctionHelper;
 import com.evacipated.cardcrawl.mod.stslib.actions.common.SelectCardsAction;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.ReduceCostForTurnAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
@@ -17,18 +18,18 @@ public class BitShift extends AbstractBronzeCard {
         super(ID, 0, CardType.SKILL, CardRarity.COMMON, CardTarget.SELF);
         exhaust = true;
         baseMagicNumber = magicNumber = 1;
-        //TODO - Crashes after choosing a card
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
-        atb(new SelectCardsAction(FunctionHelper.held.group, 1, "Choose.", (cards) -> {
+        atb(new SelectCardsAction(FunctionHelper.held.group, 1, "Choose.", (cards) -> { //TODO: Localize
             att(new ReduceCostForTurnAction(cards.get(0), magicNumber));
+            AbstractCard q = cards.get(0);
             att(new AbstractGameAction() {
                 @Override
                 public void update() {
                     isDone = true;
-                    FunctionHelper.held.removeCard(cards.get(0));
-                    p.hand.addToTop(cards.get(0));
+                    FunctionHelper.held.removeCard(q);
+                    p.hand.addToTop(q);
                 }
             });
         }));
