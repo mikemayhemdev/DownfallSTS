5218384772235dde3645bb8f27ea3f45b713b7fa
Vex Darkon <edanvirgo@gmail.com>
2020-12-19 02:07:46 -0700

    Bad stuff, but slightly better than before

--- src/main/java/automaton/cardmods/CardEffectsCardMod.java
+++ src/main/java/automaton/cardmods/CardEffectsCardMod.java
@@ -44,19 +44,19 @@ public class CardEffectsCardMod extends BronzeCardMod {
     public static String getRealDesc(AbstractCard card) {
         String x = card.rawDescription;
         if (card.rawDescription.contains(" NL bronze:Compile")) {
-            String[] splitText = card.rawDescription.split(String.format(WITH_DELIMITER, " NL bronze:Compile"));
+            String[] splitText = x.split(String.format(WITH_DELIMITER, " NL bronze:Compile"));
             String compileText = splitText[1] + splitText[2];
-            x = card.rawDescription.replaceAll(compileText, "");
+            x = x.replaceAll(compileText, "");
         } //TODO: This entire thing is terrible and placeholder. Make it good eventually!
         if (card.rawDescription.contains(" π")) {
-            String[] splitText = card.rawDescription.split(String.format(WITH_DELIMITER, " π"));
+            String[] splitText = x.split(String.format(WITH_DELIMITER, " π"));
             String compileText = splitText[1] + splitText[2];
-            x = card.rawDescription.replaceAll(compileText, "");
+            x = x.replaceAll(compileText, "");
         } // This one is for cards with other text that doesn't need to be on the Function.
-        if (card.rawDescription.contains("NL \u00A0 ")) {
-            String[] splitText = card.rawDescription.split(String.format(WITH_DELIMITER, "NL \u00A0 "));
+        if (card.rawDescription.contains(" NL \u00A0 ")) {
+            String[] splitText = x.split(String.format(WITH_DELIMITER, " NL \u00A0 "));
             String compileText = splitText[0] + splitText[1];
-            x = card.rawDescription.replaceAll(compileText, "");
+            x = x.replaceAll(compileText, "");
         } // And for non-Function-relevant text before the main card effects.
         System.out.println(x);
         return x.replaceAll("!D!", String.valueOf(card.damage)) .replaceAll("!B!", String.valueOf(card.block)).replaceAll("!M!", String.valueOf(card.magicNumber)).replaceAll("!bauto!", (String.valueOf( ((AbstractBronzeCard)card).auto)));
