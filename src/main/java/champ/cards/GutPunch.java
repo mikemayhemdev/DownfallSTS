30b14307a1e042f63ef95f1f97ddaf1b0a273e99
Vex Darkon <edanvirgo@gmail.com>
2020-12-20 17:10:58 -0700

    Gut Punch change

--- src/main/java/champ/cards/GutPunch.java
+++ src/main/java/champ/cards/GutPunch.java
@@ -6,6 +6,8 @@ import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
+import static champ.ChampMod.fatigue;
+
 public class GutPunch extends AbstractChampCard {
 
     public final static String ID = makeID("GutPunch");
@@ -36,7 +38,7 @@ public class GutPunch extends AbstractChampCard {
       //  fatigue(2);
         if (dcombo()) {
             exhaust = true;
-            applyToSelf(new ResolvePower(magicNumber));
+            fatigue(magicNumber);
         }
     }
 
