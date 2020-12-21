23bcd1f24b72abbce4409ba8811a428930fcd6ec
Vex Darkon <edanvirgo@gmail.com>
2020-12-21 11:22:10 -0700

    Exhaust and Purgeonuse cards no longer Improvise

--- src/main/java/champ/powers/ImprovisingPower.java
+++ src/main/java/champ/powers/ImprovisingPower.java
@@ -51,31 +51,33 @@ public class ImprovisingPower extends AbstractPower implements CloneablePowerInt
 
     @Override
     public void onUseCard(AbstractCard card, UseCardAction action) {
-        boolean returnCard = false;
-        if (card.hasTag(ChampMod.COMBO) && timesUsed < amount) {
-            if (card.hasTag(ChampMod.COMBOBERSERKER)) {
-                returnCard = true;
-            }
-            if (card.hasTag(ChampMod.COMBOGLADIATOR)) {
-                returnCard = true;
-            }
-            if (card.hasTag(ChampMod.COMBODEFENSIVE)) {
-                returnCard = true;
-            }
-
-        }
-        if (returnCard) {
-            flash();
-            addToBot(new AbstractGameAction() {
-                @Override
-                public void update() {
-                    isDone = true;
-                    NoDiscardField.noDiscard.set(card, true);
-                    NoDiscardField.freeCard = true;
+        if (!card.purgeOnUse && !card.exhaust) {
+            boolean returnCard = false;
+            if (card.hasTag(ChampMod.COMBO) && timesUsed < amount) {
+                if (card.hasTag(ChampMod.COMBOBERSERKER)) {
+                    returnCard = true;
                 }
-            });
-            timesUsed++;
-            updateDescription();
+                if (card.hasTag(ChampMod.COMBOGLADIATOR)) {
+                    returnCard = true;
+                }
+                if (card.hasTag(ChampMod.COMBODEFENSIVE)) {
+                    returnCard = true;
+                }
+
+            }
+            if (returnCard) {
+                flash();
+                addToBot(new AbstractGameAction() {
+                    @Override
+                    public void update() {
+                        isDone = true;
+                        NoDiscardField.noDiscard.set(card, true);
+                        NoDiscardField.freeCard = true;
+                    }
+                });
+                timesUsed++;
+                updateDescription();
+            }
         }
     }
 
