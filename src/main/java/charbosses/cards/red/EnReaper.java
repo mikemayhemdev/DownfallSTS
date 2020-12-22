5dbbe9c126bbb4dada40c0d616b48c8ffef5c3cf
Vex Darkon <edanvirgo@gmail.com>
2020-12-22 11:39:51 -0700

    Enemy Reaper fully works

--- src/main/java/charbosses/cards/red/EnReaper.java
+++ src/main/java/charbosses/cards/red/EnReaper.java
@@ -2,18 +2,18 @@ package charbosses.cards.red;
 
 import charbosses.actions.unique.CustomReaperAction;
 import charbosses.cards.AbstractBossCard;
+import charbosses.vfx.EnemyReaperEffect;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.animations.VFXAction;
-import com.megacrit.cardcrawl.actions.common.DamageAction;
-import com.megacrit.cardcrawl.actions.unique.VampireDamageAllEnemiesAction;
+import com.megacrit.cardcrawl.actions.unique.VampireDamageAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.vfx.combat.ReaperEffect;
-import downfall.downfallMod;
 
 public class EnReaper extends AbstractBossCard {
     public static final String ID = "downfall_Charboss:Reaper";
@@ -27,15 +27,22 @@ public class EnReaper extends AbstractBossCard {
         super(ID, cardStrings.NAME, "red/attack/reaper", 2, cardStrings.DESCRIPTION, CardType.ATTACK, CardColor.RED, CardRarity.RARE, CardTarget.ALL_ENEMY);
         this.baseDamage = 4;
         this.isMultiDamage = true;
-       // this.exhaust = true;
+        // this.exhaust = true;
         this.tags.add(CardTags.HEALING);
 
     }
 
     @Override
     public void use(final AbstractPlayer p, final AbstractMonster m) {
-        this.addToBot(new VFXAction(new ReaperEffect()));
-        this.addToBot(new CustomReaperAction(m, this.multiDamage, this.damageTypeForTurn, AbstractGameAction.AttackEffect.NONE));    }
+        this.addToBot(new VFXAction(new EnemyReaperEffect()));
+        for (int x = AbstractDungeon.getCurrRoom().monsters.monsters.size() -1; x >= 0; x--) {
+            AbstractMonster q = AbstractDungeon.getCurrRoom().monsters.monsters.get(x);
+            if (!q.isDead && !q.isDying && !q.id.equals(m.id)) {
+                addToBot(new VampireDamageAction(q, new DamageInfo(m, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.NONE));
+            }
+        }
+        addToBot(new VampireDamageAction(p, new DamageInfo(m, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.NONE));
+    }
 
     @Override
     public void upgrade() {
