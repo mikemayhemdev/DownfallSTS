81e8f24819c68dfd19d11a1e33380eeee460d592
Vex Darkon <edanvirgo@gmail.com>
2020-12-22 17:59:33 -0700

    Fix Spaghetti Code spaghetti code

--- src/main/java/downfall/relics/GremlinSack.java
+++ src/main/java/downfall/relics/GremlinSack.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.math.MathUtils;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.helpers.PowerTip;
 import com.megacrit.cardcrawl.vfx.cardManip.ShowCardAndObtainEffect;
 import downfall.downfallMod;
 
@@ -31,12 +32,12 @@ public class GremlinSack extends CustomRelic {
 
         }
         if (AbstractDungeon.ascensionLevel >= 15) {
-            if (starterCardName != "") {
+            if (!starterCardName.equals("")) {
                 return DESCRIPTIONS[0] + starterCardName + DESCRIPTIONS[3];
             } else {
                 return DESCRIPTIONS[0] + DESCRIPTIONS[1] + DESCRIPTIONS[3];
             }
-        } else if (starterCardName != "") {
+        } else if (!starterCardName.equals("")) {
             return DESCRIPTIONS[0] + starterCardName + DESCRIPTIONS[2];
         } else {
             return DESCRIPTIONS[0] + DESCRIPTIONS[1] + DESCRIPTIONS[2];
@@ -47,7 +48,7 @@ public class GremlinSack extends CustomRelic {
 
     public void onEquip() {
 
-        ArrayList<AbstractCard> retVal = new ArrayList();
+        ArrayList<AbstractCard> retVal = new ArrayList<>();
 
         retVal.add(AbstractDungeon.getCard(AbstractCard.CardRarity.RARE).makeCopy());
         retVal.add(AbstractDungeon.getCard(AbstractCard.CardRarity.UNCOMMON).makeCopy());
