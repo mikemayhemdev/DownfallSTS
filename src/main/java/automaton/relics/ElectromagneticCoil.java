7684861943646d6ca25c026d23105eb982803dc7
Vex Darkon <edanvirgo@gmail.com>
2020-12-23 14:03:10 -0700

    Electromagnetic Coil

---
+++ src/main/java/automaton/relics/ElectromagneticCoil.java
@@ -0,0 +1,37 @@
+package automaton.relics;
+
+import automaton.AutomatonMod;
+import automaton.FunctionHelper;
+import automaton.util.TextureLoader;
+import basemod.abstracts.CustomRelic;
+import com.badlogic.gdx.graphics.Texture;
+
+import static champ.ChampMod.makeRelicOutlinePath;
+import static champ.ChampMod.makeRelicPath;
+
+public class ElectromagneticCoil extends CustomRelic {
+
+    public static final String ID = AutomatonMod.makeID("ElectromagneticCoil");
+    private static final Texture IMG = TextureLoader.getTexture(makeRelicPath("ElectromagneticCoil.png"));
+    private static final Texture OUTLINE = TextureLoader.getTexture(makeRelicOutlinePath("ElectromagneticCoil.png"));
+
+    public ElectromagneticCoil() {
+        super(ID, IMG, OUTLINE, RelicTier.STARTER, LandingSound.MAGICAL);
+    }
+
+    @Override
+    public void onEquip() {
+        ++FunctionHelper.max;
+    }
+
+    @Override
+    public void onUnequip() {
+        --FunctionHelper.max;
+    }
+
+    @Override
+    public String getUpdatedDescription() {
+        return DESCRIPTIONS[0];
+    }
+
+}
