241414edcea90f18718330c8874b7081ac6824b2
Arjun Krishna <a.krishna.3367@gmail.com>
2020-12-22 19:52:05 -0600

    Added Ironclad Act 1 Status Archetype
    Has not been tested yet, probably has some bugs.

---
+++ src/main/java/charbosses/relics/CBR_ChampionsBelt.java
@@ -0,0 +1,33 @@
+package charbosses.relics;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.RelicAboveCreatureAction;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.powers.WeakPower;
+import com.megacrit.cardcrawl.relics.AbstractRelic;
+import com.megacrit.cardcrawl.relics.Boot;
+import com.megacrit.cardcrawl.relics.ChampionsBelt;
+
+public class CBR_ChampionsBelt extends AbstractCharbossRelic {
+    public static final String ID = "Champion Belt";
+    public static final int EFFECT = 1;
+
+    public CBR_ChampionsBelt() {
+        super(new ChampionsBelt());
+    }
+
+    public String getUpdatedDescription() {
+        return this.DESCRIPTIONS[0] + 1 + this.DESCRIPTIONS[1];
+    }
+
+    public void onTrigger(AbstractCreature target) {
+        this.addToBot(new RelicAboveCreatureAction(AbstractDungeon.player, this));
+        this.addToBot(new ApplyPowerAction(target, AbstractDungeon.player, new WeakPower(target, 1, false), 1));
+    }
+
+    @Override
+    public AbstractRelic makeCopy() {
+        return new CBR_ChampionsBelt();
+    }
+}
