465edd287f25e9496c9021cdd512a7c9a6a68e0f
Vex Darkon <edanvirgo@gmail.com>
2020-12-24 00:32:37 -0700

    Only Status Replace in combat

--- src/main/java/automaton/patches/StatusReplacePatch.java
+++ src/main/java/automaton/patches/StatusReplacePatch.java
@@ -6,7 +6,9 @@ import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePrefixPatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpireReturn;
 import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.rooms.AbstractRoom;
 
 @SpirePatch(
         clz = AbstractCard.class,
@@ -18,13 +20,17 @@ public class StatusReplacePatch {
     public static SpireReturn<AbstractCard> Prefix(AbstractCard abstractCard) {
         // SlimeboundMod.logger.info("making stat equivalent copy");
         if (abstractCard.type == AbstractCard.CardType.STATUS) {
-            //SlimeboundMod.logger.info("IS STATUS!");
-            if (AbstractDungeon.player.hasRelic(BronzeIdol.ID)) {
-                if (!abstractCard.hasTag(AutomatonMod.GOOD_STATUS)) {
+            if (AbstractDungeon.player != null && CardCrawlGame.isInARun()) {
+                if (AbstractDungeon.getCurrRoom().phase == AbstractRoom.RoomPhase.COMBAT) {
+                    //SlimeboundMod.logger.info("IS STATUS!");
+                    if (AbstractDungeon.player.hasRelic(BronzeIdol.ID)) {
+                        if (!abstractCard.hasTag(AutomatonMod.GOOD_STATUS)) {
 
-                    AbstractCard newStatus = AutomatonMod.getGoodStatus(abstractCard);
-                    //  SlimeboundMod.logger.info("replacing with " + newStatus.name);
-                    return SpireReturn.Return(newStatus);
+                            AbstractCard newStatus = AutomatonMod.getGoodStatus(abstractCard);
+                            //  SlimeboundMod.logger.info("replacing with " + newStatus.name);
+                            return SpireReturn.Return(newStatus);
+                        }
+                    }
                 }
             }
         }
