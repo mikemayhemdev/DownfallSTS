c57eb557da1353b4bdc5efa4fbae8515e1b42291
Vex Darkon <edanvirgo@gmail.com>
2020-12-24 00:12:58 -0700

    Repair heal moved to all in the Compile

--- src/main/java/automaton/cards/Repair.java
+++ src/main/java/automaton/cards/Repair.java
@@ -4,6 +4,7 @@ import basemod.helpers.CardModifierManager;
 import com.megacrit.cardcrawl.actions.common.HealAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import sneckomod.util.ExhaustMod;
 
@@ -13,7 +14,7 @@ public class Repair extends AbstractBronzeCard {
 
     //stupid intellij stuff skill, self, uncommon
 
-    private static final int MAGIC = 3;
+    private static final int MAGIC = 6;
     private static final int UPG_MAGIC = 2;
 
     public Repair() {
@@ -21,18 +22,16 @@ public class Repair extends AbstractBronzeCard {
         baseMagicNumber = magicNumber = MAGIC;
         thisEncodes();
         tags.add(CardTags.HEALING);
-        //Repair does not get the Negative Compile mod because then you could full heal.
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
-        atb(new HealAction(p, p, magicNumber));
     }
 
     @Override
     public void onCompile(AbstractCard function, boolean forGameplay) {
         super.onCompile(function, forGameplay);
         if (forGameplay) {
-            CardModifierManager.addModifier(function, new ExhaustMod());
+            atb(new HealAction(AbstractDungeon.player, AbstractDungeon.player, magicNumber));
         }
     }
 
