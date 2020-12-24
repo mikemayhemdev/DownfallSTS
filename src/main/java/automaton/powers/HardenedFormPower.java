3943956a8fc6b899a7f8ca61034f17659769f303
mikemayhemdev <mikemayhemdev@gmail.com>
2020-12-24 00:08:27 -0500

    Power aaaaarrrt

--- src/main/java/automaton/powers/HardenedFormPower.java
+++ src/main/java/automaton/powers/HardenedFormPower.java
@@ -6,7 +6,7 @@ import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 
 public class HardenedFormPower extends AbstractAutomatonPower implements OnCompilePower {
-    public static final String NAME = "Optimize";
+    public static final String NAME = "HardenedForm";
     public static final String POWER_ID = makeID(NAME);
     public static final PowerType TYPE = PowerType.BUFF;
     public static final boolean TURN_BASED = false;
--- src/main/java/automaton/powers/ReturnPower.java
+++ src/main/java/automaton/powers/ReturnPower.java
@@ -6,7 +6,7 @@ import com.megacrit.cardcrawl.actions.common.ReducePowerAction;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 
 public class ReturnPower extends AbstractAutomatonPower {
-    public static final String NAME = "ReturnPower";
+    public static final String NAME = "Return";
     public static final String POWER_ID = makeID(NAME);
     public static final PowerType TYPE = PowerType.BUFF;
     public static final boolean TURN_BASED = false;
