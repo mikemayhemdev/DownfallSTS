297071f6f39aab1ddad3189ea44dacfb3f7e58f3
Vex Darkon <edanvirgo@gmail.com>
2020-12-25 02:16:09 -0700

    Verify

--- src/main/java/automaton/powers/VerifyPower.java
+++ src/main/java/automaton/powers/VerifyPower.java
@@ -1,32 +1,42 @@
 package automaton.powers;
 
-import com.evacipated.cardcrawl.mod.stslib.powers.interfaces.NonStackablePower;
-import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
+import automaton.FunctionHelper;
+import automaton.cardmods.CardEffectsCardMod;
+import automaton.cards.AbstractBronzeCard;
+import automaton.cards.FunctionCard;
+import basemod.abstracts.AbstractCardModifier;
+import basemod.helpers.CardModifierManager;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 
-public class VerifyPower extends AbstractAutomatonPower implements AfterOutputFunctionPower {
+public class VerifyPower extends AbstractAutomatonPower {
     public static final String NAME = "Verify";
     public static final String POWER_ID = makeID(NAME);
     public static final PowerType TYPE = PowerType.BUFF;
     public static final boolean TURN_BASED = false;
-
-    private boolean activated;
 
     public VerifyPower(int amount) {
         super(NAME, TYPE, TURN_BASED, AbstractDungeon.player, null, amount);
     }
 
     @Override
-    public void atStartOfTurn() {
-        activated = false;
-    }
-
-    @Override
-    public void receiveAfterOutputFunction() {
-        if (!activated) {
-            flash();
-            addToBot(new GainEnergyAction(amount));
-            activated = true;
+    public void onAfterCardPlayed(AbstractCard function) {
+        if (function instanceof FunctionCard) {
+            for (AbstractCardModifier m : CardModifierManager.getModifiers(function, CardEffectsCardMod.ID)) {
+                if (m instanceof CardEffectsCardMod) { // always true
+                    ((CardEffectsCardMod) m).stored().baseDamage += amount;
+                    ((CardEffectsCardMod) m).stored().damage += amount;
+                    ((CardEffectsCardMod) m).stored().baseBlock += amount;
+                    ((CardEffectsCardMod) m).stored().block += amount;
+                    ((CardEffectsCardMod) m).stored().baseMagicNumber += amount;
+                    ((CardEffectsCardMod) m).stored().magicNumber += amount;
+                    ((CardEffectsCardMod) m).stored().baseAuto += amount;
+                    ((CardEffectsCardMod) m).stored().auto += amount;
+                    ((CardEffectsCardMod) m).stored().applyPowers();
+                    ((CardEffectsCardMod) m).stored().superFlash();
+                    FunctionHelper.applyPowers();
+                }
+            }
         }
     }
 
