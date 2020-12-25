b470c544726de49f09a0e9c695660c23791ef417
Vex Darkon <edanvirgo@gmail.com>
2020-12-25 01:55:59 -0700

    Sticky Shield

--- src/main/java/automaton/cards/StickyShield.java
+++ src/main/java/automaton/cards/StickyShield.java
@@ -1,25 +1,27 @@
 package automaton.cards;
 
+import com.megacrit.cardcrawl.cards.status.Slimed;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
-public class Shield extends AbstractBronzeCard {
+public class StickyShield extends AbstractBronzeCard {
 
-    public final static String ID = makeID("Shield");
+    public final static String ID = makeID("StickyShield");
 
     //stupid intellij stuff skill, self, common
 
-    private static final int BLOCK = 7;
-    private static final int UPG_BLOCK = 2;
+    private static final int BLOCK = 11;
+    private static final int UPG_BLOCK = 3;
 
-    public Shield() {
+    public StickyShield() {
         super(ID, 1, CardType.SKILL, CardRarity.COMMON, CardTarget.SELF);
         baseBlock = BLOCK;
-        thisEncodes();
+        cardsToPreview = new Slimed();
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
         blck();
+        shuffleIn(new Slimed());
     }
 
     public void upp() {
