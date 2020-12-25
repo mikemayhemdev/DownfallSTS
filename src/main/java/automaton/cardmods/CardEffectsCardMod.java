dff2a67668f62123a550d232bd76f895939e2f53
Vex Darkon <edanvirgo@gmail.com>
2020-12-24 19:38:34 -0700

    Fix first round of Bugs + Function text improved

--- src/main/java/automaton/cardmods/CardEffectsCardMod.java
+++ src/main/java/automaton/cardmods/CardEffectsCardMod.java
@@ -59,6 +59,55 @@ public class CardEffectsCardMod extends BronzeCardMod {
         return damage;
     }
 
+    public static String getDynamicValue(AbstractCard card, char key) {
+        switch (key) {
+            case 'B':
+                if (card.isBlockModified) {
+                    if (card.block >= card.baseBlock) {
+                        return "[#7fff00]" + Integer.toString(card.block) + "[]";
+                    }
+
+                    return "[#ff6563]" + Integer.toString(card.block) + "[]";
+                }
+
+                return Integer.toString(card.baseBlock);
+            case 'D':
+                if (card.isDamageModified) {
+                    if (card.damage >= card.baseDamage) {
+                        return "[#7fff00]" + Integer.toString(card.damage) + "[]";
+                    }
+
+                    return "[#ff6563]" + Integer.toString(card.damage) + "[]";
+                }
+
+                return Integer.toString(card.baseDamage);
+            case 'M':
+                if (card.isMagicNumberModified) {
+                    if (card.magicNumber >= card.baseMagicNumber) {
+                        return "[#7fff00]" + Integer.toString(card.magicNumber) + "[]";
+                    }
+
+                    return "[#ff6563]" + Integer.toString(card.magicNumber) + "[]";
+                }
+
+                return Integer.toString(card.baseMagicNumber);
+            case 'A':
+                if (card instanceof AbstractBronzeCard) {
+                    if (((AbstractBronzeCard) card).isAutoModified) {
+                        if (((AbstractBronzeCard) card).auto >= ((AbstractBronzeCard) card).baseAuto) {
+                            return "[#7fff00]" + Integer.toString(((AbstractBronzeCard) card).auto) + "[]";
+                        }
+
+                        return "[#ff6563]" + Integer.toString(((AbstractBronzeCard) card).auto) + "[]";
+                    }
+
+                    return Integer.toString(((AbstractBronzeCard) card).baseAuto);
+                }
+            default:
+                return Integer.toString(-99);
+        }
+    }
+
     public static String getRealDesc(AbstractCard card) {
         String x = card.rawDescription;
         if (card.rawDescription.contains(" NL bronze:Compile")) {
@@ -79,7 +128,19 @@ public class CardEffectsCardMod extends BronzeCardMod {
             String compileText = splitText[0] + splitText[1];
             x = x.replace(compileText, "");
         } // And for non-Function-relevant text before the main card effects.
-        return x.replaceAll("!D!", String.valueOf(card.damage)) .replaceAll("!B!", String.valueOf(card.block)).replaceAll("!M!", String.valueOf(card.magicNumber)).replaceAll("!bauto!", (String.valueOf( ((AbstractBronzeCard)card).auto)));
+        if (x.contains("!D!")) {
+            x = x.replaceAll("!D!", getDynamicValue(card, 'D'));
+        }
+        if (x.contains("!B!")) {
+            x = x.replaceAll("!B!", getDynamicValue(card, 'B'));
+        }
+        if (x.contains("!M!")) {
+            x = x.replaceAll("!B!", getDynamicValue(card, 'M'));
+        }
+        if (x.contains("!bauto!")) {
+            x = x.replaceAll("!bauto!", getDynamicValue(card, 'A'));
+        }
+        return x.replaceAll("!D!", String.valueOf(card.damage)).replaceAll("!B!", String.valueOf(card.block)).replaceAll("!M!", String.valueOf(card.magicNumber)).replaceAll("!bauto!", (String.valueOf(((AbstractBronzeCard) card).auto)));
     }
 
     @Override
@@ -101,7 +162,7 @@ public class CardEffectsCardMod extends BronzeCardMod {
             ((AbstractBronzeCard) card).baseAuto = ((AbstractBronzeCard) card).auto = ((AbstractBronzeCard) stored()).baseAuto;
         }
         card.applyPowers();
-        card.calculateCardDamage(target instanceof AbstractMonster ? (AbstractMonster)target : null);
+        card.calculateCardDamage(target instanceof AbstractMonster ? (AbstractMonster) target : null);
         stored().use(AbstractDungeon.player, target instanceof AbstractMonster ? (AbstractMonster) target : null);
     }
 
