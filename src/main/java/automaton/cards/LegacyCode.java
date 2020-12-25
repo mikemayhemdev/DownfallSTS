13270b7d69dad4bbd88d141cc714e1a7951a1e0f
Vex Darkon <edanvirgo@gmail.com>
2020-12-25 02:07:38 -0700

    Seek -> Legacy Code

--- src/main/java/automaton/cards/LegacyCode.java
+++ src/main/java/automaton/cards/LegacyCode.java
@@ -1,29 +1,28 @@
 package automaton.cards;
 
-import com.megacrit.cardcrawl.actions.defect.SeekAction;
+import com.megacrit.cardcrawl.actions.common.BetterDiscardPileToHandAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
-public class Seek extends AbstractBronzeCard {
+public class LegacyCode extends AbstractBronzeCard {
 
-    public final static String ID = makeID("Seek");
+    public final static String ID = makeID("LegacyCode");
 
     //stupid intellij stuff skill, self, rare
 
     private static final int MAGIC = 1;
 
-    public Seek() {
-        super(ID, 0, CardType.SKILL, CardRarity.RARE, CardTarget.SELF);
+    public LegacyCode() {
+        super(ID, 1, CardType.SKILL, CardRarity.RARE, CardTarget.SELF);
         baseMagicNumber = magicNumber = MAGIC;
-        exhaust = true;
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
-        atb(new SeekAction(magicNumber));
+        atb(new BetterDiscardPileToHandAction(magicNumber));
     }
 
     public void upp() {
-        selfRetain = true;
+        upgradeMagicNumber(1);
         rawDescription = UPGRADE_DESCRIPTION;
         initializeDescription();
     }
