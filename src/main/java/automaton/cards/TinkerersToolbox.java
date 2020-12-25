60816bb1d6797377c8acf459849af721bfe232aa
mikemayhemdev <mikemayhemdev@gmail.com>
2020-12-25 12:46:22 -0500

    Fixes, infinite loop text brought back

--- src/main/java/automaton/cards/TinkerersToolbox.java
+++ src/main/java/automaton/cards/TinkerersToolbox.java
@@ -72,13 +72,13 @@ public class TinkerersToolbox extends AbstractBronzeCard {
         float drawScale = 0.7f;
         float yPosition1 = this.current_y + this.hb.height * 1.3f;
         float yPosition2 = this.current_y + this.hb.height * .6F;
-        float yPosition3 = this.current_y + this.hb.height * -1F;
+        float yPosition3 = this.current_y + this.hb.height * -.1F;
 
         //changes the Arcana preview to render below the Arcana in the shop so it doesn't clip out of the screen
         if (AbstractDungeon.screen == AbstractDungeon.CurrentScreen.SHOP) {
             yPosition1 = this.current_y - this.hb.height * 1.3f;
             yPosition2 = this.current_y - this.hb.height * .6F;
-            yPosition3 = this.current_y + this.hb.height * -1F;
+            yPosition3 = this.current_y + this.hb.height * -.1F;
         }
 
         float xPosition1;
