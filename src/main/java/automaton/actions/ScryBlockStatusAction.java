d6c22efca9a9bfa883e2a03f37a22b8db363736b
Vex Darkon <edanvirgo@gmail.com>
2020-12-26 18:21:44 -0700

    Clean up ScryBlockStatusAction

--- src/main/java/automaton/actions/ScryBlockStatusAction.java
+++ src/main/java/automaton/actions/ScryBlockStatusAction.java
@@ -6,7 +6,6 @@
 package automaton.actions;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.actions.AbstractGameAction.ActionType;
 import com.megacrit.cardcrawl.actions.common.GainBlockAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.CardGroup;
@@ -16,7 +15,6 @@ import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.UIStrings;
 import com.megacrit.cardcrawl.powers.AbstractPower;
-import java.util.Iterator;
 
 public class ScryBlockStatusAction extends AbstractGameAction {
     private static final UIStrings uiStrings;
@@ -41,13 +39,8 @@ public class ScryBlockStatusAction extends AbstractGameAction {
         if (AbstractDungeon.getMonsters().areMonstersBasicallyDead()) {
             this.isDone = true;
         } else {
-            Iterator var1;
-            AbstractCard c;
             if (this.duration == this.startingDuration) {
-                var1 = AbstractDungeon.player.powers.iterator();
-
-                while(var1.hasNext()) {
-                    AbstractPower p = (AbstractPower)var1.next();
+                for (AbstractPower p : AbstractDungeon.player.powers) {
                     p.onScry();
                 }
 
@@ -58,26 +51,20 @@ public class ScryBlockStatusAction extends AbstractGameAction {
 
                 CardGroup tmpGroup = new CardGroup(CardGroupType.UNSPECIFIED);
                 if (this.amount != -1) {
-                    for(int i = 0; i < Math.min(this.amount, AbstractDungeon.player.drawPile.size()); ++i) {
-                        tmpGroup.addToTop((AbstractCard)AbstractDungeon.player.drawPile.group.get(AbstractDungeon.player.drawPile.size() - i - 1));
+                    for (int i = 0; i < Math.min(this.amount, AbstractDungeon.player.drawPile.size()); ++i) {
+                        tmpGroup.addToTop((AbstractCard) AbstractDungeon.player.drawPile.group.get(AbstractDungeon.player.drawPile.size() - i - 1));
                     }
                 } else {
-                    Iterator var5 = AbstractDungeon.player.drawPile.group.iterator();
-
-                    while(var5.hasNext()) {
-                        c = (AbstractCard)var5.next();
+                    for (AbstractCard c : AbstractDungeon.player.drawPile.group) {
                         tmpGroup.addToBottom(c);
                     }
                 }
 
                 AbstractDungeon.gridSelectScreen.open(tmpGroup, this.amount, true, TEXT[0]);
             } else if (!AbstractDungeon.gridSelectScreen.selectedCards.isEmpty()) {
-                var1 = AbstractDungeon.gridSelectScreen.selectedCards.iterator();
-
-                while(var1.hasNext()) {
-                    c = (AbstractCard)var1.next();
+                for (AbstractCard c : AbstractDungeon.gridSelectScreen.selectedCards) {
                     AbstractDungeon.player.drawPile.moveToDiscardPile(c);
-                    if (c.type == AbstractCard.CardType.STATUS){
+                    if (c.type == AbstractCard.CardType.STATUS) {
                         addToBot(new GainBlockAction(AbstractDungeon.player, blockPerCard));
                     }
                 }
@@ -85,10 +72,7 @@ public class ScryBlockStatusAction extends AbstractGameAction {
                 AbstractDungeon.gridSelectScreen.selectedCards.clear();
             }
 
-            var1 = AbstractDungeon.player.discardPile.group.iterator();
-
-            while(var1.hasNext()) {
-                c = (AbstractCard)var1.next();
+            for (AbstractCard c : AbstractDungeon.player.discardPile.group) {
                 c.triggerOnScry();
             }
 
