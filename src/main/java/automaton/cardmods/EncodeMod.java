54a268ed35ac1c8503b0273d20dc46e4e0c904e3
Vex Darkon <edanvirgo@gmail.com>
2020-12-28 01:37:10 -0700

    Encode before Compile.

--- src/main/java/automaton/cardmods/EncodeMod.java
+++ src/main/java/automaton/cardmods/EncodeMod.java
@@ -4,6 +4,8 @@ import automaton.AutomatonMod;
 import basemod.abstracts.AbstractCardModifier;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 
+import static automaton.FunctionHelper.WITH_DELIMITER;
+
 public class EncodeMod extends BronzeCardMod {
 
     public static String ID = "bronze:EncodeModifier";
@@ -11,13 +13,20 @@ public class EncodeMod extends BronzeCardMod {
 
     @Override
     public String modifyDescription(String rawDescription, AbstractCard card) {
+        if (card.rawDescription.contains(" NL bronze:Compile")) {//TODO:Localize
+            String[] splitText = card.rawDescription.split(String.format(WITH_DELIMITER, " NL bronze:Compile"));//TODO:Localize
+            String compileText = splitText[1] + splitText[2];
+            return splitText[0] + " NL bronze:Encode." + compileText; //TODO: Localize
+        } else if (card.rawDescription.contains("bronze:Compile")) { //TODO: Localize
+            return "bronze:Encode. NL " + card.rawDescription; // TODO: Localize
+        }
         return rawDescription + " NL bronze:Encode."; //TODO: Localize
     }
 
     @Override
     public void onInitialApplication(AbstractCard card) {
         super.onInitialApplication(card);
-        if (!card.hasTag(AutomatonMod.ENCODES)){
+        if (!card.hasTag(AutomatonMod.ENCODES)) {
             card.tags.add(AutomatonMod.ENCODES);
         }
     }
