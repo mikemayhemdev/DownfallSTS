a9cd5c255561803ed92afb76b463caa564344990
mikemayhemdev <mikemayhemdev@gmail.com>
2020-12-27 22:20:28 -0500

    Act 1 Defect genetic algorithm swapped for Aged

---
+++ src/main/java/charbosses/cards/curses/EnAged.java
@@ -0,0 +1,54 @@
+package charbosses.cards.curses;
+
+import champ.util.TextureLoader;
+import charbosses.actions.common.EnemyMakeTempCardInHandAction;
+import charbosses.cards.AbstractBossCard;
+import charbosses.cards.status.EnSlimed;
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInDiscardAction;
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInDrawPileAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.status.Slimed;
+import com.megacrit.cardcrawl.cards.status.VoidCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import guardian.GuardianMod;
+import slimebound.SlimeboundMod;
+
+public class EnAged extends AbstractBossCard {
+    public static final String ID = "downfall_Charboss:Aged";
+    private static final CardStrings cardStrings;
+    public static final String IMG_PATH = "cards/aged.png";
+
+    static {
+        cardStrings = CardCrawlGame.languagePack.getCardStrings("Guardian:Aged");
+    }
+
+    public EnAged() {
+        super(ID, cardStrings.NAME, GuardianMod.getResourcePath(IMG_PATH), -2, cardStrings.DESCRIPTION, CardType.CURSE, CardColor.CURSE, CardRarity.CURSE, CardTarget.NONE, AbstractMonster.Intent.MAGIC);
+        this.isEthereal = true;
+        portrait = TextureLoader.getTextureAsAtlasRegion(SlimeboundMod.getResourcePath(IMG_PATH));
+        portraitImg = TextureLoader.getTexture(SlimeboundMod.getResourcePath(IMG_PATH));
+
+    }
+
+    @Override
+    public void use(final AbstractPlayer p, final AbstractMonster m) {
+    }
+
+    @Override
+    public void triggerWhenDrawn() {
+        addToBot(new MakeTempCardInDrawPileAction(new VoidCard(), 1, false, true));
+
+    }
+
+    @Override
+    public void upgrade() {
+    }
+
+    @Override
+    public AbstractCard makeCopy() {
+        return new EnAged();
+    }
+}
