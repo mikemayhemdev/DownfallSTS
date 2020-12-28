2d8e57abdb24b2a333f4737382f84738818ae3ee
mikemayhemdev <mikemayhemdev@gmail.com>
2020-12-28 02:50:19 -0500

    Safeguard fix 2

--- src/main/java/automaton/cards/Safeguard.java
+++ src/main/java/automaton/cards/Safeguard.java
@@ -15,7 +15,7 @@ public class Safeguard extends AbstractBronzeCard {
     //stupid intellij stuff skill, self, uncommon
 
     private static final int BLOCK = 13;
-    private static final int MAGIC = 1;
+    private static final int MAGIC = 2;
     private static final int UPG_MAGIC = -1;
 
     public Safeguard() {
