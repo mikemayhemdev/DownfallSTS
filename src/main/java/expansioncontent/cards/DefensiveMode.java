d89a011ec8c919b7a1a5c5370b2d883db946724f
mikemayhemdev <mikemayhemdev@gmail.com>
2020-12-31 08:00:50 -0500

    defensive mode fixes

--- src/main/java/expansioncontent/cards/DefensiveMode.java
+++ src/main/java/expansioncontent/cards/DefensiveMode.java
@@ -15,8 +15,8 @@ import expansioncontent.expansionContentMod;
 public class DefensiveMode extends AbstractExpansionCard {
     public final static String ID = makeID("DefensiveMode");
 
-    private static final int BLOCK = 12;
-    private static final int UPGRADE_BLOCK = 8;
+    private static final int BLOCK = 5;
+    private static final int UPGRADE_BLOCK = 5;
     private static final int MAGIC = 3;
     private static final int UPGRADE_MAGIC = 1;
 
@@ -32,7 +32,7 @@ public class DefensiveMode extends AbstractExpansionCard {
 
     public void use(AbstractPlayer p, AbstractMonster m) {
 
-        atb(new GainBlockAction(p, p, this.block));
+       if (upgraded) atb(new GainBlockAction(p, p, this.block));
         atb(new ChangeStanceAction(guardian.stances.DefensiveMode.STANCE_ID));
 //        atb(new ApplyPowerAction(p, p, new ThornsPower(p, this.magicNumber), this.magicNumber));
 //        atb(new ApplyPowerAction(p, p, new NextTurnBlockPower(p, this.block), this.block));
@@ -43,8 +43,8 @@ public class DefensiveMode extends AbstractExpansionCard {
     public void upgrade() {
         if (!upgraded) {
             upgradeName();
-            upgradeBlock(UPGRADE_BLOCK);
-            upgradeMagicNumber(UPGRADE_MAGIC);
+            rawDescription = UPGRADE_DESCRIPTION;
+            initializeDescription();
         }
     }
 
