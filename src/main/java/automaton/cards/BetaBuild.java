31deda78ae8f0d8a01d5a3bdd24d80c9eb2bf9df
mikemayhemdev <mikemayhemdev@gmail.com>
2021-01-01 06:47:42 -0500

    Replacement for Copy Paste mostly done, minus the power part.  Vex Magic awaits.  Updated the function frame to have brackets.

---
+++ src/main/java/automaton/cards/BetaBuild.java
@@ -0,0 +1,47 @@
+package automaton.cards;
+
+import automaton.AutomatonMod;
+import basemod.AutoAdd;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.GainStrengthPower;
+import com.megacrit.cardcrawl.powers.StrengthPower;
+import sneckomod.SneckoMod;
+
+import static sneckomod.SneckoMod.getRandomStatus;
+
+public class BetaBuild extends AbstractBronzeCard {
+
+    public final static String ID = makeID("BetaBuild");
+
+    //stupid intellij stuff attack, enemy, rare
+    private static final int BLOCK = 15;
+
+    public BetaBuild() {
+        super(ID, 2, CardType.SKILL, CardRarity.SPECIAL, CardTarget.SELF);
+        baseBlock = BLOCK;
+        baseMagicNumber = magicNumber = 1;
+        thisEncodes();
+        cardsToPreview = new FullRelease();
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        blck();
+        shuffleIn(SneckoMod.getRandomStatus());
+    }
+
+    @Override
+    public void onCompile(AbstractCard function, boolean forGameplay) {
+        if (forGameplay) {
+            AbstractCard q = new FullRelease();
+            shuffleIn(q, 1);
+        }
+    }
+
+    public void upp() {
+        upgradeBlock(5);
+    }
+}
