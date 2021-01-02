7f7afacef32da674137810b4a53eab7dfc26366f
mikemayhemdev <mikemayhemdev@gmail.com>
2021-01-02 12:18:20 -0500

    Wounds for the IC1 loop

--- src/main/java/charbosses/bosses/Ironclad/NewAge/ArchetypeAct1StatusesNewAge.java
+++ src/main/java/charbosses/bosses/Ironclad/NewAge/ArchetypeAct1StatusesNewAge.java
@@ -98,7 +98,7 @@ public class ArchetypeAct1StatusesNewAge extends ArchetypeBaseIronclad {
         } else {
             switch (turn) {
                 case 0:
-                    addToList(cardsList, new EnPowerThrough());
+                    addToList(cardsList, new EnPowerThrough(true));
                     addToList(cardsList, new EnTrueGrit(), true);
                     //IC will exhaust the Doubt the first time through the loop instead of a wound
                     //So the wound will appear the second time through the loop onward
