a563c778130c87b6b7a2f809512049a7e76ab7d3
mikemayhemdev <mikemayhemdev@gmail.com>
2021-01-02 15:33:47 -0500

    fr 2 c

--- src/main/java/automaton/cards/FullRelease.java
+++ src/main/java/automaton/cards/FullRelease.java
@@ -17,7 +17,7 @@ public class FullRelease extends AbstractBronzeCard {
 
 
     public FullRelease() {
-        super(ID, 3, CardType.SKILL, CardRarity.SPECIAL, CardTarget.SELF);
+        super(ID, 2, CardType.SKILL, CardRarity.SPECIAL, CardTarget.SELF);
         thisEncodes();
     }
 
