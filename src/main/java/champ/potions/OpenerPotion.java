8bd6c53288bae902a27d578a4edace1a5037de72
Vex Darkon <edanvirgo@gmail.com>
2021-01-03 11:09:52 -0700

    Opener Potion change

--- src/main/java/champ/potions/OpenerPotion.java
+++ src/main/java/champ/potions/OpenerPotion.java
@@ -3,12 +3,17 @@ package champ.potions;
 
 import basemod.abstracts.CustomPotion;
 import champ.ChampMod;
-import champ.actions.DiscoverOpenerAction;
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInHandAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.helpers.CardLibrary;
 import com.megacrit.cardcrawl.helpers.PowerTip;
 import com.megacrit.cardcrawl.localization.PotionStrings;
+import downfall.util.SelectCardsCenteredAction;
+
+import java.util.ArrayList;
 
 
 public class OpenerPotion extends CustomPotion {
@@ -28,7 +33,7 @@ public class OpenerPotion extends CustomPotion {
     public void initializeData() {
         this.potency = getPotency();
         if (AbstractDungeon.player != null && potency > 1) {
-            if (potency > 2){
+            if (potency > 2) {
                 this.description = potionStrings.DESCRIPTIONS[0] + potionStrings.DESCRIPTIONS[1] + potency + potionStrings.DESCRIPTIONS[3];
             } else {
                 this.description = potionStrings.DESCRIPTIONS[0] + potionStrings.DESCRIPTIONS[1] + potency + potionStrings.DESCRIPTIONS[2];
@@ -41,7 +46,25 @@ public class OpenerPotion extends CustomPotion {
     }
 
     public void use(AbstractCreature target) {
-        addToBot(new DiscoverOpenerAction(potency));
+        ArrayList<AbstractCard> cardsList = generateColorlessCardChoices();
+        addToBot(new SelectCardsCenteredAction(cardsList, "Choose.", (cards) -> {
+            for (int i = 0; i < potency; i++) {
+                AbstractDungeon.actionManager.addToTop(new MakeTempCardInHandAction(cards.get(0).makeStatEquivalentCopy()));
+            }
+        }));
+    }
+
+    private ArrayList<AbstractCard> generateColorlessCardChoices() {
+        ArrayList<AbstractCard> choiceList = new ArrayList<>();
+        ArrayList<AbstractCard> bersOpenersList = new ArrayList<>();
+        ArrayList<AbstractCard> defOpenersList = new ArrayList<>();
+        for (AbstractCard q : CardLibrary.getAllCards()) {
+            if (q.hasTag(ChampMod.OPENERBERSERKER)) bersOpenersList.add(q.makeCopy());
+            if (q.hasTag(ChampMod.OPENERDEFENSIVE)) defOpenersList.add(q.makeCopy());
+        }
+        choiceList.add(bersOpenersList.get(AbstractDungeon.cardRandomRng.random(bersOpenersList.size() - 1)));
+        choiceList.add(defOpenersList.get(AbstractDungeon.cardRandomRng.random(defOpenersList.size() - 1)));
+        return choiceList;
     }
 
     public CustomPotion makeCopy() {
