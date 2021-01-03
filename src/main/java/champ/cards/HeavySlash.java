85df7a39ace1d7903218bf135a5c8ceae29de06f
Vex Darkon <edanvirgo@gmail.com>
2021-01-03 16:23:43 -0700

    JSOn fix

--- src/main/java/champ/cards/HeavySlash.java
+++ src/main/java/champ/cards/HeavySlash.java
@@ -1,6 +1,5 @@
 package champ.cards;
 
-import champ.ChampMod;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
