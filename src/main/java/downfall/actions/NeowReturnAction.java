f1fc51b912c82b4735a9112afaa1aee6136e6a6c
mikemayhemdev <mikemayhemdev@gmail.com>
2021-01-05 11:52:54 -0500

    grumble grumble

--- src/main/java/downfall/actions/NeowReturnAction.java
+++ src/main/java/downfall/actions/NeowReturnAction.java
@@ -18,16 +18,22 @@ public class NeowReturnAction extends AbstractGameAction {
 
     @Override
     public void update() {
+        NeowBoss.neowboss.escape();
+        NeowBoss.neowboss = null;
+        /*
+        AbstractDungeon.bossList.clear();
+        AbstractDungeon.bossList.add(NeowBossFinal.ID);
+        AbstractDungeon.bossList.add(NeowBossFinal.ID);
+        AbstractDungeon.bossList.add(NeowBossFinal.ID);
         AbstractDungeon.bossKey = NeowBossFinal.ID;
-        CardCrawlGame.music.fadeOutBGM();
-        CardCrawlGame.music.fadeOutTempBGM();
-        MapRoomNode node = new MapRoomNode(-1, 5);
+        MapRoomNode node = new MapRoomNode(-1, 10);
         node.room = new MonsterRoomBoss();
         AbstractDungeon.nextRoom = node;
         AbstractDungeon.closeCurrentScreen();
         AbstractDungeon.nextRoomTransitionStart();
         AbstractDungeon.fadeIn();
-        AbstractDungeon.getCurrRoom().cannotLose = false;
+        //AbstractDungeon.getCurrRoom().cannotLose = false;
+        */
         this.isDone = true;
     }
 }
