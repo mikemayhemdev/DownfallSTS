150c61edf0c786103975d959aefe9d49d50eb1d4
Vex Darkon <edanvirgo@gmail.com>
2021-01-05 18:39:32 -0700

    Fix Unknown with Bottle

--- src/main/java/sneckomod/patches/UnknownNoBottlePatch.java
+++ src/main/java/sneckomod/patches/UnknownNoBottlePatch.java
@@ -1,18 +1,13 @@
-/*
 package sneckomod.patches;
 
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.megacrit.cardcrawl.cards.CardGroup;
 import sneckomod.cards.unknowns.AbstractUnknownCard;
 
-@SpirePatch(
-        clz = CardGroup.class,
-        method = "getGroupWithoutBottledCards"
-)
+@SpirePatch(clz = CardGroup.class, method = "getGroupWithoutBottledCards")
 public class UnknownNoBottlePatch {
-    public static CardGroup Postfix(CardGroup __param, CardGroup __result) {
+    public static CardGroup Postfix(CardGroup __result, CardGroup group) {
         __result.group.removeIf(c -> c instanceof AbstractUnknownCard);
         return __result;
     }
 }
-*/
