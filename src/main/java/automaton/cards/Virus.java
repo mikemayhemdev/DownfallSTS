2e22ad44aa65e1c57750d1ec6c0aae9a12da30b7
mikemayhemdev <mikemayhemdev@gmail.com>
2021-01-06 07:04:55 -0500

    Remove Exhaust from Virus

--- src/main/java/automaton/cards/Virus.java
+++ src/main/java/automaton/cards/Virus.java
@@ -24,7 +24,6 @@ public class Virus extends AbstractBronzeCard {
         super(ID, 0, CardType.ATTACK, CardRarity.UNCOMMON, CardTarget.ENEMY);
         baseDamage = DAMAGE;
         cardsToPreview = new MinorBeam();
-        exhaust = true;
         selfRetain = true;
     }
 
