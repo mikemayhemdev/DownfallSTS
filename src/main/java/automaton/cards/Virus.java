21a757150d1cbf66b936a95af1f0fdbaa98bb62e
mikemayhemdev <mikemayhemdev@gmail.com>
2021-01-06 06:54:28 -0500

    Virus update

--- src/main/java/automaton/cards/Virus.java
+++ src/main/java/automaton/cards/Virus.java
@@ -1,6 +1,8 @@
 package automaton.cards;
 
+import com.megacrit.cardcrawl.actions.common.ExhaustSpecificCardAction;
 import com.megacrit.cardcrawl.actions.common.MakeTempCardInDrawPileAction;
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInHandAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
@@ -8,6 +10,7 @@ import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.FrailPower;
 
 import static com.megacrit.cardcrawl.actions.AbstractGameAction.AttackEffect.FIRE;
+import static com.megacrit.cardcrawl.actions.AbstractGameAction.AttackEffect.LIGHTNING;
 
 public class Virus extends AbstractBronzeCard {
 
@@ -15,37 +18,30 @@ public class Virus extends AbstractBronzeCard {
 
     //stupid intellij stuff attack, enemy, uncommon
 
-    private static final int DAMAGE = 5;
+    private static final int DAMAGE = 3;
 
     public Virus() {
         super(ID, 0, CardType.ATTACK, CardRarity.UNCOMMON, CardTarget.ENEMY);
         baseDamage = DAMAGE;
         thisEncodes();
-        baseMagicNumber = magicNumber = 2;
-    }
-
-
-    @Override
-    public void onCompile(AbstractCard function, boolean forGameplay) {
-        if (forGameplay) {
-            shuffleIn(makeStatEquivalentCopy(), magicNumber);
-        }
-    }
-
-    @Override
-    public String getSpecialCompileText() {
-        return EXTENDED_DESCRIPTION[0];
-    }
-
-    @Override
-    public void upgrade() {
-        upgradeDamage(3);
+        cardsToPreview = new MinorBeam();
+        exhaust = true;
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
-        dmg(m, FIRE);
+        dmg(m, LIGHTNING);
+        for (AbstractCard c:AbstractDungeon.player.hand.group){
+            atb(new ExhaustSpecificCardAction(c,AbstractDungeon.player.hand));
+            AbstractCard c2 = new MinorBeam();
+            if (upgraded)c2.upgrade();
+            atb(new MakeTempCardInHandAction(c2));
+        }
     }
 
     public void upp() {
+        cardsToPreview.upgrade();
+        upgradeDamage(1);
+        rawDescription = UPGRADE_DESCRIPTION;
+        initializeDescription();
     }
 }
