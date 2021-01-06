48af9b4eea6b6964035152f0e13e3fd9cb521b26
Vex Darkon <edanvirgo@gmail.com>
2021-01-05 17:19:04 -0700

    Memorize+

--- src/main/java/sneckomod/cards/Memorize.java
+++ src/main/java/sneckomod/cards/Memorize.java
@@ -1,15 +1,12 @@
 package sneckomod.cards;
 
 import com.evacipated.cardcrawl.mod.stslib.fields.cards.AbstractCard.FleetingField;
-import com.evacipated.cardcrawl.modthespire.lib.SpireField;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import sneckomod.SneckoMod;
 import sneckomod.actions.MemorizeAction;
-import sneckomod.actions.NopeAction;
-import sneckomod.cards.unknowns.AbstractUnknownCard;
 import sneckomod.patches.UnknownExtraUiPatch;
 
 public class Memorize extends AbstractSneckoCard {
@@ -21,7 +18,6 @@ public class Memorize extends AbstractSneckoCard {
     public Memorize() {
         super(ID, 0, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.SELF);
         tags.add(SneckoMod.SNEKPROOF);
-        selfRetain = true;
         FleetingField.fleeting.set(this, true);
     }
 
@@ -29,12 +25,13 @@ public class Memorize extends AbstractSneckoCard {
         atb(new MemorizeAction());
     }
 
-    @Override
-    public boolean canUpgrade() {
-        return false;
-    }
-
     public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            selfRetain = true;
+            rawDescription = UPGRADE_DESCRIPTION;
+            initializeDescription();
+        }
     }
 
     @Override
@@ -42,7 +39,7 @@ public class Memorize extends AbstractSneckoCard {
 
         for (AbstractCard c : AbstractDungeon.player.hand.group) {
             c.update();
-            if (UnknownExtraUiPatch.parentCard.get(c) != null){
+            if (UnknownExtraUiPatch.parentCard.get(c) != null) {
                 return super.canUse(p, m);
             }
         }
