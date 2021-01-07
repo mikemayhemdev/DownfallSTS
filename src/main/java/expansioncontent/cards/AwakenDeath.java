ccbc8905815c16471681a935c1be1472b7c79d0b
mikemayhemdev <mikemayhemdev@gmail.com>
2021-01-07 18:54:43 -0500

    Arts & texts.  Awaken buffed

--- src/main/java/expansioncontent/cards/AwakenDeath.java
+++ src/main/java/expansioncontent/cards/AwakenDeath.java
@@ -4,6 +4,7 @@ package expansioncontent.cards;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.StrengthPower;
 import expansioncontent.expansionContentMod;
 import expansioncontent.powers.AwakenDeathPower;
 
@@ -11,7 +12,7 @@ import expansioncontent.powers.AwakenDeathPower;
 public class AwakenDeath extends AbstractExpansionCard {
     public final static String ID = makeID("AwakenDeath");
 
-    private static final int MAGIC = 10;
+    private static final int MAGIC = 20;
     private static final int UPGRADE_MAGIC = 10;
 
     public AwakenDeath() {
@@ -24,7 +25,7 @@ public class AwakenDeath extends AbstractExpansionCard {
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
-
+        atb(new ApplyPowerAction(p, p, new StrengthPower(p, 1), 1));
         atb(new ApplyPowerAction(p, p, new AwakenDeathPower(p, p, this.magicNumber), this.magicNumber));
 
     }
