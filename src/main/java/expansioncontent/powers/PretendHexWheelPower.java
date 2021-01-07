52731aadda775d69367edcbc1e1b2849b7bc07f3
Vex Darkon <edanvirgo@gmail.com>
2021-01-07 14:06:16 -0700

    Hexaburn

--- src/main/java/expansioncontent/powers/PretendHexWheelPower.java
+++ src/main/java/expansioncontent/powers/PretendHexWheelPower.java
@@ -4,27 +4,27 @@ package expansioncontent.powers;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.evacipated.cardcrawl.mod.stslib.powers.interfaces.NonStackablePower;
-import com.megacrit.cardcrawl.actions.common.MakeTempCardInHandAction;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.actions.common.ReducePowerAction;
-import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.AbstractPower;
 import expansioncontent.expansionContentMod;
 import theHexaghost.util.TextureLoader;
 
 
-public class AddCopyPower extends AbstractPower implements NonStackablePower {
-    public static final String POWER_ID = expansionContentMod.makeID("AddCopyPower");
+public class PretendHexWheelPower extends AbstractPower implements NonStackablePower {
+    public static final String POWER_ID = expansionContentMod.makeID("PretendHexWheelPower");
     public static final String NAME = CardCrawlGame.languagePack.getPowerStrings(POWER_ID).NAME;
     public static final String DESCRIPTIONS[] = CardCrawlGame.languagePack.getPowerStrings(POWER_ID).DESCRIPTIONS;
 
     private static final Texture tex84 = TextureLoader.getTexture(expansionContentMod.getModID() + "Resources/images/powers/darkRitual84.png");
     private static final Texture tex32 = TextureLoader.getTexture(expansionContentMod.getModID() + "Resources/images/powers/darkRitual32.png");
 
-    private AbstractCard held;
-
-    public AddCopyPower(int amount, AbstractCard held) {
+    public PretendHexWheelPower(int amount) {
         this.name = NAME;
         this.ID = POWER_ID;
         this.owner = AbstractDungeon.player;
@@ -37,19 +37,28 @@ public class AddCopyPower extends AbstractPower implements NonStackablePower {
 
         this.updateDescription();
 
-        this.held = held;
-
     }
 
     @Override
-    public void atStartOfTurnPostDraw() {
+    public void atEndOfTurn(boolean isPlayer) {
         addToBot(new ReducePowerAction(owner, owner, this, 1));
-        addToBot(new MakeTempCardInHandAction(held.makeStatEquivalentCopy()));
+        if (amount == 1) {
+            for (int i = 0; i < 6; i++) {
+                addToBot(new AbstractGameAction() {
+                    @Override
+                    public void update() {
+                        isDone = true;
+                        AbstractMonster m = AbstractDungeon.getRandomMonster();
+                        addToTop(new DamageAction(m, new DamageInfo(owner, 6, DamageInfo.DamageType.THORNS), AttackEffect.FIRE));
+                    }
+                });
+            }
+        }
     }
 
     @Override
     public void updateDescription() {
-        description = DESCRIPTIONS[0] + held.name + DESCRIPTIONS[1];
+        description = amount == 1 ? DESCRIPTIONS[2] : DESCRIPTIONS[0] + amount + DESCRIPTIONS[1];
     }
 
 }
