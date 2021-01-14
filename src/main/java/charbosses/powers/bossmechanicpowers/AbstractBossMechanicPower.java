895ce6c46b55abdd2b45f138fec63d0fded37ec6
Vex Darkon <edanvirgo@gmail.com>
2021-01-13 17:08:15 -0700

    undo that stuff

--- src/main/java/charbosses/powers/bossmechanicpowers/AbstractBossMechanicPower.java
+++ src/main/java/charbosses/powers/bossmechanicpowers/AbstractBossMechanicPower.java
@@ -7,15 +7,8 @@ package charbosses.powers.bossmechanicpowers;
 
 import basemod.ReflectionHacks;
 import charbosses.bosses.AbstractCharBoss;
-import charbosses.vfx.QuietGainPowerEffect;
 import com.badlogic.gdx.Gdx;
-import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
-import com.megacrit.cardcrawl.core.AbstractCreature;
-import com.megacrit.cardcrawl.core.CardCrawlGame;
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
-import com.megacrit.cardcrawl.localization.PowerStrings;
 import com.megacrit.cardcrawl.powers.AbstractPower;
-import com.megacrit.cardcrawl.powers.ArtifactPower;
 import com.megacrit.cardcrawl.vfx.AbstractGameEffect;
 import com.megacrit.cardcrawl.vfx.combat.GainPowerEffect;
 
@@ -32,7 +25,7 @@ public abstract class AbstractBossMechanicPower extends AbstractPower {
         if (firstTurn){
             if (this.timer <= 0F){
                 ArrayList<AbstractGameEffect> effect2 = (ArrayList<AbstractGameEffect>) ReflectionHacks.getPrivate(this, AbstractPower.class, "effect");
-                effect2.add(new QuietGainPowerEffect(this));
+                effect2.add(new GainPowerEffect(this));
                 this.timer = 1F;
                 if (AbstractCharBoss.boss != null) {
                     if (AbstractCharBoss.boss.hb.hovered) {
