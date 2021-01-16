2c99b076b86eb5f8e4e6025da765da4f14c06b5d
mikemayhemdev <mikemayhemdev@gmail.com>
2021-01-15 19:49:15 -0500

    Update ChosenStrike.java

--- src/main/java/automaton/cards/ChosenStrike.java
+++ src/main/java/automaton/cards/ChosenStrike.java
@@ -19,7 +19,7 @@ public class ChosenStrike extends AbstractBronzeCard {
 
     public ChosenStrike() {
         super(ID, 1, CardType.ATTACK, CardRarity.UNCOMMON, CardTarget.ENEMY);
-        baseDamage = DAMAGE;
+        baseDamage = DAMAGE + misc;
         baseMagicNumber = magicNumber = 1;
         thisEncodes();
     }
@@ -59,6 +59,8 @@ public class ChosenStrike extends AbstractBronzeCard {
     }
 
     public void upp() {
-        upgradeDamage(UPG_DAMAGE);
+        misc += 3;
+        upgradedDamage = true;
+        baseDamage = DAMAGE + misc;
     }
 }
