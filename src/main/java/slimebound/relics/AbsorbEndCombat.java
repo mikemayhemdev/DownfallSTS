4b253196d3cdc94c989342ca605158ed5505f98e
Vex Darkon <edanvirgo@gmail.com>
2021-01-18 17:45:37 -0700

    Slime starter relic rework

--- src/main/java/slimebound/relics/AbsorbEndCombat.java
+++ src/main/java/slimebound/relics/AbsorbEndCombat.java
@@ -5,19 +5,13 @@ import com.badlogic.gdx.graphics.Texture;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.helpers.RelicLibrary;
 import com.megacrit.cardcrawl.relics.AbstractRelic;
-import slimebound.actions.SlimeSpawnAction;
-import slimebound.orbs.AttackSlime;
-import slimebound.orbs.PoisonSlime;
-import slimebound.orbs.ShieldSlime;
-import slimebound.orbs.SlimingSlime;
-
-import java.util.ArrayList;
 
 public class AbsorbEndCombat extends CustomRelic {
     public static final String ID = "Slimebound:AbsorbEndCombat";
     public static final String IMG_PATH = "relics/heartofgoo.png";
     public static final String OUTLINE_IMG_PATH = "relics/heartofgooOutline.png";
-    private static final int HP_PER_CARD = 1;
+    private static final int HP_PER_SLURP = 2;
+    private static final int HP_PER_COMBAT = 8;
 
     public AbsorbEndCombat() {
         super(ID, new Texture(slimebound.SlimeboundMod.getResourcePath(IMG_PATH)), new Texture(slimebound.SlimeboundMod.getResourcePath(OUTLINE_IMG_PATH)),
@@ -29,38 +23,24 @@ public class AbsorbEndCombat extends CustomRelic {
         return this.DESCRIPTIONS[0];
     }
 
-
-    /*
-    public void atBattleStartPreDraw() {
-        //Cheaty hack for testing max orbslots
-        //com.megacrit.cardcrawl.dungeons.AbstractDungeon.actionManager.addToBottom(new com.megacrit.cardcrawl.actions.defect.IncreaseMaxOrbAction(6));
-
-        this.flash();
-        ArrayList<Integer> orbs = new ArrayList();
-        orbs.add(1);
-        orbs.add(2);
-        orbs.add(3);
-        orbs.add(4);
-        Integer o = orbs.get(AbstractDungeon.cardRng.random(orbs.size() - 1));
-
-        switch (o) {
-            case 1:
-                AbstractDungeon.actionManager.addToBottom(new SlimeSpawnAction(new AttackSlime(), false, true));
-                break;
-            case 2:
-                AbstractDungeon.actionManager.addToBottom(new SlimeSpawnAction(new ShieldSlime(), false, true));
-                break;
-            case 3:
-                AbstractDungeon.actionManager.addToBottom(new SlimeSpawnAction(new SlimingSlime(), false, true));
-                break;
-            case 4:
-                AbstractDungeon.actionManager.addToBottom(new SlimeSpawnAction(new PoisonSlime(), false, true));
-                break;
-        }
-
-
+    @Override
+    public void atBattleStart() {
+        grayscale = false;
+        counter = HP_PER_COMBAT;
     }
-    */
+
+    @Override
+    public void onTrigger() {
+        int realAmount = Math.min(HP_PER_SLURP, counter);
+        if (realAmount > 0) {
+            AbstractDungeon.actionManager.addToBottom(new com.megacrit.cardcrawl.actions.common.HealAction(AbstractDungeon.player, AbstractDungeon.player, realAmount));
+            flash();
+            counter -= realAmount;
+            if (counter == 0) {
+                grayscale = true;
+            }
+        }
+    }
 
     @Override
     public void onUnequip() {
@@ -76,5 +56,4 @@ public class AbsorbEndCombat extends CustomRelic {
     public AbstractRelic makeCopy() {
         return new AbsorbEndCombat();
     }
-
 }
