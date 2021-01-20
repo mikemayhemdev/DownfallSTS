eaa45d99ed0bfbda6f59e2d7f3dea0b71f0e63c9
mikemayhemdev <mikemayhemdev@gmail.com>
2021-01-19 21:52:51 -0500

    Update PoisonProtectionPower.java

--- src/main/java/charbosses/powers/general/PoisonProtectionPower.java
+++ src/main/java/charbosses/powers/general/PoisonProtectionPower.java
@@ -1,18 +1,27 @@
 package charbosses.powers.general;
 
+import basemod.ReflectionHacks;
 import basemod.interfaces.CloneablePowerInterface;
+import charbosses.bosses.AbstractCharBoss;
 import charbosses.powers.bossmechanicpowers.AbstractBossMechanicPower;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.megacrit.cardcrawl.actions.common.ExhaustAction;
 import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.vfx.AbstractGameEffect;
+import com.megacrit.cardcrawl.vfx.combat.GainPowerEffect;
 import downfall.downfallMod;
+import downfall.monsters.NeowBossFinal;
 import downfall.util.TextureLoader;
 
-public class PoisonProtectionPower extends AbstractBossMechanicPower implements CloneablePowerInterface {
+import java.util.ArrayList;
+
+public class PoisonProtectionPower extends AbstractPower implements CloneablePowerInterface {
 
     public static final String POWER_ID = downfallMod.makeID("PoisonResist");
     public static final String NAME = CardCrawlGame.languagePack.getPowerStrings(POWER_ID).NAME;
@@ -22,6 +31,9 @@ public class PoisonProtectionPower extends AbstractBossMechanicPower implements 
     private static final Texture tex32 = TextureLoader.getTexture(downfallMod.assetPath("images/powers/PoisonResist32.png"));
 
     public static boolean CANNOT_END = false;
+
+    private float timer;
+    private boolean firstTurn = true;
 
     public PoisonProtectionPower(AbstractCreature owner) {
         this.ID = POWER_ID;
@@ -38,10 +50,35 @@ public class PoisonProtectionPower extends AbstractBossMechanicPower implements 
     }
 
     @Override
+    public void update(int slot) {
+        super.update(slot);
+        if (firstTurn){
+            if (this.timer <= 0F){
+                ArrayList<AbstractGameEffect> effect2 = (ArrayList<AbstractGameEffect>) ReflectionHacks.getPrivate(this, AbstractPower.class, "effect");
+                effect2.add(new GainPowerEffect(this));
+                this.timer = 1F;
+                if (AbstractDungeon.player != null) {
+                    if (AbstractDungeon.player.hb.hovered) {
+                        firstTurn = false;
+                    }
+                }
+            } else {
+                this.timer -= Gdx.graphics.getDeltaTime();
+            }
+        }
+    }
+
+
+    @Override
     public void updateDescription() {
         this.description = DESCRIPTIONS[0];
     }
 
+
+    @Override
+    public void playApplyPowerSfx() {
+        //to prevent the 'last turn' warning from pinging audio all the time
+    }
 
     @Override
     public AbstractPower makeCopy() {
