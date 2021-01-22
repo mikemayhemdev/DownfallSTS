570fd575f30fbb1e409cfefefa62d7032e6772b4
Arjun Krishna <a.krishna.3367@gmail.com>
2021-01-22 03:35:06 -0600

    Enabling A19 relics / random stuff

--- src/main/java/downfall/powers/NeowInvulnerablePower.java
+++ src/main/java/downfall/powers/NeowInvulnerablePower.java
@@ -57,7 +57,7 @@ public class NeowInvulnerablePower extends AbstractPower {
 
     @Override
     public void updateDescription() {
-        this.description = DESCRIPTIONS[0];
+        this.description = DESCRIPTIONS[0] + amount + DESCRIPTIONS[1];
     }
 
 }
