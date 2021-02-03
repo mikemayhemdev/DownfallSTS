32475c95b8335821480fa5ff06794dfe5822a3ee
HypoSoc <hyposoc1@gmail.com>
2021-02-03 01:02:43 -0500

    Updating Resurrect

--- src/main/java/gremlin/patches/CampfirePatch.java
+++ src/main/java/gremlin/patches/CampfirePatch.java
@@ -1,37 +1,51 @@
 package gremlin.patches;
 
 import basemod.ReflectionHacks;
-import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.evacipated.cardcrawl.modthespire.lib.*;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.rooms.CampfireUI;
 import com.megacrit.cardcrawl.ui.campfire.AbstractCampfireOption;
 import gremlin.characters.GremlinCharacter;
 import gremlin.ui.campfire.ResurrectOption;
+import javassist.CtBehavior;
 
 import java.util.ArrayList;
 
 @SpirePatch(cls = "com.megacrit.cardcrawl.rooms.CampfireUI", method = "initializeButtons")
 public class CampfirePatch
 {
-    public static void Postfix(final Object meObj) {
+//    public static void Postfix(final Object meObj) {
+//        if (AbstractDungeon.player instanceof GremlinCharacter) {
+//            final CampfireUI campfire = (CampfireUI) meObj;
+//            try {
+//                final ArrayList<AbstractCampfireOption> campfireButtons = (ArrayList<AbstractCampfireOption>) ReflectionHacks.getPrivate((Object) campfire, (Class) CampfireUI.class, "buttons");
+//                campfireButtons.add(new ResurrectOption());
+//                float x = 950.0f;
+//                final float y = 990.0f - 270.0f * ((campfireButtons.size() + 1) / 2);
+//                if (campfireButtons.size() % 2 == 0) {
+//                    x = 1110.0f;
+//                    campfireButtons.get(campfireButtons.size() - 2).setPosition(800.0f * Settings.scale, y * Settings.scale);
+//                }
+//                campfireButtons.get(campfireButtons.size() - 1).setPosition(x * Settings.scale, y * Settings.scale);
+//            } catch (SecurityException ex) {
+//            } catch (IllegalArgumentException ex2) {
+//            }
+//        }
+//    }
+    @SpireInsertPatch(locator = Locator.class)
+    public static void patch(CampfireUI __instance, ArrayList<AbstractCampfireOption> ___buttons) {
         if (AbstractDungeon.player instanceof GremlinCharacter) {
-            if(((GremlinCharacter)AbstractDungeon.player).canRez()) {
-                final CampfireUI campfire = (CampfireUI) meObj;
-                try {
-                    final ArrayList<AbstractCampfireOption> campfireButtons = (ArrayList<AbstractCampfireOption>) ReflectionHacks.getPrivate((Object) campfire, (Class) CampfireUI.class, "buttons");
-                    campfireButtons.add(new ResurrectOption());
-                    float x = 950.0f;
-                    final float y = 990.0f - 270.0f * ((campfireButtons.size() + 1) / 2);
-                    if (campfireButtons.size() % 2 == 0) {
-                        x = 1110.0f;
-                        campfireButtons.get(campfireButtons.size() - 2).setPosition(800.0f * Settings.scale, y * Settings.scale);
-                    }
-                    campfireButtons.get(campfireButtons.size() - 1).setPosition(x * Settings.scale, y * Settings.scale);
-                } catch (SecurityException ex) {
-                } catch (IllegalArgumentException ex2) {
-                }
-            }
+            ___buttons.add(new ResurrectOption());
+        }
+    }
+
+    public static class Locator extends SpireInsertLocator {
+        @Override
+        public int[] Locate(CtBehavior ctBehavior) throws Exception {
+            Matcher finalMatcher = new Matcher.FieldAccessMatcher(AbstractPlayer.class, "relics");
+            return LineFinder.findInOrder(ctBehavior, finalMatcher);
         }
     }
 }
--- src/main/java/gremlin/ui/campfire/ResurrectOption.java
+++ src/main/java/gremlin/ui/campfire/ResurrectOption.java
@@ -1,10 +1,17 @@
 package gremlin.ui.campfire;
 
+import basemod.ReflectionHacks;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.helpers.ImageMaster;
+import com.megacrit.cardcrawl.helpers.MathHelper;
+import com.megacrit.cardcrawl.helpers.controller.CInputActionSet;
+import com.megacrit.cardcrawl.helpers.input.InputHelper;
 import com.megacrit.cardcrawl.localization.UIStrings;
 import com.megacrit.cardcrawl.rooms.AbstractRoom;
+import com.megacrit.cardcrawl.rooms.CampfireUI;
+import com.megacrit.cardcrawl.rooms.RestRoom;
 import com.megacrit.cardcrawl.ui.campfire.AbstractCampfireOption;
 import gremlin.GremlinMod;
 import gremlin.characters.GremlinCharacter;
@@ -13,10 +20,30 @@ public class ResurrectOption extends AbstractCampfireOption {
     private static final UIStrings uiStrings;
     public static final String[] TEXT;
 
+    private boolean used;
+    private boolean hacked;
+
     public ResurrectOption() {
-        this.label = ResurrectOption.TEXT[0];
-        this.description = ResurrectOption.TEXT[1];
-        this.img = ImageMaster.loadImage(GremlinMod.getResourcePath("ui/campfire/resurrect.png"));
+        this.usable = ((GremlinCharacter)AbstractDungeon.player).canRez();
+        updateImage();
+    }
+
+    public void updateImage() {
+        this.label = TEXT[0];
+        if (this.usable) {
+            this.img = ImageMaster.loadImage(GremlinMod.getResourcePath("ui/campfire/resurrect.png"));
+        } else {
+            this.img = ImageMaster.loadImage(GremlinMod.getResourcePath("ui/campfire/resurrect_unusable.png"));
+        }
+        if (!this.used) {
+            if (this.usable) {
+                this.description += TEXT[1];
+            } else {
+                this.description = TEXT[2];
+            }
+        } else {
+            this.description = TEXT[2];
+        }
     }
 
     @Override
@@ -26,7 +53,55 @@ public class ResurrectOption extends AbstractCampfireOption {
         if(AbstractDungeon.player instanceof GremlinCharacter){
             ((GremlinCharacter)AbstractDungeon.player).resurrect();
         }
+        this.used = true;
         AbstractDungeon.getCurrRoom().phase = AbstractRoom.RoomPhase.COMPLETE;
+    }
+
+    @Override
+    public void update() {
+        float hackScale = (float) ReflectionHacks.getPrivate(this, AbstractCampfireOption.class, "scale");
+
+        if (this.hb.hovered) {
+
+            if (!this.hb.clickStarted) {
+                ReflectionHacks.setPrivate(this, AbstractCampfireOption.class, "scale", MathHelper.scaleLerpSnap(hackScale, Settings.scale));
+                ReflectionHacks.setPrivate(this, AbstractCampfireOption.class, "scale", MathHelper.scaleLerpSnap(hackScale, Settings.scale));
+
+            } else {
+                ReflectionHacks.setPrivate(this, AbstractCampfireOption.class, "scale", MathHelper.scaleLerpSnap(hackScale, 0.9F * Settings.scale));
+
+            }
+        } else {
+            ReflectionHacks.setPrivate(this, AbstractCampfireOption.class, "scale", MathHelper.scaleLerpSnap(hackScale, 0.9F * Settings.scale));
+        }
+
+        super.update();
+
+        if (!this.used) {
+            if (!((GremlinCharacter)AbstractDungeon.player).canRez() && this.usable) {
+                this.usable = false;
+                updateImage();
+            }
+            if (((GremlinCharacter)AbstractDungeon.player).canRez() && !this.usable) {
+                this.usable = true;
+                updateImage();
+            }
+        }
+
+        CampfireUI campfire = ((RestRoom) AbstractDungeon.getCurrRoom()).campfireUI;
+
+
+        if (this.used && !this.hacked) {
+            this.hacked = true;
+            campfire.somethingSelected = false;
+            campfire.touchOption = null;
+            campfire.confirmButton.hide();
+            campfire.confirmButton.hideInstantly();
+            campfire.confirmButton.isDisabled = true;
+
+            AbstractDungeon.overlayMenu.proceedButton.hide();
+            AbstractDungeon.overlayMenu.proceedButton.hideInstantly();
+        }
     }
 
     static {
