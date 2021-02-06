7e2b3b61fef948c2c27a32501db9abf69000ef4a
HypoSoc <hyposoc1@gmail.com>
2021-02-05 21:53:40 -0500

    Relic patches.

--- src/main/java/gremlin/patches/relicpatches/GremlinMaskPatch.java
+++ src/main/java/gremlin/patches/relicpatches/GremlinMaskPatch.java
@@ -1,4 +1,4 @@
-package gremlin.patches;
+package gremlin.patches.relicpatches;
 
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpireReturn;
--- src/main/java/gremlin/patches/relicpatches/TinyHouseDescriptionPatch.java
+++ src/main/java/gremlin/patches/relicpatches/TinyHouseDescriptionPatch.java
@@ -1,23 +1,23 @@
-package gremlin.patches;
+package gremlin.patches.relicpatches;
 
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpireReturn;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.RelicStrings;
-import com.megacrit.cardcrawl.relics.GremlinMask;
+import com.megacrit.cardcrawl.relics.TinyHouse;
 import gremlin.characters.GremlinCharacter;
 
 @SpirePatch(
-        clz= GremlinMask.class,
+        clz= TinyHouse.class,
         method="getUpdatedDescription"
 )
-public class GremlinMaskDescriptionPatch {
-    private static final RelicStrings strings = CardCrawlGame.languagePack.getRelicStrings("Gremlin:GremlinMask");
+public class TinyHouseDescriptionPatch {
+    private static final RelicStrings strings = CardCrawlGame.languagePack.getRelicStrings("Gremlin:PatchFixes");
 
-    public static SpireReturn<String> Prefix(GremlinMask __instance){
+    public static SpireReturn<String> Prefix(TinyHouse __instance){
         if(AbstractDungeon.player instanceof GremlinCharacter){
-            return SpireReturn.Return(strings.DESCRIPTIONS[0]);
+            return SpireReturn.Return(__instance.DESCRIPTIONS[0] + 50 + strings.DESCRIPTIONS[2] + 1 + __instance.DESCRIPTIONS[2]);
         }
         return SpireReturn.Continue();
     }
