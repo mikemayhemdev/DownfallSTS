f57bd1679b74120e2553095fa0ad9b89a1518733
DarkVexon <edanvirgo@gmail.com>
2021-02-09 11:21:21 -0700

    First steps

--- src/main/java/theHexaghost/cards/FlamesFromBeyond.java
+++ src/main/java/theHexaghost/cards/FlamesFromBeyond.java
@@ -14,8 +14,8 @@ public class FlamesFromBeyond extends AbstractHexaCard {
 
     //stupid intellij stuff SKILL, NONE, COMMON
 
-    private static final int MAGIC = 12;
-    private static final int UPG_MAGIC = 6;
+    private static final int MAGIC = 15;
+    private static final int UPG_MAGIC = 5;
 
     public FlamesFromBeyond() {
         super(ID, -2, CardType.SKILL, CardRarity.COMMON, CardTarget.NONE);
--- src/main/java/theHexaghost/cards/Sear.java
+++ src/main/java/theHexaghost/cards/Sear.java
@@ -1,9 +1,13 @@
 package theHexaghost.cards;
 
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.animations.VFXAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.vfx.combat.FireballEffect;
+import theHexaghost.powers.BurnPower;
 
 public class Sear extends AbstractHexaCard {
 
@@ -11,16 +15,31 @@ public class Sear extends AbstractHexaCard {
 
     //stupid intellij stuff SKILL, ENEMY, BASIC
 
-    private static final int MAGIC = 9;
+    private static final int MAGIC = 10;
 
     public Sear() {
         super(ID, 1, CardType.SKILL, CardRarity.BASIC, CardTarget.ENEMY);
         baseBurn = burn = MAGIC;
+        isEthereal = true;
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
         atb(new VFXAction(new FireballEffect(p.hb.cX, p.hb.cY, m.hb.cX, m.hb.cY), 0.5F));
         burn(m, burn);
+    }
+
+    @Override
+    public void triggerOnExhaust() {
+        atb(new AbstractGameAction() {
+            @Override
+            public void update() {
+                isDone = true;
+                AbstractMonster m = AbstractDungeon.getRandomMonster();
+                att(new ApplyPowerAction(m, AbstractDungeon.player, new BurnPower(m, burn), burn));
+                att(new VFXAction(new FireballEffect(AbstractDungeon.player.hb.cX, AbstractDungeon.player.hb.cY, m.hb.cX, m.hb.cY), 0.5F));
+            }
+        });
+
     }
 
     public void upgrade() {
