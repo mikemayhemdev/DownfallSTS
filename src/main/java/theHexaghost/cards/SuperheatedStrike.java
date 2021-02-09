5bf88f50a9a4a4707902ed3a456a20df77b15771
DarkVexon <edanvirgo@gmail.com>
2021-02-09 11:28:21 -0700

    Nuke some cards

--- src/main/java/theHexaghost/cards/SuperheatedStrike.java
+++ src/main/java/theHexaghost/cards/SuperheatedStrike.java
@@ -5,7 +5,9 @@ import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import downfall.util.CardIgnore;
 
+@CardIgnore
 public class SuperheatedStrike extends AbstractHexaCard {
 
     public final static String ID = makeID("SuperheatedStrike");
