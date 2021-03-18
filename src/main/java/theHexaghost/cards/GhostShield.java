4e0d50bddaead895beaf8c06fcb7297f35093f5a
DarkVexon <edanvirgo@gmail.com>
2021-03-18 13:20:18 -0700

    Ghost Lash and Ghost Shield changes

--- src/main/java/theHexaghost/cards/GhostShield.java
+++ src/main/java/theHexaghost/cards/GhostShield.java
@@ -5,6 +5,7 @@ import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import theHexaghost.patches.ExhaustCardTickPatch;
 
 public class GhostShield extends AbstractHexaCard {
 
@@ -23,20 +24,17 @@ public class GhostShield extends AbstractHexaCard {
 
     public void use(AbstractPlayer p, AbstractMonster m) {
         blck();
-        if (hasEthereal())
+        if (ExhaustCardTickPatch.exhaustedLastTurn)
             addToBot(new GainBlockAction(p, block));
     }
 
-    private boolean hasEthereal() {
-        for (AbstractCard c : AbstractDungeon.player.hand.group) {
-            if (c.isEthereal && c != this)
-                return true;
-        }
-        return false;
+    @Override
+    public void triggerOnExhaust() {
+        use(AbstractDungeon.player, null);
     }
 
     public void triggerOnGlowCheck() {
-        this.glowColor = hasEthereal() ? AbstractCard.GOLD_BORDER_GLOW_COLOR : AbstractCard.BLUE_BORDER_GLOW_COLOR;// 65
+        this.glowColor = ExhaustCardTickPatch.exhaustedLastTurn ? AbstractCard.GOLD_BORDER_GLOW_COLOR : AbstractCard.BLUE_BORDER_GLOW_COLOR;// 65
     }// 68
 
     public void upgrade() {
