954cd8423f4ec4e6baa38b09d94a00db4b2e2590
Rita-Bernstein <13536709069@163.com>
2021-03-29 16:24:28 +0800

    Animation improve

--- src/main/java/reskinContent/skinCharacter/skins/Champ/ChampSister.java
+++ src/main/java/reskinContent/skinCharacter/skins/Champ/ChampSister.java
@@ -10,12 +10,9 @@ public class ChampSister extends AbstractSkin {
 
     public ChampSister() {
         this.portraitStatic_IMG = ImageMaster.loadImage(reskinContent.assetPath("img/ChampMod/Chan-P/portrait_waifu.png"));
-        this.portraitAnimation_IMG = ImageMaster.loadImage(reskinContent.assetPath("img/ChampMod/Chan-P/portrait_waifu2.png"));
 
         this.NAME = CardCrawlGame.languagePack.getUIString("reskinContent:ReSkinChamp").TEXT[0];
         this.DESCRIPTION = CardCrawlGame.languagePack.getCharacterString(ChampChar.ID).TEXT[0];
-
-        this.portraitAtlasPath = reskinContent.assetPath("img/ChampMod/Chan-P/animation/GuardianChan_portrait");
 
         this.SHOULDER1 = "reskinContent/img/ChampMod/Chan-P/shoulder2.png";
         this.SHOULDER2 = "reskinContent/img/ChampMod/Chan-P/shoulder.png";
@@ -24,11 +21,6 @@ public class ChampSister extends AbstractSkin {
         this.jsonURL = "reskinContent/img/ChampMod/Chan-P/animation/TheChampPlayer.json";
         this.renderscale = 2.5f;
     }
-    @Override
-    public void InitializeStaticPortraitVar() {
-
-    }
-
 }
 
 
