9cc13b2254482665515eecbdba576b8e6b5d6108
DoomZero <doomzero755@gmail.com>
2021-03-31 02:14:57 -0400

    commit

--- src/main/java/reskinContent/skinCharacter/skins/Hexaghost/BetaHexaghost.java
+++ src/main/java/reskinContent/skinCharacter/skins/Hexaghost/BetaHexaghost.java
@@ -2,6 +2,7 @@ package reskinContent.skinCharacter.skins.Hexaghost;
 
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.helpers.ImageMaster;
+import reskinContent.reskinContent;
 import reskinContent.skinCharacter.AbstractSkin;
 import theHexaghost.TheHexaghost;
 
@@ -10,7 +11,7 @@ public class BetaHexaghost extends AbstractSkin {
     public BetaHexaghost() {
         this.NAME = CardCrawlGame.languagePack.getUIString("reskinContent:ReSkinHexaghost").TEXT[1];
         this.DESCRIPTION = CardCrawlGame.languagePack.getCharacterString(TheHexaghost.ID).TEXT[0];
-        this.portraitStatic_IMG = ImageMaster.loadImage("reskinContent/img/HexaghostMod/BetaHexaghost/portrait_beta.png");
+        this.portraitStatic_IMG = ImageMaster.loadImage(getAssetPath("portrait_beta_hexaghost.png"));
 
         this.SHOULDER1 = "hexamodResources/images/char/mainChar/shoulder.png";;
         this.SHOULDER2 = "hexamodResources/images/char/mainChar/shoulderR.png";
@@ -18,6 +19,10 @@ public class BetaHexaghost extends AbstractSkin {
         this.atlasURL = "reskinContent/img/HexaghostMod/BetaHexaghost/animation/BetaHexaghost.atlas";
         this.jsonURL = "reskinContent/img/HexaghostMod/BetaHexaghost/animation/BetaHexaghost.json";
         this.renderscale = 1.0F;
+    }
+
+    public String getAssetPath(String filename) {
+        return reskinContent.assetPath("img/HexaghostMod/" + this.getClass().getSimpleName() + "/" + filename);
     }
 }
 
