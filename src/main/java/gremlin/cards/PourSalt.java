52a89c9cde8c157c47fabe7a62097a4bf1463dfa
HypoSoc <hyposoc1@gmail.com>
2021-05-14 17:10:38 -0400

    Pour Salt nerf

--- src/main/java/gremlin/cards/PourSalt.java
+++ src/main/java/gremlin/cards/PourSalt.java
@@ -23,7 +23,7 @@ public class PourSalt extends AbstractGremlinCard {
     private static final AbstractCard.CardTarget TARGET = AbstractCard.CardTarget.ENEMY;
 
     private static final int COST = 1;
-    private static final int POWER = 6;
+    private static final int POWER = 4;
     private static final int UPGRADE_BONUS = 2;
     private static final int MAGIC = 2;
     private static final int UPGRADE_MAGIC = 1;
