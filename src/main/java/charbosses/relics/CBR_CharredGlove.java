2f915d6617f7a64f761076550983ef7675077794
Vex Darkon <edanvirgo@gmail.com>
2022-03-08 19:45:10 -0700

    Hermit fixes

--- src/main/java/charbosses/relics/CBR_CharredGlove.java
+++ src/main/java/charbosses/relics/CBR_CharredGlove.java
@@ -1,14 +1,14 @@
 package charbosses.relics;
 
 import charbosses.bosses.AbstractCharBoss;
+import com.badlogic.gdx.graphics.Texture;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
-import com.megacrit.cardcrawl.actions.common.RelicAboveCreatureAction;
+import com.megacrit.cardcrawl.actions.utility.ReApplyPowersAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
-import com.megacrit.cardcrawl.powers.DexterityPower;
 import com.megacrit.cardcrawl.powers.LoseStrengthPower;
 import com.megacrit.cardcrawl.powers.StrengthPower;
 import com.megacrit.cardcrawl.relics.AbstractRelic;
-import com.megacrit.cardcrawl.relics.OddlySmoothStone;
 import hermit.relics.CharredGlove;
 
 public class CBR_CharredGlove extends AbstractCharbossRelic {
@@ -16,6 +16,7 @@ public class CBR_CharredGlove extends AbstractCharbossRelic {
 
     public CBR_CharredGlove() {
         super(new CharredGlove());
+        setTextureOutline(CharredGlove.IMG, CharredGlove.OUTLINE);
     }
 
     @Override
@@ -24,14 +25,37 @@ public class CBR_CharredGlove extends AbstractCharbossRelic {
     }
 
     @Override
-    public void atTurnStart() {
-        for (AbstractCard q : AbstractCharBoss.boss.hand.group) {
-            if (q.type == AbstractCard.CardType.CURSE || q.color == AbstractCard.CardColor.CURSE) {
-                flash();
-                addToBot(new ApplyPowerAction(AbstractCharBoss.boss, AbstractCharBoss.boss, new StrengthPower(AbstractCharBoss.boss, 4), 4));
-                addToBot(new ApplyPowerAction(AbstractCharBoss.boss, AbstractCharBoss.boss, new LoseStrengthPower(AbstractCharBoss.boss, 4), 4));
+    public void atTurnStartPostDraw() {
+        addToBot(new AbstractGameAction() {
+            @Override
+            public void update() {
+                isDone = true;
+                addToTop(new AbstractGameAction() {
+                    @Override
+                    public void update() {
+                        isDone = true;
+                        AbstractCharBoss.boss.hand.applyPowers();
+                    }
+                });
+                for (AbstractCard q : AbstractCharBoss.boss.hand.group) {
+                    if (q.type == AbstractCard.CardType.CURSE || q.color == AbstractCard.CardColor.CURSE) {
+                        flash();
+                        addToTop(new ApplyPowerAction(AbstractCharBoss.boss, AbstractCharBoss.boss, new LoseStrengthPower(AbstractCharBoss.boss, 4), 4));
+                        addToTop(new ApplyPowerAction(AbstractCharBoss.boss, AbstractCharBoss.boss, new StrengthPower(AbstractCharBoss.boss, 4), 4));
+                    }
+                }
             }
-        }
+        });
+    }
+
+    public void setTexture(Texture t) {
+        this.img = t;
+        this.outlineImg = t;
+    }
+
+    public void setTextureOutline(Texture t, Texture o) {
+        this.img = t;
+        this.outlineImg = o;
     }
 
     @Override
