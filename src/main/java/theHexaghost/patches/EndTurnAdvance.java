3affc11338d5f0e4dadcf7fd1460aeb3cd4fc34a
Stanek-K <jgdgkot@gmail.com>
2022-03-17 10:06:20 +0100

    Hexa's power polish + Technical Jig rework
    Technical Jig now triggers Technique effect of Stance you entered, and no longer has Technique tag.
    
    Updated Doomsday, Here and Now, and Speedrunning.

--- src/main/java/theHexaghost/patches/EndTurnAdvance.java
+++ src/main/java/theHexaghost/patches/EndTurnAdvance.java
@@ -1,13 +1,9 @@
 package theHexaghost.patches;
 
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
-import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.GameActionManager;
-import com.megacrit.cardcrawl.actions.common.DamageRandomEnemyAction;
 import com.megacrit.cardcrawl.actions.common.ReducePowerAction;
 import com.megacrit.cardcrawl.actions.utility.WaitAction;
-import com.megacrit.cardcrawl.cards.DamageInfo;
-import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.powers.AbstractPower;
 import downfall.downfallMod;
@@ -19,7 +15,7 @@ import theHexaghost.ghostflames.AbstractGhostflame;
 import theHexaghost.ghostflames.InfernoGhostflame;
 import theHexaghost.ghostflames.MayhemGhostflame;
 import theHexaghost.powers.AgainPower;
-import theHexaghost.powers.GainStrengthThatGoesAwayPower;
+import theHexaghost.powers.StopFromAdvancingPower;
 
 @SpirePatch(
         clz = GameActionManager.class,
@@ -34,8 +30,8 @@ public class EndTurnAdvance {
                 AbstractPower p = AbstractDungeon.player.getPower(AgainPower.POWER_ID);
                 p.flash();
                 AbstractDungeon.actionManager.addToBottom(new ReducePowerAction(p.owner, p.owner, p, 1));
-            } else if (AbstractDungeon.player.hasPower(GainStrengthThatGoesAwayPower.POWER_ID)) {
-                AbstractPower p = AbstractDungeon.player.getPower(GainStrengthThatGoesAwayPower.POWER_ID);
+            } else if (AbstractDungeon.player.hasPower(StopFromAdvancingPower.POWER_ID)) {
+                AbstractPower p = AbstractDungeon.player.getPower(StopFromAdvancingPower.POWER_ID);
                 p.flash();
             } else if (GhostflameHelper.activeGhostFlame.charged) {
                 if (GhostflameHelper.activeGhostFlame instanceof InfernoGhostflame) {
