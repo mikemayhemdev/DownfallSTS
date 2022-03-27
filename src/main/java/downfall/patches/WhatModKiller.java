793935af8c619f27ace569fe2906a359b52aef78
DarkVexon <edanvirgo@gmail.com>
2022-03-27 13:28:07 -0700

    Don't nuke whatmod on non-steam

--- src/main/java/downfall/patches/WhatModKiller.java
+++ src/main/java/downfall/patches/WhatModKiller.java
@@ -4,10 +4,13 @@ import basemod.patches.whatmod.WhatMod;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpireReturn;
+import downfall.downfallMod;
 
 @SpirePatch(clz = WhatMod.class, method = "renderModTooltip", paramtypez = {SpriteBatch.class, Class.class, float.class, float.class})
 public class WhatModKiller {
     public static SpireReturn Prefix(SpriteBatch sb, Class cla, float x, float y) {
-        return SpireReturn.Return();
+        if (downfallMod.STEAM_MODE)
+            return SpireReturn.Return();
+        return SpireReturn.Continue();
     }
 }
