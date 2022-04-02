1fee6d327fc5bc5f69f27325a3a80edaa41feff3
DarkVexon <edanvirgo@gmail.com>
2022-04-01 23:45:08 -0700

    Suspend helper more stuff

--- src/main/java/timeeater/suspend/SuspendHelper.java
+++ src/main/java/timeeater/suspend/SuspendHelper.java
@@ -1,18 +1,28 @@
 package timeeater.suspend;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.DrawCardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.CardGroup;
+import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 
 import java.util.ArrayList;
 
+import static automaton.FunctionHelper.HEIGHT_SEQUENCE;
 import static timeeater.util.Wiz.atb;
 
 public class SuspendHelper {
-    public static ArrayList<AbstractCard> suspendGroup = new ArrayList<>();
+    public static CardGroup suspendGroup = new CardGroup(CardGroup.CardGroupType.UNSPECIFIED);
     public static final int SUSPEND_DRAW_PER_TURN = 3;
+
+    public static final Vector2[] cardPositions = {
+            new Vector2(218f * Settings.xScale, HEIGHT_SEQUENCE),
+            new Vector2(293f * Settings.xScale, HEIGHT_SEQUENCE),
+            new Vector2(368f * Settings.xScale, HEIGHT_SEQUENCE)
+    };
 
     public static void atCombatStart() {
         suspendGroup.clear();
@@ -26,8 +36,9 @@ public class SuspendHelper {
                 @Override
                 public void update() {
                     isDone = true;
-                    AbstractCard q = suspendGroup.remove(0);
+                    AbstractCard q = suspendGroup.getTopCard();
                     AbstractDungeon.player.drawPile.addToTop(q);
+                    suspendGroup.removeCard(q);
                     AbstractDungeon.actionManager.addToTop(new DrawCardAction(1));
                 }
             });
@@ -35,14 +46,22 @@ public class SuspendHelper {
     }
 
     public static void suspend(AbstractCard q) {
-        suspendGroup.add(q);
+        suspendGroup.addToBottom(q);
     }
 
     public static void update() {
-
+        for (int i = 0; i < suspendGroup.size(); i++) {
+            AbstractCard c = suspendGroup.group.get(i);
+            c.target_x = cardPositions[i].x;
+            c.target_y = cardPositions[i].y;
+            c.update();
+            c.updateHoverLogic();
+        }
     }
 
     public static void render(SpriteBatch sb) {
-
+        for (AbstractCard q : suspendGroup.group) {
+            q.render(sb);
+        }
     }
 }
