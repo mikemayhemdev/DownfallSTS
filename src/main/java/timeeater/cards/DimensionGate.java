8ee83a5e520e2c15b3e1a107b96f5277bbd80189
DarkVexon <edanvirgo@gmail.com>
2022-04-03 23:10:07 -0700

    Dimension Gate works

--- src/main/java/timeeater/cards/DimensionGate.java
+++ src/main/java/timeeater/cards/DimensionGate.java
@@ -1,43 +1,47 @@
 package timeeater.cards;
 
+import com.evacipated.cardcrawl.mod.stslib.actions.common.SelectCardsCenteredAction;
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInHandAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.helpers.CardLibrary;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import hermit.cards.Grudge;
 import hermit.cards.LuckOfTheDraw;
 import hermit.cards.Malice;
 import timeeater.TimeEaterMod;
 import timeeater.cards.alternateDimension.*;
+import timeeater.util.Wiz;
 
 import java.util.ArrayList;
 import java.util.Collections;
 
 import static timeeater.TimeEaterMod.makeID;
+import static timeeater.util.Wiz.att;
 
-public class DimensionGate extends AbstractMemoryCard {
+public class DimensionGate extends AbstractTimeEaterCard {
     public final static String ID = makeID("DimensionGate");
     // intellij stuff  uncommon,
 
 
     public DimensionGate() {
-        super(ID, 1, CardRarity.RARE);
+        super(ID, 1, CardType.SKILL, CardRarity.RARE, CardTarget.SELF);
     }
 
-
-
     @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new SelectCardsCenteredAction(cards(), cardStrings.EXTENDED_DESCRIPTION[0], (cards) -> {
+            att(new MakeTempCardInHandAction(cards.get(0)));
+        }));
+    }
+
     protected ArrayList<AbstractCard> cards() {
         if (TimeEaterMod.dimensionCards.size() == 0)
             TimeEaterMod.initializeDimensionCardStrings();
-        Collections.shuffle(TimeEaterMod.dimensionCards);
         ArrayList<AbstractCard> retVal = new ArrayList<>();
-
-        //TODO this crashes!  Maybe a race condition? card library isn't fully initialized yet?
-        //Ideally there is one stored place for the dimension card array that isn't a card
-        //for performance reasons, no need to be duping the list of dimension cards all over.
-
-      //  retVal.add(CardLibrary.getCard(TimeEaterMod.dimensionCards.get(0)).makeCopy());
-      //  retVal.add(CardLibrary.getCard(TimeEaterMod.dimensionCards.get(1)).makeCopy());
-       // retVal.add(CardLibrary.getCard(TimeEaterMod.dimensionCards.get(2)).makeCopy());
+        for (int i = 0; i < 3; i++) {
+            retVal.add(CardLibrary.getCard(Wiz.getRandomItem(TimeEaterMod.dimensionCards)).makeCopy());
+        }
         return retVal;
     }
 
