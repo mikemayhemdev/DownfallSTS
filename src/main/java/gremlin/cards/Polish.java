e455b422ea0e52135ac6acf132220eb02e4d90cc
Mwallx <89969909+Mwallx@users.noreply.github.com>
2022-05-04 12:10:19 +0800

    preview for polish

--- src/main/java/gremlin/cards/Polish.java
+++ src/main/java/gremlin/cards/Polish.java
@@ -1,14 +1,19 @@
 package gremlin.cards;
 
+import com.badlogic.gdx.Gdx;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.tempCards.Shiv;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.helpers.CardLibrary;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.AccuracyPower;
 import gremlin.powers.PolishPower;
+
+import java.util.ArrayList;
 
 import static gremlin.GremlinMod.SHIELD_GREMLIN;
 
@@ -29,6 +34,9 @@ public class Polish extends AbstractGremlinCard {
     public Polish()
     {
         super(ID, NAME, IMG_PATH, COST, strings.DESCRIPTION, TYPE, RARITY, TARGET);
+
+        cardsList.add(new Shiv());
+        cardsList.add(new Ward());
 
         this.baseMagicNumber = MAGIC;
         this.magicNumber = baseMagicNumber;
@@ -52,5 +60,31 @@ public class Polish extends AbstractGremlinCard {
             upgradeMagicNumber(UPGRADE_BONUS);
         }
     }
+
+    @Override
+    public void update() {
+        super.update();
+        if (hb.hovered) {
+            if (rotationTimer <= 0F) {
+                rotationTimer = 2F;
+                if (cardsList.size() == 0) {
+                    cardsToPreview = CardLibrary.cards.get("Madness");
+                } else {
+                    cardsToPreview = cardsList.get(previewIndex);
+                }
+                if (previewIndex == cardsList.size() - 1) {
+                    previewIndex = 0;
+                } else {
+                    previewIndex++;
+                }
+            } else {
+                rotationTimer -= Gdx.graphics.getDeltaTime();
+            }
+        }
+    }
+
+    private float rotationTimer;
+    private int previewIndex;
+    private ArrayList<AbstractCard> cardsList = new ArrayList<>();
 }
 
