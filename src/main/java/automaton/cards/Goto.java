d60147eabe71a92b1464986a1599474ba0a3b013
mikemayhemdev <mikemayhemdev@gmail.com>
2022-08-21 00:42:20 -0400

    Kill that experiment

--- src/main/java/automaton/cards/Goto.java
+++ src/main/java/automaton/cards/Goto.java
@@ -1,13 +1,9 @@
 package automaton.cards;
 
-import automaton.cards.encodedcards.EncodedInsight;
 import com.megacrit.cardcrawl.actions.common.DrawCardAction;
-import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.tempCards.Insight;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.DrawCardNextTurnPower;
 
 public class Goto extends AbstractBronzeCard {
 
@@ -18,7 +14,7 @@ public class Goto extends AbstractBronzeCard {
     public Goto() {
         super(ID, 1, CardType.SKILL, CardRarity.BASIC, CardTarget.SELF);
         baseMagicNumber = magicNumber = 1;
-        cardsToPreview = new EncodedInsight();
+        cardsToPreview = new Insight();
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
