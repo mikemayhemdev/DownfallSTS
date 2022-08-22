5ff9e7377597f7cc68cf7e727c8b00b142c2e0cc
Vex Darkon <edanvirgo@gmail.com>
2022-08-21 21:52:45 -0700

    More fix

--- src/main/java/automaton/cards/Invalidate.java
+++ src/main/java/automaton/cards/Invalidate.java
@@ -6,6 +6,7 @@ import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.colorless.Blind;
 import com.megacrit.cardcrawl.cards.colorless.Trip;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.localization.LocalizedStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.VulnerablePower;
 import com.megacrit.cardcrawl.powers.WeakPower;
@@ -24,13 +25,13 @@ public class Invalidate extends AbstractBronzeCard {
 
     public void use(AbstractPlayer p, AbstractMonster m) {
         ArrayList<AbstractCard> easyCardList = new ArrayList<>();
-        easyCardList.add(new EasyModalChoiceCard(makeID("InvalidateWeak"), cardStrings.EXTENDED_DESCRIPTION[0], cardStrings.EXTENDED_DESCRIPTION[1] + magicNumber + cardStrings.EXTENDED_DESCRIPTION[2], () -> {
+        easyCardList.add(new EasyModalChoiceCard(makeID("InvalidateWeak"), cardStrings.EXTENDED_DESCRIPTION[0], cardStrings.EXTENDED_DESCRIPTION[1] + magicNumber + cardStrings.EXTENDED_DESCRIPTION[2] + (upgraded ? "+" : "") + LocalizedStrings.PERIOD, () -> {
             applyToEnemy(m, new WeakPower(m, magicNumber, false));
             AbstractCard q = new Trip();
             if (upgraded) q.upgrade();
             addCardToFunction(q);
         }));
-        easyCardList.add(new EasyModalChoiceCard(makeID("InvalidateVuln"), cardStrings.EXTENDED_DESCRIPTION[3], cardStrings.EXTENDED_DESCRIPTION[4] + magicNumber + cardStrings.EXTENDED_DESCRIPTION[5], () -> {
+        easyCardList.add(new EasyModalChoiceCard(makeID("InvalidateVuln"), cardStrings.EXTENDED_DESCRIPTION[3], cardStrings.EXTENDED_DESCRIPTION[4] + magicNumber + cardStrings.EXTENDED_DESCRIPTION[5] + (upgraded ? "+" : "")+ LocalizedStrings.PERIOD, () -> {
             applyToEnemy(m, new VulnerablePower(m, magicNumber, false));
             AbstractCard q = new Blind();
             if (upgraded) q.upgrade();
