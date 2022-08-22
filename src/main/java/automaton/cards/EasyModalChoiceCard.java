033048ce704acaec0eb78908ffd067cbab54abd5
Vex Darkon <edanvirgo@gmail.com>
2022-08-22 13:03:27 -0700

    Fix

--- src/main/java/automaton/cards/EasyModalChoiceCard.java
+++ src/main/java/automaton/cards/EasyModalChoiceCard.java
@@ -4,8 +4,9 @@ import basemod.AutoAdd;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import downfall.util.CardIgnore;
 
-@AutoAdd.Ignore
+@CardIgnore
 public class EasyModalChoiceCard extends AbstractBronzeCard {
 
     private Runnable onUseOrChosen;
