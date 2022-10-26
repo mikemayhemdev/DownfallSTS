77948ef7959c33479862a4a6a36ee109cf9fa72d
ezrarod <github@erod.33mail.com>
2022-10-26 16:09:10 -0400

    Beta Art

--- src/main/java/gremlin/cards/Kablamo.java
+++ src/main/java/gremlin/cards/Kablamo.java
@@ -8,6 +8,7 @@ import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import gremlin.GremlinMod;
 
 import static gremlin.GremlinMod.WIZARD_GREMLIN;
 
@@ -35,6 +36,7 @@ public class Kablamo extends AbstractGremlinCard {
         this.wizardry = true;
         this.tags.add(WIZARD_GREMLIN);
         setBackgrounds();
+        GremlinMod.loadJokeCardImage(this, "Kablamo.png");
     }
 
     @Override
