896e7157c9648257da13cf27e516b6668e80c505
Vex Darkon <edanvirgo@gmail.com>
2022-11-05 22:04:36 -0700

    Collect setup

--- src/main/java/collector/CollectorCollection.java
+++ src/main/java/collector/CollectorCollection.java
@@ -1,14 +1,23 @@
 package collector;
 
-import basemod.abstracts.CustomSavable;
+import collector.util.CollectionReward;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.CardGroup;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
-public class CollectorCollection implements CustomSavable<ArrayList<AbstractCard>> {
+public class CollectorCollection {
     public static CardGroup collection;
     public static CardGroup combatCollection;
+    private static HashMap<String, String> collectionPool;
+    private static ArrayList<String> collectedCardsThisCombat = new ArrayList<>();
+
+    static {
+        // TODO: Populate Collection Pool
+    }
 
     public static void init() {
         collection = new CardGroup(CardGroup.CardGroupType.UNSPECIFIED);
@@ -20,21 +29,31 @@ public class CollectorCollection implements CustomSavable<ArrayList<AbstractCard
         for (AbstractCard q : collection.group) {
             combatCollection.addToTop(q.makeSameInstanceOf());
         }
+
+        collectedCardsThisCombat.clear();
     }
 
     public static void atBattleEnd() {
         combatCollection.clear();
+
+        AbstractDungeon.getCurrRoom().rewards.add(new CollectionReward(collectedCardsThisCombat));
+        collectedCardsThisCombat.clear();
     }
 
-    @Override
-    public ArrayList<AbstractCard> onSave() {
-        return collection.group;
-    }
+    //TODO: Save/load
+//    public ArrayList<AbstractCard> onSave() {
+//        return collection.group;
+//    }
+//
+//    public void onLoad(ArrayList<AbstractCard> abstractCards) {
+//        for (AbstractCard c : abstractCards) {
+//            collection.addToTop(c);
+//        }
+//    }
 
-    @Override
-    public void onLoad(ArrayList<AbstractCard> abstractCards) {
-        for (AbstractCard c : abstractCards) {
-            collection.addToTop(c);
+    public static void collect(AbstractMonster m) {
+        if (collectionPool.containsKey(m.id)) {
+            collectedCardsThisCombat.add(collectionPool.get(m.id));
         }
     }
 }
--- src/main/java/collector/util/CollectionReward.java
+++ src/main/java/collector/util/CollectionReward.java
@@ -1,18 +1,10 @@
 package collector.util;
 
 import collector.CollectorMod;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
-import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
-import com.megacrit.cardcrawl.helpers.FontHelper;
-import com.megacrit.cardcrawl.helpers.Hitbox;
-import com.megacrit.cardcrawl.helpers.ImageMaster;
+import com.megacrit.cardcrawl.helpers.CardLibrary;
 import com.megacrit.cardcrawl.rewards.RewardItem;
-import com.megacrit.cardcrawl.vfx.AbstractGameEffect;
 
 import java.util.ArrayList;
 
@@ -20,80 +12,15 @@ public class CollectionReward extends RewardItem {
     public static final String ID = CollectorMod.makeID("CollectionReward");
     public static final String[] TEXT = CardCrawlGame.languagePack.getUIString(ID).TEXT;
 
-    private final float REWARD_TEXT_X = 833.0F * Settings.scale;
-    private final ArrayList<AbstractGameEffect> effects = new ArrayList<>();
-    public static ArrayList<AbstractCard> collectPool = new ArrayList<>();
-
-    public CollectionReward() {
-        this.hb = new Hitbox(460.0F * Settings.scale, 90.0F * Settings.scale);
-        this.flashTimer = 0.0F;
-        this.isDone = false;
-        this.ignoreReward = false;
-        this.redText = false;
+    public CollectionReward(ArrayList<String> collected) {
         this.type = RewardType.CARD;
+
         this.cards.clear();
-        cards.addAll(collectPool);
+        collected.forEach(s -> cards.add(CardLibrary.getCard(s).makeCopy()));
+
         this.text = TEXT[0];
 
-        for (AbstractCard c : this.cards) {
-            if (c.type == AbstractCard.CardType.ATTACK && AbstractDungeon.player.hasRelic("Molten Egg 2")) {
-                c.upgrade();
-            } else if (c.type == AbstractCard.CardType.SKILL && AbstractDungeon.player.hasRelic("Toxic Egg 2")) {
-                c.upgrade();
-            } else if (c.type == AbstractCard.CardType.POWER && AbstractDungeon.player.hasRelic("Frozen Egg 2")) {
-                c.upgrade();
-            }
-        }
+        AbstractDungeon.player.relics.forEach(q -> cards.forEach(c -> q.onPreviewObtainCard(c)));
     }
 
-    @Override
-    public void render(SpriteBatch sb) {
-
-
-        if (this.hb.hovered) {
-            sb.setColor(new Color(0.4F, 0.6F, 0.6F, 1.0F));
-        } else {
-            sb.setColor(new Color(0.5F, 0.6F, 0.6F, 0.8F));
-        }
-
-        if (this.hb.clickStarted) {
-            sb.draw(ImageMaster.REWARD_SCREEN_ITEM, (float) Settings.WIDTH / 2.0F - 232.0F, this.y - 49.0F, 232.0F, 49.0F, 464.0F, 98.0F, Settings.scale * 0.98F, Settings.scale * 0.98F, 0.0F, 0, 0, 464, 98, false, false);
-        } else {
-            sb.draw(ImageMaster.REWARD_SCREEN_ITEM, (float) Settings.WIDTH / 2.0F - 232.0F, this.y - 49.0F, 232.0F, 49.0F, 464.0F, 98.0F, Settings.scale, Settings.scale, 0.0F, 0, 0, 464, 98, false, false);
-        }
-
-        if (this.flashTimer != 0.0F) {
-            sb.setColor(0.6F, 1.0F, 1.0F, this.flashTimer * 1.5F);
-            sb.setBlendFunction(770, 1);
-            sb.draw(ImageMaster.REWARD_SCREEN_ITEM, (float) Settings.WIDTH / 2.0F - 232.0F, this.y - 49.0F, 232.0F, 49.0F, 464.0F, 98.0F, Settings.scale * 1.03F, Settings.scale * 1.15F, 0.0F, 0, 0, 464, 98, false, false);
-            sb.setBlendFunction(770, 771);
-        }
-
-        Texture cardImg = ImageMaster.REWARD_CARD_NORMAL;
-
-        sb.setColor(Color.WHITE);
-        sb.draw(cardImg, REWARD_ITEM_X - 32.0F, this.y - 32.0F - 2.0F * Settings.scale, 32.0F, 32.0F, 64.0F, 64.0F, Settings.scale, Settings.scale, 0.0F, 0, 0, 64, 64, false, false);
-
-
-        Color color;
-        if (this.hb.hovered) {
-            color = Settings.GOLD_COLOR;
-        } else {
-            color = Settings.CREAM_COLOR;
-        }
-
-        if (this.redText) {
-            color = Settings.RED_TEXT_COLOR;
-        }
-
-        FontHelper.renderSmartText(sb, FontHelper.cardDescFont_N, TEXT[0], REWARD_TEXT_X, this.y + 5.0F * Settings.scale, 1000.0F * Settings.scale, 0.0F, color);
-        if (!this.hb.hovered) {
-
-            for (AbstractGameEffect e : effects) {
-                e.render(sb);
-            }
-        }
-
-        this.hb.render(sb);
-    }
 }
