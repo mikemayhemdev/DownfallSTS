a16681f6f997c3790618fcfbc9a846963a88ba89
Vex Darkon <edanvirgo@gmail.com>
2022-11-14 15:00:31 -0700

    More torch progress. Ugly but the right way to do it

--- src/main/java/collector/torchhead/PetTorch.java
+++ src/main/java/collector/torchhead/PetTorch.java
@@ -1,14 +1,20 @@
 package collector.torchhead;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Interpolation;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.helpers.FontHelper;
 import com.megacrit.cardcrawl.helpers.Hitbox;
 import com.megacrit.cardcrawl.helpers.ImageMaster;
+import com.megacrit.cardcrawl.helpers.MathHelper;
 
 public class PetTorch {
+
+    private static final float WIDTH = 500F;
+    private static final float HEIGHT = 500F;
 
     private int damage;
     private int hits;
@@ -17,7 +23,11 @@ public class PetTorch {
     private int maxHp;
 
     private Hitbox hb;
+    public Hitbox healthHb;
+    public Hitbox intentHb;
+    private float hbShowTimer;
     private float healthHideTimer = 0.0F;
+    private float healthBarAnimTimer = 0.0F;
     private float healthBarWidth;
     private float targetHealthBarWidth;
     private Color hbBgColor;
@@ -34,12 +44,21 @@ public class PetTorch {
     private float blockOffset = 0.0F;
     public float hbAlpha = 0.0F;
 
-    public void update() {
+    public PetTorch() {
+        this.hbBgColor = new Color(0.0F, 0.0F, 0.0F, 0.0F);
+        this.hb = new Hitbox(WIDTH, HEIGHT);
+        this.intentHb = new Hitbox(64f * Settings.scale, 64f * Settings.scale);
+    }
 
+    public void update() {
+        this.updateHealthBar();
     }
 
     public void render(SpriteBatch sb) {
-
+        this.hb.render(sb);
+        this.intentHb.render(sb);
+        this.healthHb.render(sb);
+        renderHealth(sb);
     }
 
     public void renderHealth(SpriteBatch sb) {
@@ -126,6 +145,103 @@ public class PetTorch {
         FontHelper.renderFontCentered(sb, FontHelper.blockInfoFont, Integer.toString(AbstractDungeon.player.currentBlock), x + BLOCK_ICON_X, y - 16.0F * Settings.scale, this.blockTextColor, this.blockScale);
     }
 
+    protected void updateHealthBar() {
+        this.updateHbHoverFade();
+        this.updateHbPopInAnimation();
+        this.updateHbDamageAnimation();
+        this.updateHbAlpha();
+    }
+
+
+    public void healthBarUpdatedEvent() {
+        this.healthBarAnimTimer = 1.2F;
+        this.targetHealthBarWidth = this.hb.width * (float) this.hp / (float) this.maxHp;
+        if (this.maxHp == this.hp) {
+            this.healthBarWidth = this.targetHealthBarWidth;
+        } else if (this.hp == 0) {
+            this.healthBarWidth = 0.0F;
+            this.targetHealthBarWidth = 0.0F;
+        }
+
+        if (this.targetHealthBarWidth > this.healthBarWidth) {
+            this.healthBarWidth = this.targetHealthBarWidth;
+        }
+
+    }
+
+    private void updateHbDamageAnimation() {
+        if (this.healthBarAnimTimer > 0.0F) {
+            this.healthBarAnimTimer -= Gdx.graphics.getDeltaTime();
+        }
+
+        if (this.healthBarWidth != this.targetHealthBarWidth && this.healthBarAnimTimer <= 0.0F && this.targetHealthBarWidth < this.healthBarWidth) {
+            this.healthBarWidth = MathHelper.uiLerpSnap(this.healthBarWidth, this.targetHealthBarWidth);
+        }
+
+    }
+
+    private void updateHbHoverFade() {
+        if (this.healthHb.hovered) {
+            this.healthHideTimer -= Gdx.graphics.getDeltaTime() * 4.0F;
+            if (this.healthHideTimer < 0.2F) {
+                this.healthHideTimer = 0.2F;
+            }
+        } else {
+            this.healthHideTimer += Gdx.graphics.getDeltaTime() * 4.0F;
+            if (this.healthHideTimer > 1.0F) {
+                this.healthHideTimer = 1.0F;
+            }
+        }
+
+    }
+
+    public void healthBarRevivedEvent() {
+        this.healthBarAnimTimer = 1.2F;
+        this.targetHealthBarWidth = this.hb.width * (float) this.hp / (float) this.maxHp;
+        this.healthBarWidth = this.targetHealthBarWidth;
+        this.hbBgColor.a = 0.75F;
+        this.hbShadowColor.a = 0.5F;
+        this.hbTextColor.a = 1.0F;
+    }
+
+    private void updateHbAlpha() {
+        if (this.targetHealthBarWidth == 0.0F && this.healthBarAnimTimer <= 0.0F) {
+            this.hbShadowColor.a = MathHelper.fadeLerpSnap(this.hbShadowColor.a, 0.0F);
+            this.hbBgColor.a = MathHelper.fadeLerpSnap(this.hbBgColor.a, 0.0F);
+            this.hbTextColor.a = MathHelper.fadeLerpSnap(this.hbTextColor.a, 0.0F);
+            this.blockOutlineColor.a = MathHelper.fadeLerpSnap(this.blockOutlineColor.a, 0.0F);
+        } else {
+            this.hbBgColor.a = this.hbAlpha * 0.5F;
+            this.hbShadowColor.a = this.hbAlpha * 0.2F;
+            this.hbTextColor.a = this.hbAlpha;
+            this.orangeHbBarColor.a = this.hbAlpha;
+            this.redHbBarColor.a = this.hbAlpha;
+            this.blueHbBarColor.a = this.hbAlpha;
+            this.blockOutlineColor.a = this.hbAlpha;
+        }
+    }
+
+    public void showHealthBar() {
+        this.hbShowTimer = 0.7F;
+        this.hbAlpha = 0.0F;
+    }
+
+    private void updateHbPopInAnimation() {
+        if (this.hbShowTimer > 0.0F) {
+            this.hbShowTimer -= Gdx.graphics.getDeltaTime();
+            if (this.hbShowTimer < 0.0F) {
+                this.hbShowTimer = 0.0F;
+            }
+
+            this.hbAlpha = Interpolation.fade.apply(0.0F, 1.0F, 1.0F - this.hbShowTimer / 0.7F);
+            this.hbYOffset = Interpolation.exp10Out.apply(HB_Y_OFFSET_DIST * 5.0F, 0.0F, 1.0F - this.hbShowTimer / 0.7F);
+        }
+
+    }
+
+    public void hideHealthBar() {
+        this.hbAlpha = 0.0F;
+    }
 
     private static final float BLOCK_OFFSET_DIST = 12.0F * Settings.scale;
     private static final float HB_Y_OFFSET_DIST = 12.0F * Settings.scale;
