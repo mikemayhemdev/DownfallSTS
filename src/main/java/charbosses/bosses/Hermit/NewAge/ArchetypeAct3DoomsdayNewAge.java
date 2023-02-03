afbe7ec317dfe0e1fc36c3eff3a18293908c06f1
Vex Darkon <edanvirgo@gmail.com>
2023-02-02 18:37:16 -0700

    Bleeeeeeeeeh progress on hermit boss changes but... it's looking rough...

--- src/main/java/charbosses/bosses/Hermit/NewAge/ArchetypeAct3DoomsdayNewAge.java
+++ src/main/java/charbosses/bosses/Hermit/NewAge/ArchetypeAct3DoomsdayNewAge.java
@@ -6,14 +6,17 @@ import charbosses.cards.colorless.EnBandage;
 import charbosses.cards.colorless.EnMasterOfStrategy;
 import charbosses.cards.curses.EnClumsy;
 import charbosses.cards.curses.EnDoubt;
+import charbosses.cards.curses.EnInjury;
 import charbosses.cards.curses.EnPain;
 import charbosses.cards.hermit.*;
 import charbosses.powers.bossmechanicpowers.HermitDoomsday;
 import charbosses.relics.*;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.curses.Injury;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.powers.RitualPower;
 
 import java.util.ArrayList;
 
@@ -22,8 +25,8 @@ public class ArchetypeAct3DoomsdayNewAge extends ArchetypeBaseIronclad {
     public ArchetypeAct3DoomsdayNewAge() {
         super("HERMIT_DOOMSDAY_ARCHETYPE", "Doomsday");
 
-        maxHPModifier += 315;
-        actNum = 1;
+        maxHPModifier += 315; //TODO: High ascension bonus HP from periapt
+        actNum = 3;
         bossMechanicName = HermitDoomsday.NAME;
         bossMechanicDesc = HermitDoomsday.DESC[0];
     }
@@ -33,6 +36,7 @@ public class ArchetypeAct3DoomsdayNewAge extends ArchetypeBaseIronclad {
         super.addedPreBattle();
         AbstractCreature p = AbstractCharBoss.boss;
         AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new HermitDoomsday(p), 1));
+        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new RitualPower(p, 1, false), 1));
     }
 
 
@@ -40,12 +44,14 @@ public class ArchetypeAct3DoomsdayNewAge extends ArchetypeBaseIronclad {
 
         /////   RELICS   /////
         addRelic(new CBR_NeowsBlessing());
-        addRelic(new CBR_CallingBell());
-        addRelic(new CBR_Omamori());
-        addRelic(new CBR_ArtOfWar());
-        addRelic(new CBR_MaskGremlin());
+        addRelic(new CBR_CursedKey());
         addRelic(new CBR_CharredGlove());
+        addRelic(new CBR_BrassTacks()); //TODO: En Brass Tacks because Red Scarf will be mega annoying
+        addRelic(new CBR_Omamori()); //TODO: Make sure it's 0
     }
+
+    //TODO: Grudge counter that goes up over time? Or check how Malice is working. Oh no, Malice exhausts doubt
+    //TODO:
 
     @Override
     public ArrayList<AbstractCard> getThisTurnCards() {
@@ -56,47 +62,21 @@ public class ArchetypeAct3DoomsdayNewAge extends ArchetypeBaseIronclad {
         if (!looped) {
             switch (turn) {
                 case 0:
-                    addToList(cardsList, new EnCovet(), true);
-                    addToList(cardsList, new EnDefendHermit());
-                    addToList(cardsList, new EnPain());
-                    addToList(cardsList, new EnClumsy());
-                    addToList(cardsList, new EnSpite());
+                    addToList(cardsList, new EnShadowCloak());
+                    addToList(cardsList, new EnGrudge());
+                    addToList(cardsList, new EnGlare());
                     turn++;
                     break;
                 case 1:
-                    addToList(cardsList, new EnMisfire(), extraUpgrades);
-                    addToList(cardsList, new EnGlare());
-                    addToList(cardsList, new EnGhostlyPresence());
-                    turn++;
-                    break;
-                case 2:
-                    addToList(cardsList, new EnMisfire());
-                    addToList(cardsList, new EnMasterOfStrategy());
-                    addToList(cardsList, new EnLoneWolf());
-                    addToList(cardsList, new EnBandage());
-                    AbstractCard wide = new EnWideOpen();
-                    wide.modifyCostForCombat(-99);
-                    addToList(cardsList, wide);
-                    addToList(cardsList, new EnMalice());
-                    turn++;
-                    break;
-                case 3:
-                    addToList(cardsList, new EnSprayNPray());
-                    addToList(cardsList, new EnEnervate());
-                    addToList(cardsList, new EnLowProfile());
-                    addToList(cardsList, new EnGlare());
-                    turn++;
-                    break;
-                case 4:
-                    addToList(cardsList, new EnClumsy());
-                    addToList(cardsList, new EnClumsy());
+                    addToList(cardsList, new EnInjury());
+                    addToList(cardsList, new EnInjury());
                     addToList(cardsList, new EnDoubt());
                     turn++;
                     break;
-                case 5:
+                case 2:
                     addToList(cardsList, new EnPurgatory());
-                    addToList(cardsList, new EnGrudge());
-                    addToList(cardsList, new EnGrudge());
+                    addToList(cardsList, new EnMalice());
+                    addToList(cardsList, new EnSprayNPray());
                     turn = 0;
                     looped = true;
                     break;
@@ -104,43 +84,21 @@ public class ArchetypeAct3DoomsdayNewAge extends ArchetypeBaseIronclad {
         } else {
             switch (turn) {
                 case 0:
-                    AbstractCard q = new EnDoubt();
-                    AbstractCard m = new EnMalice();
-                    ((EnMalice) m).setExhaust(q);
-                    addToList(cardsList, m);
-                    addToList(cardsList, q);
-                    AbstractCard wide = new EnWideOpen();
-                    wide.modifyCostForCombat(-99);
-                    addToList(cardsList, wide);
-                    turn++;
+                    addToList(cardsList, new EnInjury());
+                    addToList(cardsList, new EnGrudge());
+                    addToList(cardsList, new EnSprayNPray());
                     break;
                 case 1:
-                    addToList(cardsList, new EnMisfire(), extraUpgrades);
-                    addToList(cardsList, new EnSpite());
-                    addToList(cardsList, new EnClumsy());
-                    addToList(cardsList, new EnPain());
-                    addToList(cardsList, new EnGrudge(12));
+                    addToList(cardsList, new EnMalice());
+                    addToList(cardsList, new EnGlare());
+                    addToList(cardsList, new EnDoubt());
                     turn++;
                     break;
                 case 2:
-                    addToList(cardsList, new EnSprayNPray());
-                    addToList(cardsList, new EnLowProfile());
-                    addToList(cardsList, new EnMisfire());
-                    turn++;
-                    break;
-                case 3:
-                    addToList(cardsList, new EnCovet(), true);
-                    addToList(cardsList, new EnDefendHermit());
-                    addToList(cardsList, new EnGlare());
-                    addToList(cardsList, new EnGlare());
-                    addToList(cardsList, new EnGhostlyPresence());
-                    turn++;
-                    break;
-                case 4:
                     addToList(cardsList, new EnPurgatory());
-                    addToList(cardsList, new EnEnervate());
-                    addToList(cardsList, new EnGrudge(12));
-                    turn = 0;
+                    addToList(cardsList, new EnDoubt());
+                    addToList(cardsList, new EnInjury());
+                    turn=0;
                     break;
             }
         }
@@ -149,6 +107,6 @@ public class ArchetypeAct3DoomsdayNewAge extends ArchetypeBaseIronclad {
 
     @Override
     public void initializeBonusRelic() {
-        addRelic(new CBR_Orichalcum());
+        addRelic(new CBR_DarkstonePeriapt()); //TODO: En darkstone periapt (no need for functionality)
     }
 }
