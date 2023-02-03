641796d69df90006c2859cecf6e8cfcecab99660
ezrarod <github@erod.33mail.com>
2023-02-03 08:21:18 -0500

    Art

--- src/main/java/gremlin/cards/Encore.java
+++ src/main/java/gremlin/cards/Encore.java
@@ -7,6 +7,7 @@ import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import gremlin.GremlinMod;
 import gremlin.powers.EncorePower;
 import gremlin.powers.WizPower;
 
@@ -37,6 +38,7 @@ public class Encore extends AbstractGremlinCard {
         this.magicNumber = baseMagicNumber;
         this.tags.add(WIZARD_GREMLIN);
         setBackgrounds();
+        GremlinMod.loadJokeCardImage(this, "Encore.png");
     }
 
     public void use(AbstractPlayer p, AbstractMonster m)
