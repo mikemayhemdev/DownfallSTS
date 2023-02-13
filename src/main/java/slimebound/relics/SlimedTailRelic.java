51c86661f96177309b7100dab191aeed08711339
Stanek-K <jgdgkot@gmail.com>
2023-02-13 17:32:24 +0100

    Slimeboss relics
    -Stone of Nomakk changed: now a common Relic, no loner Splits into a Mire Slime.
    -Slimed Tail reworked: now reads "Whenever you Split, gain 3 Block."
    -Jeremiah's Banner changed: now splits into a random Slime instead of Leeching one.

--- src/main/java/slimebound/relics/SlimedTailRelic.java
+++ src/main/java/slimebound/relics/SlimedTailRelic.java
@@ -2,25 +2,29 @@ package slimebound.relics;
 
 import basemod.abstracts.CustomRelic;
 import com.badlogic.gdx.graphics.Texture;
+import com.evacipated.cardcrawl.mod.stslib.relics.OnChannelRelic;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
 import com.megacrit.cardcrawl.actions.common.RelicAboveCreatureAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.helpers.ImageMaster;
 import com.megacrit.cardcrawl.helpers.PowerTip;
+import com.megacrit.cardcrawl.orbs.AbstractOrb;
 import com.megacrit.cardcrawl.relics.AbstractRelic;
 import com.megacrit.cardcrawl.rooms.AbstractRoom;
 import slimebound.actions.SlimeSpawnAction;
 import slimebound.characters.SlimeboundCharacter;
+import slimebound.orbs.SpawnedSlime;
 import slimebound.powers.SlimeSacrificePower;
 
-public class SlimedTailRelic extends CustomRelic {
+public class SlimedTailRelic extends CustomRelic implements OnChannelRelic {
     public static final String ID = "Slimebound:SlimedTailRelic";
     public static final String IMG_PATH = "relics/slimedTail.png";
     public static final String IMG_PATH_LARGE = "relics/slimedTailLarge.png";
     public static final String OUTLINE_IMG_PATH = "relics/slimedTailOutline.png";
-    private boolean isActive = false;
+    private static final int BLOCK_PER_TRIGGER = 3;
 
     public SlimedTailRelic() {
         super(ID, new Texture(slimebound.SlimeboundMod.getResourcePath(IMG_PATH)), new Texture(slimebound.SlimeboundMod.getResourcePath(OUTLINE_IMG_PATH)),
@@ -31,62 +35,13 @@ public class SlimedTailRelic extends CustomRelic {
 
     @Override
     public String getUpdatedDescription() {
-        return this.DESCRIPTIONS[0];
+        return this.DESCRIPTIONS[0] + BLOCK_PER_TRIGGER + this.DESCRIPTIONS[1];
     }
 
-    public void atBattleStart() {
-        this.isActive = true;
-        this.grayscale = false;
-        AbstractDungeon.actionManager.addToBottom(new AbstractGameAction() {
-            public void update() {
-                if (SlimedTailRelic.this.isActive && AbstractDungeon.player.isBloodied) {
-                    activate();
-
-                }
-
-                this.isDone = true;
-            }
-        });
-    }
-
-    public void activate() {
-        AbstractPlayer p = AbstractDungeon.player;
-        flash();
-
-        AbstractDungeon.actionManager.addToTop(new SlimeSpawnAction(new slimebound.orbs.PoisonSlime(true), false, false));
-
-        AbstractDungeon.actionManager.addToTop(new RelicAboveCreatureAction(p, this));
-        AbstractDungeon.actionManager.addToTop(new ApplyPowerAction(p, p, new SlimeSacrificePower(AbstractDungeon.player, 1), 1, true));
-
-        this.isActive = false;
-        AbstractDungeon.player.hand.applyPowers();
-        this.grayscale = true;
-
-        this.usedUp = true;
-        this.description = this.DESCRIPTIONS[1];
-        this.tips.clear();
-        this.tips.add(new PowerTip(this.name, this.description));
-        this.initializeTips();
-    }
-
-    public void onBloodied() {
-        if (this.isActive && AbstractDungeon.getCurrRoom().phase == AbstractRoom.RoomPhase.COMBAT) {
-            activate();
-        }
-
-    }
-
-
-    public void onVictory() {
-        if (!isActive) {
-            this.grayscale = false;
-            this.isActive = true;
-            this.usedUp = false;
-            this.description = this.getUpdatedDescription();
-
-            this.tips.clear();
-            this.tips.add(new PowerTip(this.name, this.description));
-            this.initializeTips();
+    @Override
+    public void onChannel(AbstractOrb abstractOrb) {
+        if (abstractOrb instanceof SpawnedSlime) {
+            addToBot(new GainBlockAction(AbstractDungeon.player, BLOCK_PER_TRIGGER));
         }
     }
 
