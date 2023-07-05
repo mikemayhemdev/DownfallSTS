60d6dc086391107540831dd76a3edb4f835e1f60
mikemayhemdev <mikemayhemdev@gmail.com>
2023-07-05 17:22:20 -0400

    Trying out You Are Mine, starter card edition

--- src/main/java/collector/cards/YouAreMine.java
+++ src/main/java/collector/cards/YouAreMine.java
@@ -1,6 +1,7 @@
 package collector.cards;
 
 import collector.powers.DoomPower;
+import com.megacrit.cardcrawl.actions.animations.VFXAction;
 import com.megacrit.cardcrawl.actions.common.GainBlockAction;
 import com.megacrit.cardcrawl.actions.common.RemoveAllBlockAction;
 import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
@@ -10,6 +11,7 @@ import com.megacrit.cardcrawl.powers.ArtifactPower;
 import com.megacrit.cardcrawl.powers.StrengthPower;
 import com.megacrit.cardcrawl.powers.VulnerablePower;
 import com.megacrit.cardcrawl.powers.WeakPower;
+import com.megacrit.cardcrawl.vfx.CollectorCurseEffect;
 
 import static collector.CollectorMod.makeID;
 import static collector.util.Wiz.*;
@@ -19,21 +21,19 @@ public class YouAreMine extends AbstractCollectorCard {
     // intellij stuff skill, enemy, rare, , , , , 4, 2
 
     public YouAreMine() {
-        super(ID, 3, CardType.SKILL, CardRarity.RARE, CardTarget.ENEMY);
-        baseMagicNumber = magicNumber = 3;
-        baseSecondMagic = secondMagic = 12;
-        exhaust = true;
+        super(ID, 3, CardType.SKILL, CardRarity.BASIC, CardTarget.ENEMY);
+        baseMagicNumber = magicNumber = 2;
+        baseSecondMagic = secondMagic = 15;
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
+        atb(new VFXAction(new CollectorCurseEffect(m.hb.cX, m.hb.cY), .2F));
         applyToEnemy(m, new WeakPower(m, magicNumber, false));
         applyToEnemy(m, new VulnerablePower(m, magicNumber, false));
-        applyToEnemy(m, new StrengthPower(m, -magicNumber));
         applyToEnemy(m, new DoomPower(m, secondMagic));
     }
 
     public void upp() {
-        upgradeMagicNumber(1);
-        upgradeSecondMagic(4);
+        upgradeSecondMagic(5);
     }
 }
