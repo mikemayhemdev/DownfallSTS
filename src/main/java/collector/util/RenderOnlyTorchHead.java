415bdc24406f90c1650c5ab1ad5c7f270bc113a9
Vex <edanvirgo@gmail.com>
2023-07-06 20:48:21 -0700

    Sentry nerf, Torchhead summon has fire, many other fixes

--- src/main/java/collector/util/RenderOnlyTorchHead.java
+++ src/main/java/collector/util/RenderOnlyTorchHead.java
@@ -1,13 +1,19 @@
 package collector.util;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.city.TorchHead;
+import com.megacrit.cardcrawl.vfx.TorchHeadFireEffect;
 
 public class RenderOnlyTorchHead extends TorchHead {
     public RenderOnlyTorchHead() {
         super(0, 0);
         flipHorizontal = true;
     }
+
+    private float fireTimer = 0.0F;
 
     @Override
     public void renderHealth(SpriteBatch sb) {
@@ -19,6 +25,16 @@ public class RenderOnlyTorchHead extends TorchHead {
 
     }
 
+    @Override
+    public void update() {
+        if (!this.isDying) {
+            this.fireTimer -= Gdx.graphics.getDeltaTime();
+            if (this.fireTimer < 0.0F) {
+                this.fireTimer = 0.04F;
+                AbstractDungeon.effectList.add(new TorchHeadFireEffect(this.skeleton.getX() + this.skeleton.findBone("fireslot").getX() + 10.0F * Settings.scale, this.skeleton.getY() + this.skeleton.findBone("fireslot").getY() + 110.0F * Settings.scale));
+            }
+        }
+    }
 
     //TODO: More render hijinx
 
