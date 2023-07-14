623f14b3fb6f795fc86abfdf842241dbf6615d48
Vex <edanvirgo@gmail.com>
2023-07-14 10:35:20 -0700

    Some minor fixes

--- src/main/java/collector/cards/LanternFlare.java
+++ src/main/java/collector/cards/LanternFlare.java
@@ -8,30 +8,22 @@ import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 import static collector.CollectorMod.makeID;
-import static collector.util.Wiz.applyToEnemyTop;
-import static collector.util.Wiz.atb;
+import static collector.util.Wiz.*;
 
 public class LanternFlare extends AbstractCollectorCard {
     public final static String ID = makeID(LanternFlare.class.getSimpleName());
     // intellij stuff attack, all_enemy, uncommon, 12, 3, , , 12, 3
 
     public LanternFlare() {
-        super(ID, 2, CardType.ATTACK, CardRarity.UNCOMMON, CardTarget.ALL_ENEMY);
+        super(ID, 2, CardType.ATTACK, CardRarity.UNCOMMON, CardTarget.ENEMY);
         baseMagicNumber = magicNumber = 12;
         baseSecondMagic = secondMagic = 4;
         isPyre();
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
-        atb(new AbstractGameAction() {
-            @Override
-            public void update() {
-                isDone = true;
-                AbstractMonster q = AbstractDungeon.getRandomMonster();
-                applyToEnemyTop(q, new LanternFlarePower(q, secondMagic));
-                applyToEnemyTop(q, new DoomPower(q, magicNumber));
-            }
-        });
+        applyToEnemy(m, new DoomPower(m, magicNumber));
+        applyToEnemy(m, new LanternFlarePower(m, secondMagic));
     }
 
     public void upp() {
