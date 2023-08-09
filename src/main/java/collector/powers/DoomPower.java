f8c4315e27cfadece5048f2ae67b12ba55cd5adf
Vex Darkon <edanvirgo@gmail.com>
2023-08-08 22:00:08 -0500

    Change color of reserve spend cards, maybe demise fix

--- src/main/java/collector/powers/DoomPower.java
+++ src/main/java/collector/powers/DoomPower.java
@@ -3,18 +3,12 @@ package collector.powers;
 import com.badlogic.gdx.graphics.Color;
 import com.evacipated.cardcrawl.mod.stslib.powers.interfaces.HealthBarRenderPower;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.actions.animations.VFXAction;
 import com.megacrit.cardcrawl.actions.common.LoseHPAction;
 import com.megacrit.cardcrawl.actions.common.ReducePowerAction;
 import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.AbstractPower;
 import com.megacrit.cardcrawl.rooms.AbstractRoom;
-import theHexaghost.powers.BurnPerTurnPower;
-import theHexaghost.powers.LivingBombPower;
-import theHexaghost.relics.SoulConsumer;
-import theHexaghost.vfx.ExplosionSmallEffectGreen;
 
 import static collector.util.Wiz.atb;
 import static collector.util.Wiz.isAfflicted;
@@ -34,7 +28,7 @@ public class DoomPower extends AbstractCollectorPower implements HealthBarRender
 
     @Override
     public int getHealthBarAmount() {
-            return amount;
+        return amount;
     }
 
     public void atStartOfTurn() {
@@ -43,22 +37,21 @@ public class DoomPower extends AbstractCollectorPower implements HealthBarRender
         }
     }// 70
 
-    public void explode(){
+    public void explode() {
         this.flashWithoutSound();
 
-        if (isAfflicted((AbstractMonster)this.owner)){
+        if (isAfflicted((AbstractMonster) this.owner)) {
 
         } else {
 
-            if (this.owner.hasPower(DemisePower.POWER_ID)){
-                atb(new ReducePowerAction(this.owner, this.owner, this.owner.getPower(DemisePower.POWER_ID), 1));
+            if (this.owner.hasPower(DemisePower.POWER_ID)) {
+                atb(new ReducePowerAction(this.owner, this.owner, this, 1));
             } else {
                 this.addToBot(new RemoveSpecificPowerAction(this.owner, this.owner, this));
             }
         }
 
         this.addToBot(new LoseHPAction(owner, owner, amount, AbstractGameAction.AttackEffect.FIRE));
-
 
 
     }
