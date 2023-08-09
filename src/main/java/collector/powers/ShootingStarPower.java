06e01daf3f07b032ba4b9b532723c3a4966da4c9
Vex Darkon <edanvirgo@gmail.com>
2023-08-08 23:28:25 -0500

    Shooting Star Fixes

--- src/main/java/collector/powers/ShootingStarPower.java
+++ src/main/java/collector/powers/ShootingStarPower.java
@@ -6,7 +6,7 @@ import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 
 public class ShootingStarPower extends AbstractCollectorPower implements OnPyrePower {
-    public static final String NAME = "ShootingStarPower";
+    public static final String NAME = "ShootingStar";
     public static final String POWER_ID = makeID(NAME);
     public static final PowerType TYPE = PowerType.BUFF;
     public static final boolean TURN_BASED = false;
