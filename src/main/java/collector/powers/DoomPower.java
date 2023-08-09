6293751de9ab0f98623789329b3907dd00863783
mikemayhemdev <mikemayhemdev@gmail.com>
2023-08-08 23:34:41 -0400

    Art update

--- src/main/java/collector/powers/DoomPower.java
+++ src/main/java/collector/powers/DoomPower.java
@@ -57,7 +57,7 @@ public class DoomPower extends AbstractCollectorPower implements HealthBarRender
             }
         }
 
-        this.addToBot(new LoseHPAction(owner, owner, amount, AbstractGameAction.AttackEffect.FIRE));
+        this.addToBot(new LoseHPAction(owner, owner, amount, AbstractGameAction.AttackEffect.NONE));
 
 
 
