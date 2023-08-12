9923828ecfa37f14ad5e06d7cc75c9a932c54394
Vex <edanvirgo@gmail.com>
2023-08-11 19:01:44 -0700

    Tutorial setup

--- src/main/java/downfall/actions/MessageCaller.java
+++ src/main/java/downfall/actions/MessageCaller.java
@@ -5,6 +5,7 @@ import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import downfall.downfallMod;
 import downfall.tutorials.CharbossTutorials;
+import downfall.tutorials.CollectorTutorials;
 import downfall.tutorials.GuardianTutorials;
 import downfall.tutorials.HexaghostTutorials;
 import hermit.util.HermitTutorials;
@@ -40,6 +41,9 @@ public class MessageCaller extends AbstractGameAction {
                     break;
                 case 3:
                     AbstractDungeon.ftue = new CharbossTutorials();
+                    break;
+                case 4:
+                    AbstractDungeon.ftue = new CollectorTutorials();
                     break;
             }
             downfallMod.unseenTutorials[code] = false;
