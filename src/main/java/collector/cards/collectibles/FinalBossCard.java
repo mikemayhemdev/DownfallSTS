419b2b1112b8dcac0ee4a938a783a99098389f8b
Mwallx <89969909+Mwallx@users.noreply.github.com>
2023-08-13 13:28:57 +0800

    Update FinalBossCard.java

--- src/main/java/collector/cards/collectibles/FinalBossCard.java
+++ src/main/java/collector/cards/collectibles/FinalBossCard.java
@@ -14,7 +14,7 @@ public class FinalBossCard extends AbstractCollectibleCard {
     // intellij stuff skill, enemy, rare, , , , , , 
 
     public FinalBossCard() {
-        super(ID, 4, CardType.SKILL, CardRarity.RARE, CardTarget.ENEMY);
+        super(ID, 6, CardType.SKILL, CardRarity.RARE, CardTarget.ENEMY);
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
@@ -23,6 +23,6 @@ public class FinalBossCard extends AbstractCollectibleCard {
     }
 
     public void upp() {
-        upgradeBaseCost(3);
+        upgradeBaseCost(4);
     }
 }
