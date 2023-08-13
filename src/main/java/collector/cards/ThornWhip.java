68f32bad26b82d828a6020bc9611d950e897bc8e
Vex <edanvirgo@gmail.com>
2023-08-12 17:09:21 -0700

    Thorn Whip change

--- src/main/java/collector/cards/ThornWhip.java
+++ src/main/java/collector/cards/ThornWhip.java
@@ -1,45 +1,32 @@
 package collector.cards;
 
+import collector.util.EssenceSystem;
+import com.evacipated.cardcrawl.mod.stslib.cards.interfaces.OnObtainCard;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.VulnerablePower;
-import com.megacrit.cardcrawl.powers.WeakPower;
 
 import static collector.CollectorMod.makeID;
-import static collector.util.Wiz.getEnemies;
-import static collector.util.Wiz.isAfflicted;
 
-public class ThornWhip extends AbstractCollectorCard {
+public class ThornWhip extends AbstractCollectorCard implements OnObtainCard {
     public final static String ID = makeID(ThornWhip.class.getSimpleName());
     // intellij stuff attack, enemy, rare, 11, 1, , , , 
 
     public ThornWhip() {
         super(ID, 1, CardType.ATTACK, CardRarity.RARE, CardTarget.ENEMY);
-        baseDamage = 10;
+        baseDamage = 12;
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
         dmg(m, AbstractGameAction.AttackEffect.SLASH_HORIZONTAL);
-        if (isAfflicted(m)) {
-            dmg(m, AbstractGameAction.AttackEffect.SLASH_VERTICAL);
-            dmg(m, AbstractGameAction.AttackEffect.SLASH_VERTICAL);
-        }
     }
 
     public void upp() {
-        upgradeDamage(3);
+        upgradeDamage(5);
     }
 
     @Override
-    public void triggerOnGlowCheck() {
-        for (AbstractMonster m : getEnemies()) {
-            if (m.hasPower(VulnerablePower.POWER_ID) && m.hasPower(WeakPower.POWER_ID)) {
-                this.glowColor = AbstractCard.GOLD_BORDER_GLOW_COLOR;
-                return;
-            }
-        }
-        this.glowColor = AbstractCard.BLUE_BORDER_GLOW_COLOR;
+    public void onObtainCard() {
+        EssenceSystem.changeEssence(9);
     }
 }
