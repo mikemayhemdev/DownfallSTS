fa9c757fa1c60020cb087cb3c05823380f06a444
Vex <edanvirgo@gmail.com>
2023-10-04 21:30:32 -0700

    Soulforge change

--- src/main/java/collector/cards/Soulforge.java
+++ src/main/java/collector/cards/Soulforge.java
@@ -1,12 +1,16 @@
-
 package collector.cards;
 
-import collector.actions.DrawAndRemoveMegatherealFromCollectedCardAction;
+import collector.actions.DrawCardFromCollectionAction;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInHandAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 import static collector.CollectorMod.makeID;
 import static collector.util.Wiz.atb;
+import static collector.util.Wiz.att;
 
 public class Soulforge extends AbstractCollectorCard {
     public final static String ID = makeID(Soulforge.class.getSimpleName());
@@ -20,7 +24,20 @@ public class Soulforge extends AbstractCollectorCard {
 
     public void use(AbstractPlayer p, AbstractMonster m) {
         for (int i = 0; i < magicNumber; i++) {
-            atb(new DrawAndRemoveMegatherealFromCollectedCardAction());
+            atb(new DrawCardFromCollectionAction());
+            atb(new AbstractGameAction() {
+                @Override
+                public void update() {
+                    isDone = true;
+                    for (AbstractCard q : DrawCardAction.drawnCards) {
+                        AbstractCard dupe = q.makeStatEquivalentCopy();
+                        if (dupe.canUpgrade()) {
+                            dupe.upgrade();
+                        }
+                        att(new MakeTempCardInHandAction(dupe, 1, true));
+                    }
+                }
+            });
         }
     }
 
