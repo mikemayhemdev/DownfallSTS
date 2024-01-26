ea636c804c7856b6573360f23eb012203729707c
JamesTripleQ <90627380+JamesTripleQ@users.noreply.github.com>
2024-01-26 23:57:12 +0100

    Add files via upload

--- src/main/java/gremlin/potions/SwapPotion.java
+++ src/main/java/gremlin/potions/SwapPotion.java
@@ -1,10 +1,12 @@
 package gremlin.potions;
 
+import basemod.BaseMod;
 import basemod.abstracts.CustomPotion;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.helpers.PowerTip;
+import com.megacrit.cardcrawl.helpers.TipHelper;
 import com.megacrit.cardcrawl.localization.PotionStrings;
 import gremlin.GremlinMod;
 import gremlin.actions.RandomGremlinSwapAction;
@@ -27,6 +29,7 @@ public class SwapPotion extends CustomPotion {
         description = DESCRIPTIONS[0];
         this.tips.clear();
         this.tips.add(new PowerTip(this.name, this.description));
+        this.tips.add(new PowerTip(TipHelper.capitalize(BaseMod.getKeywordTitle("gremlin:swap")), BaseMod.getKeywordDescription("gremlin:swap")));
     }
 
     public void use(AbstractCreature target) {
