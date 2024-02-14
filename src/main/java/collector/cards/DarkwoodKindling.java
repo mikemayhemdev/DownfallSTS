f6454634e4d84a57602d71ffd3a3a179aa7d1b0e
mikemayhemdev <45570650+mikemayhemdev@users.noreply.github.com>
2024-02-14 15:45:59 -0500

    Darkwood next turn

--- src/main/java/collector/cards/DarkwoodKindling.java
+++ src/main/java/collector/cards/DarkwoodKindling.java
@@ -2,6 +2,7 @@ package collector.cards;
 
 import collector.actions.GainReservesAction;
 import collector.powers.DoomPower;
+import collector.powers.NextTurnReservePower;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
@@ -10,6 +11,7 @@ import hermit.util.Wiz;
 
 import static collector.CollectorMod.makeID;
 import static collector.util.Wiz.applyToEnemy;
+import static collector.util.Wiz.applyToSelf;
 
 public class DarkwoodKindling extends AbstractCollectorCard {
     public final static String ID = makeID(DarkwoodKindling.class.getSimpleName());
@@ -17,7 +19,7 @@ public class DarkwoodKindling extends AbstractCollectorCard {
 
     public DarkwoodKindling() {
         super(ID, -2, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.NONE);
-        baseMagicNumber = magicNumber = 8;
+        baseMagicNumber = magicNumber = 2;
         tags.add(expansionContentMod.UNPLAYABLE);
     }
 
@@ -34,7 +36,7 @@ public class DarkwoodKindling extends AbstractCollectorCard {
     public void triggerOnExhaust() {
         CardCrawlGame.sound.play("CARD_EXHAUST", 0.2F);
         CardCrawlGame.sound.play("CARD_EXHAUST", 0.2F);
-        addToBot(new GainReservesAction(2));
+        applyToSelf(new NextTurnReservePower(magicNumber));
     }
 
     public void upp() {
