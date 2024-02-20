a7c2a7e0786c78cc0da03a84c8005319fcfcad79
Mwallx <89969909+Mwallx@users.noreply.github.com>
2024-02-21 00:57:46 +0800

    Update PrismaticTorch.java

--- src/main/java/collector/relics/PrismaticTorch.java
+++ src/main/java/collector/relics/PrismaticTorch.java
@@ -1,89 +1,267 @@
-package collector.relics;
+package downfall.events;
 
-import basemod.abstracts.CustomRelic;
-import basemod.helpers.CardPowerTip;
-import collector.CollectorMod;
-import collector.actions.GainReservesAction;
-import collector.cards.Ember;
+
+import automaton.AutomatonChar;
+import basemod.ReflectionHacks;
+import champ.ChampChar;
+import collector.CollectorChar;
+import com.badlogic.gdx.math.MathUtils;
+import com.evacipated.cardcrawl.modthespire.Loader;
 import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.curses.Doubt;
+import com.megacrit.cardcrawl.cards.curses.Normality;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
-import downfall.util.TextureLoader;
+import com.megacrit.cardcrawl.events.AbstractImageEvent;
+import com.megacrit.cardcrawl.helpers.CardLibrary;
+import com.megacrit.cardcrawl.helpers.MonsterHelper;
+import com.megacrit.cardcrawl.localization.EventStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.monsters.MonsterGroup;
+import com.megacrit.cardcrawl.monsters.city.BronzeAutomaton;
+import com.megacrit.cardcrawl.monsters.city.Champ;
+import com.megacrit.cardcrawl.monsters.city.GremlinLeader;
+import com.megacrit.cardcrawl.monsters.city.TheCollector;
+import com.megacrit.cardcrawl.powers.StrengthPower;
+import com.megacrit.cardcrawl.relics.AbstractRelic.RelicTier;
+import com.megacrit.cardcrawl.vfx.RainingGoldEffect;
+import com.megacrit.cardcrawl.vfx.UpgradeShineEffect;
+import com.megacrit.cardcrawl.vfx.cardManip.ShowCardAndObtainEffect;
+import com.megacrit.cardcrawl.vfx.cardManip.ShowCardBrieflyEffect;
+import downfall.downfallMod;
+import downfall.monsters.GremlinMirror;
+import downfall.monsters.SneckoMirror;
+import downfall.patches.ui.campfire.AddBustKeyButtonPatches;
+import downfall.relics.HeartBlessingBlue;
+import downfall.relics.HeartBlessingGreen;
+import downfall.relics.HeartBlessingRed;
+import gremlin.characters.GremlinCharacter;
+import guardian.characters.GuardianCharacter;
+import slimebound.characters.SlimeboundCharacter;
+import sneckomod.TheSnecko;
+import theHexaghost.TheHexaghost;
 
-import static collector.util.Wiz.atb;
-import static collector.util.Wiz.makeInHand;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
 
-public class PrismaticTorch extends CustomRelic {
-    public static final String ID = CollectorMod.makeID(PrismaticTorch.class.getSimpleName());
-    private static final String IMG_PATH = PrismaticTorch.class.getSimpleName() + ".png";
-    private static final String OUTLINE_IMG_PATH = PrismaticTorch.class.getSimpleName() + ".png";
+import static com.megacrit.cardcrawl.dungeons.AbstractDungeon.lastCombatMetricKey;
+import static com.megacrit.cardcrawl.helpers.MonsterHelper.getGremlin;
 
-    private static final int EMBER_COUNT = 1;
+public class MindBloom_Evil extends AbstractImageEvent {
+    public static final String ID = "downfall:MindBloom";
+    private static final EventStrings eventStrings;
+    public static final String NAME;
+    public static final String[] DESCRIPTIONS;
+    public static final String[] OPTIONS;
+    public static final String[] DESCRIPTIONSALT;
+    public static final String[] OPTIONSALT;
+    private CurScreen screen;
 
-    public PrismaticTorch() {
-        super(ID, TextureLoader.getTexture(CollectorMod.makeRelicPath(IMG_PATH)), TextureLoader.getTexture(CollectorMod.makeRelicOutlinePath(OUTLINE_IMG_PATH)), RelicTier.BOSS, LandingSound.MAGICAL);
-        this.tips.add(new CardPowerTip(new Ember()));
-    }
+    public MindBloom_Evil() {
+        super(NAME, DESCRIPTIONSALT[0], "images/events/mindBloom.jpg");
+        this.screen = CurScreen.INTRO;
 
-    @Override
-    public void atBattleStart() {
-        flash();
-        makeInHand(new Ember(), EMBER_COUNT);
-        atb(new GainReservesAction(1));
-    }
-
-    @Override
-    public void atTurnStart() {
-        grayscale=false;
-    }
-
-    @Override
-    public void onExhaust(AbstractCard card) {
-        if (!grayscale) {
-            if (card.cardID.equals(Ember.ID)) {
-                flash();
-                atb(new GainReservesAction(1));
-                grayscale=true;
-            }
+        if (downfallMod.isDownfallCharacter(AbstractDungeon.player)) {
+            if (Loader.isModLoaded("DownfallExtension"))
+                this.imageEventText.setDialogOption(CardCrawlGame.languagePack.getEventString("DownfallExtension:MindBloom").OPTIONS[0]);
+            else
+                this.imageEventText.setDialogOption(OPTIONSALT[0]);
+        } else {
+            this.imageEventText.setDialogOption(OPTIONS[0]);   //original Mind Bloom if you are not a villain
         }
+
+        if (AddBustKeyButtonPatches.KeyFields.bustedRuby.get(AbstractDungeon.player) && AddBustKeyButtonPatches.KeyFields.bustedEmerald.get(AbstractDungeon.player) && AddBustKeyButtonPatches.KeyFields.bustedSapphire.get(AbstractDungeon.player)) {
+            if (AbstractDungeon.player instanceof GremlinCharacter) {
+                this.imageEventText.setDialogOption(OPTIONSALT[5]);
+            } else {
+                this.imageEventText.setDialogOption(OPTIONSALT[2]);
+            }
+        } else {
+            this.imageEventText.setDialogOption(OPTIONSALT[3], true);
+        }
+
+        if (AbstractDungeon.floorNum % 50 <= 40) {
+            this.imageEventText.setDialogOption(OPTIONSALT[1], CardLibrary.getCopy("Normality"));
+        } else {
+            this.imageEventText.setDialogOption(OPTIONS[2], CardLibrary.getCopy("Doubt"));
+        }
+
     }
 
-    @Override
-    public boolean canSpawn() {
-        return AbstractDungeon.player.hasRelic(EmeraldTorch.ID);
-    }
+    protected void buttonEffect(int buttonPressed) {
+        switch (this.screen) {
+            case INTRO:
+                switch (buttonPressed) {
+                    case 0:
+                        this.imageEventText.updateBodyText(DESCRIPTIONSALT[1]);
+                        this.screen = CurScreen.FIGHT;
 
-    @Override
-    public void obtain() {
-        if (AbstractDungeon.player.hasRelic(EmeraldTorch.ID)) {
-            for (int i = 0; i < AbstractDungeon.player.relics.size(); ++i) {
-                if (AbstractDungeon.player.relics.get(i).relicId.equals(EmeraldTorch.ID)) {
-                    instantObtain(AbstractDungeon.player, i, true);
-                    break;
+                        if (AbstractDungeon.player instanceof SlimeboundCharacter) {
+                            AbstractDungeon.getCurrRoom().monsters = MonsterHelper.getEncounter("Slime Boss");
+                        } else if (AbstractDungeon.player instanceof GuardianCharacter) {
+                            if (Loader.isModLoaded("DownfallExtension"))
+                                AbstractDungeon.getCurrRoom().monsters = MonsterHelper.getEncounter("DownfallExtension:Crowbot");
+                            else
+                                AbstractDungeon.getCurrRoom().monsters = MonsterHelper.getEncounter("The Guardian");
+                        } else if (AbstractDungeon.player instanceof TheHexaghost) {
+
+                            AbstractDungeon.getCurrRoom().monsters = MonsterHelper.getEncounter("Hexaghost");
+                        } else if (AbstractDungeon.player instanceof ChampChar) {
+                            if (Loader.isModLoaded("DownfallExtension"))
+                                AbstractDungeon.getCurrRoom().monsters = MonsterHelper.getEncounter("DownfallExtension:Irondead");
+                            else {
+                                AbstractMonster m = new Champ();
+                                m.maxHealth = Math.round(m.maxHealth * .6F);
+                                m.currentHealth = m.maxHealth;
+                                m.powers.add(new StrengthPower(m, -3));
+                                AbstractDungeon.getCurrRoom().monsters = new MonsterGroup(m);
+                            }
+                        } else if (AbstractDungeon.player instanceof AutomatonChar) {
+                            if (Loader.isModLoaded("DownfallExtension"))
+                                AbstractDungeon.getCurrRoom().monsters = MonsterHelper.getEncounter("DownfallExtension:Crowbot");
+                            else {
+                                AbstractMonster m = new BronzeAutomaton();
+                                m.maxHealth = Math.round(m.maxHealth * .6F);
+                                m.currentHealth = m.maxHealth;
+                                ReflectionHacks.setPrivate(m, BronzeAutomaton.class, "firstTurn", false);
+                                m.powers.add(new StrengthPower(m, -3));
+                                AbstractDungeon.getCurrRoom().monsters = new MonsterGroup(m);
+                            }
+                        } else if (AbstractDungeon.player instanceof TheSnecko) {
+                            AbstractDungeon.getCurrRoom().monsters = new MonsterGroup(new SneckoMirror());
+                        } else if (AbstractDungeon.player instanceof GremlinCharacter) {
+                            lastCombatMetricKey = "Gremlin Mirror";
+                            AbstractDungeon.getCurrRoom().monsters = new MonsterGroup(new AbstractMonster[]{spawnGremlin(GremlinLeader.POSX[0], GremlinLeader.POSY[0]), spawnGremlin(GremlinLeader.POSX[1], GremlinLeader.POSY[1]), new GremlinMirror()});
+                        } else if (AbstractDungeon.player instanceof CollectorChar) {
+                            AbstractMonster m = new TheCollector();
+                            m.maxHealth = Math.round(m.maxHealth * .6F);
+                            m.currentHealth = m.maxHealth;
+                            m.powers.add(new StrengthPower(m, -3));
+                            AbstractDungeon.getCurrRoom().monsters = new MonsterGroup(m);
+                        } else {
+                            ArrayList<String> list = new ArrayList();
+                            list.add("Slime Boss");
+                            list.add("Hexaghost");
+                            list.add("The Guardian");
+
+                            Collections.shuffle(list, new Random(AbstractDungeon.miscRng.randomLong()));
+                            AbstractDungeon.getCurrRoom().monsters = MonsterHelper.getEncounter((String) list.get(0));
+                        }
+
+                        AbstractDungeon.getCurrRoom().rewards.clear();
+                        if (AbstractDungeon.ascensionLevel >= 13) {
+                            AbstractDungeon.getCurrRoom().addGoldToRewards(25);
+                        } else {
+                            AbstractDungeon.getCurrRoom().addGoldToRewards(50);
+                        }
+
+                        AbstractDungeon.getCurrRoom().addRelicToRewards(RelicTier.RARE);
+                        this.enterCombatFromImage();
+                        //                        CardCrawlGame.music.fadeOutBGM();
+                        CardCrawlGame.music.fadeOutTempBGM();
+                        CardCrawlGame.music.playTempBgmInstantly("MINDBLOOM", true);
+                        // note: the former bug that causes bgm to disappear when finishing this fight is likely due to the wrong usage of fadeOutBGM()
+                        // actually playTempBgmInstantly() is already stopping main music, calling that would turn off another bool variable controlling main music
+                        // and wouldnt recover without manually toggling back, causing the bgm to disappear, I guess
+                        logMetric(ID, "Fight Yourself");
+                        break;
+                    case 1:
+                        this.imageEventText.updateBodyText(DESCRIPTIONSALT[2]);
+                        this.screen = CurScreen.LEAVE;
+                        int effectCount = 0;
+                        List<String> upgradedCards = new ArrayList();
+
+                        for (AbstractCard c : AbstractDungeon.player.masterDeck.group) {
+                            if (c.canUpgrade()) {
+                                ++effectCount;
+                                if (effectCount <= 20) {
+                                    float x = MathUtils.random(0.1F, 0.9F) * (float) Settings.WIDTH;
+                                    float y = MathUtils.random(0.2F, 0.8F) * (float) Settings.HEIGHT;
+                                    AbstractDungeon.effectList.add(new ShowCardBrieflyEffect(c.makeStatEquivalentCopy(), x, y));
+                                    AbstractDungeon.topLevelEffects.add(new UpgradeShineEffect(x, y));
+                                }
+
+                                upgradedCards.add(c.cardID);
+                                c.upgrade();
+                                AbstractDungeon.player.bottledCardUpgradeCheck(c);
+                            }
+                        }
+
+                        AbstractDungeon.player.loseRelic(HeartBlessingRed.ID);
+                        AbstractDungeon.player.loseRelic(HeartBlessingBlue.ID);
+                        AbstractDungeon.player.loseRelic(HeartBlessingGreen.ID);
+
+                        logMetricUpgradeCards(ID, "Upgrade", upgradedCards);
+
+                        this.imageEventText.updateDialogOption(0, OPTIONS[4]);
+                        break;
+                    case 2:
+                        if (AbstractDungeon.floorNum % 50 <= 40) {
+                            this.imageEventText.updateBodyText(DESCRIPTIONSALT[1]);
+                            this.screen = CurScreen.LEAVE;
+                            List<String> cardsAdded = new ArrayList<>();
+                            cardsAdded.add("Normality");
+                            cardsAdded.add("Normality");
+                            int goldGain = 999;
+                            AbstractDungeon.effectList.add(new RainingGoldEffect(goldGain));
+                            AbstractDungeon.player.gainGold(goldGain);
+                            AbstractDungeon.effectList.add(new ShowCardAndObtainEffect(new Normality(), (float) Settings.WIDTH * 0.6F, (float) Settings.HEIGHT / 2.0F));
+                            AbstractDungeon.effectList.add(new ShowCardAndObtainEffect(new Normality(), (float) Settings.WIDTH * 0.3F, (float) Settings.HEIGHT / 2.0F));
+                            this.imageEventText.updateDialogOption(0, OPTIONS[4]);
+                            logMetric(ID, "Gold", cardsAdded, null, null, null, null, null, null, 0, 0, 0, 0, goldGain, 0);
+                        } else {
+                            this.imageEventText.updateBodyText(DESCRIPTIONSALT[1]);
+                            this.screen = CurScreen.LEAVE;
+                            AbstractCard curse = new Doubt();
+                            logMetricObtainCardAndHeal(ID, "Heal", curse, AbstractDungeon.player.maxHealth - AbstractDungeon.player.currentHealth);
+                            AbstractDungeon.player.heal(AbstractDungeon.player.maxHealth);
+                            AbstractDungeon.effectList.add(new ShowCardAndObtainEffect(curse, (float) Settings.WIDTH / 2.0F, (float) Settings.HEIGHT / 2.0F));
+                            this.imageEventText.updateDialogOption(0, OPTIONS[4]);
+                        }
                 }
-            }
-        } else {
-            super.obtain();
+
+                this.imageEventText.clearRemainingOptions();
+                break;
+            case LEAVE:
+                this.openMap();
+                break;
+            default:
+                this.openMap();
         }
+
     }
 
+    private static AbstractMonster spawnGremlin(float x, float y) {
+        ArrayList<String> gremlinPool = new ArrayList<String>();
+        gremlinPool.add("GremlinWarrior");
+        gremlinPool.add("GremlinWarrior");
+        gremlinPool.add("GremlinThief");
+        gremlinPool.add("GremlinThief");
+        gremlinPool.add("GremlinFat");
+        gremlinPool.add("GremlinFat");
+        gremlinPool.add("GremlinTsundere");
+        gremlinPool.add("GremlinWizard");
+        return getGremlin((String) gremlinPool.get(AbstractDungeon.miscRng.random(0, gremlinPool.size() - 1)), x, y);
+    }
 
-    @Override
-    public String getUpdatedDescription() {
-        // Colorize the starter relic's name
-        String name = new EmeraldTorch().name;
-        StringBuilder sb = new StringBuilder();
-        if (Settings.language == Settings.GameLanguage.ZHS || Settings.language == Settings.GameLanguage.ZHT) {
-            sb.append("[#").append(CollectorMod.characterColor.toString()).append("]").append(name).append("[]");
+    static {
+        eventStrings = CardCrawlGame.languagePack.getEventString("MindBloom");
+        NAME = eventStrings.NAME;
+        DESCRIPTIONS = eventStrings.DESCRIPTIONS;
+        OPTIONS = eventStrings.OPTIONS;
+        DESCRIPTIONSALT = CardCrawlGame.languagePack.getEventString(ID).DESCRIPTIONS;
+        OPTIONSALT = CardCrawlGame.languagePack.getEventString(ID).OPTIONS;
+    }
 
-        } else {
-            for (String word : name.split(" ")) {
-                sb.append("[#").append(CollectorMod.characterColor.toString()).append("]").append(word).append("[] ");
-            }
-            sb.setLength(sb.length() - 1);
-            sb.append("[#").append(CollectorMod.characterColor.toString()).append("]");
+    private static enum CurScreen {
+        INTRO,
+        FIGHT,
+        LEAVE;
+
+        private CurScreen() {
         }
-
-        return DESCRIPTIONS[0] + sb + DESCRIPTIONS[1];
     }
 }
