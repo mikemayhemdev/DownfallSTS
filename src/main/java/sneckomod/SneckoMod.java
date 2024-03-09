7c650cb8b0d1651eaa2ada7dc801b6b35db3ec0a
Mwallx <89969909+Mwallx@users.noreply.github.com>
2024-03-09 14:02:13 +0800

    Update SneckoMod.java

--- src/main/java/sneckomod/SneckoMod.java
+++ src/main/java/sneckomod/SneckoMod.java
@@ -562,6 +562,11 @@ public class SneckoMod implements
                     if (q != null) {
                         tv = q.type;
                     }
+                    //Help us locate which one is causing the null pointer without delving too deep
+                    System.out.println("Is strings null? [0] " + (strings[0] == null) + " [1] " + (strings[1] == null) + " [2] " + (strings[2] == null) + " [3] " + (strings[3] == null));
+                    System.out.println("Is s null? " + (s == null));
+                    System.out.println("Is r null? " + (r == null));
+                    System.out.println("Is tv null? " + (tv == null));
                     CustomCard c = new OctoChoiceCard("UNVERIFIED", strings[0] + s + strings[1], "bronzeResources/images/cards/BuggyMess.png", strings[2] + s + strings[3], r, tv);
                     if (q != null && q.portrait != null)
                         c.portrait = q.portrait;
