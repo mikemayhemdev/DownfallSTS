96c0a6a84dcbb3f870e687388e32f67102121290
Mwallx <Mwallxs@gmail.com>
2024-07-27 23:36:16 +0800

    forgot to change time of need's effect

--- src/main/java/theHexaghost/cards/TimeOfNeed.java
+++ src/main/java/theHexaghost/cards/TimeOfNeed.java
@@ -5,6 +5,7 @@ import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import slimebound.actions.MakeTempCardInHandActionReduceCost;
 import theHexaghost.HexaMod;
 
 import static automaton.AutomatonMod.makeBetaCardPath;
@@ -33,8 +34,8 @@ public class TimeOfNeed extends AbstractHexaCard {
             }
         }
 
-        q.freeToPlayOnce = true;
-        this.addToBot(new MakeTempCardInHandAction(q, true));
+//        q.freeToPlayOnce = true;
+        this.addToBot(new MakeTempCardInHandActionReduceCost(q));
     }
 
     public void upgrade() {
