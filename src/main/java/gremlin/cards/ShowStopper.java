c562cce7075ced41c2c977224e5a257ab99d6b1f
blue-userx <blue63147@gmail.com>
2025-01-18 11:06:25 -0500

    show stopper buff

--- src/main/java/gremlin/cards/ShowStopper.java
+++ src/main/java/gremlin/cards/ShowStopper.java
@@ -36,7 +36,7 @@ public class ShowStopper extends AbstractGremlinCard {
         super(ID, NAME, IMG_PATH, COST, strings.DESCRIPTION, TYPE, RARITY, TARGET);
 
         this.baseDamage = POWER;
-
+        this.baseMagicNumber = 5;
         this.isMultiDamage = true;
         this.tags.add(SneckoMod.BANNEDFORSNECKO);
         this.tags.add(WIZARD_GREMLIN);
@@ -47,7 +47,7 @@ public class ShowStopper extends AbstractGremlinCard {
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         AbstractDungeon.actionManager.addToBottom(new VFXAction(new GrandFinalEffect(), 1.0F));
-        for(int i=0;i<5;i++) {
+        for(int i=0;i<this.magicNumber;i++) {
             AbstractDungeon.actionManager.addToBottom(
                     new DamageAllEnemiesAction(p, this.multiDamage, this.damageTypeForTurn,
                             AbstractGameAction.AttackEffect.FIRE));
@@ -76,7 +76,7 @@ public class ShowStopper extends AbstractGremlinCard {
         if (!this.upgraded)
         {
             upgradeName();
-            upgradeDamage(UPGRADE_BONUS);
+upgradeMagicNumber(1);
         }
     }
 }
