74bc811bcd94239b2a418ce3df43d9e3c763e9ee
blue-userx <blue63147@gmail.com>
2025-02-05 22:28:31 -0500

    Goop now actually works with Vulnerable.

--- src/main/java/sneckomod/cards/Blunderbus.java
+++ src/main/java/sneckomod/cards/Blunderbus.java
@@ -33,8 +33,8 @@ public class Blunderbus extends AbstractSneckoCard {
 
     public final static String ID = SneckoMod.makeID("Blunderbus");
 
-    private static final int DAMAGE = 9;
-    private static final int UPGRADE_DAMAGE = 2;
+    private static final int DAMAGE = 8;
+    private static final int UPGRADE_DAMAGE = 3;
     private static final int BASE_HITS = 1;
 
     public Blunderbus() {
