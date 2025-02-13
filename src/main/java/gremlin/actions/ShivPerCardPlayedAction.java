414082f86d4c83969c78a3274d82fffdd0664635
blue-userx <blue63147@gmail.com>
2025-02-13 00:25:08 -0500

    gremlins

--- src/main/java/gremlin/actions/ShivPerCardPlayedAction.java
+++ src/main/java/gremlin/actions/ShivPerCardPlayedAction.java
@@ -22,8 +22,8 @@ public class ShivPerCardPlayedAction extends AbstractGameAction
         int count = AbstractDungeon.actionManager.cardsPlayedThisTurn.size();
         --count;
         int availableSpace = BaseMod.MAX_HAND_SIZE - AbstractDungeon.player.hand.size();
-        if (count > availableSpace-1) {
-            count = availableSpace-1;
+        if (count > availableSpace) {
+            count = availableSpace;
         }
         AbstractCard c = new Shiv();
         if (upgraded) {
