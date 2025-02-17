fc95deb7d20b555e611a253388b69f808e2f5322
blue-userx <blue63147@gmail.com>
2025-02-17 18:41:46 -0500

    blank card and exotic form

--- src/main/java/sneckomod/relics/BlankCard.java
+++ src/main/java/sneckomod/relics/BlankCard.java
@@ -18,6 +18,9 @@ import sneckomod.cards.unknowns.AbstractUnknownCard;
 
 import java.util.ArrayList;
 
+import static com.megacrit.cardcrawl.cards.AbstractCard.CardType.CURSE;
+import static com.megacrit.cardcrawl.cards.AbstractCard.CardType.STATUS;
+
 public class BlankCard extends CustomRelic {
 
     public static final String ID = SneckoMod.makeID("BlankCard");
@@ -29,41 +32,43 @@ public class BlankCard extends CustomRelic {
         super(ID, IMG, OUTLINE, RelicTier.UNCOMMON, LandingSound.MAGICAL);
     }
 
-//    @Override
-//    public void atBattleStart() {
-//        ArrayList<AbstractCard> possCardsList = new ArrayList<>(AbstractDungeon.player.drawPile.group);
-//        AbstractCard card2 = possCardsList.get(AbstractDungeon.cardRandomRng.random(possCardsList.size() - 1)).makeStatEquivalentCopy();
-//        if (card2 instanceof AbstractUnknownCard) {
-//            card2 = ((AbstractUnknownCard) card2).generateFromPoolButNotIntoHand();
-//        } // Get one of the cards in the unknown pool instead of the Unknown card which is already free to play.
-//        flash();
-//        addToBot(new RelicAboveCreatureAction(AbstractDungeon.player, this));
-//        card2.freeToPlayOnce = true;
-//        addToBot(new EchoACardAction(card2));
-//    }
 
     @Override
     public void atBattleStart() {
-        activated = false;
-        beginLongPulse();
+        ArrayList<AbstractCard> possCardsList = new ArrayList<>(AbstractDungeon.player.drawPile.group);
+        possCardsList.removeIf(c -> (c.color == AbstractCard.CardColor.CURSE || c.type == CURSE || c.type == STATUS));
+        AbstractCard card2 = possCardsList.get(AbstractDungeon.cardRandomRng.random(possCardsList.size() - 1)).makeStatEquivalentCopy();
+        if (card2 instanceof AbstractUnknownCard) {
+            card2 = ((AbstractUnknownCard) card2).generateFromPoolButNotIntoHand();
+        } // Get one of the cards in the unknown pool instead of the Unknown card which is already free to play.
+        flash();
+        addToBot(new RelicAboveCreatureAction(AbstractDungeon.player, this));
+        card2.freeToPlayOnce = true;
+        addToBot(new EchoACardAction(card2));
     }
 
-    @Override
-    public void onCardDraw(AbstractCard card) {
-        if (!activated)
-            addToBot(new AbstractGameAction() {
-                @Override
-                public void update() {
-                    isDone = true;
-                    if (card.cost == 3 && !activated) {
-                        stopPulse();
-                        flash();
-                        activated = true;
-                        addToBot(new MuddleAction(card));
-                    }
-                }
-            });
-    }
+//    @Override
+//    public void atBattleStart() {
+//        activated = false;
+//        beginLongPulse();
+//    }
+
+//    @Override
+//    public void onCardDraw(AbstractCard card) {
+//        if (!activated)
+//            addToBot(new AbstractGameAction() {
+//                @Override
+//                public void update() {
+//                    isDone = true;
+//                    if (card.cost == 3 && !activated) {
+//                        stopPulse();
+//                        flash();
+//                        activated = true;
+//                        addToBot(new MuddleAction(card));
+//                    }
+//                }
+//            });
+//    }
 
     public String getUpdatedDescription() {
         return DESCRIPTIONS[0];
