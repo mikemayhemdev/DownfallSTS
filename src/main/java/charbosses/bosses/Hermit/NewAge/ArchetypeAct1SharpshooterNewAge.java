b7c81a00e16c573d2abace65274a8046afea7602
blue-userx <blue63147@gmail.com>
2025-04-03 00:00:09 -0400

    abacus works now

--- src/main/java/charbosses/bosses/Hermit/NewAge/ArchetypeAct1SharpshooterNewAge.java
+++ src/main/java/charbosses/bosses/Hermit/NewAge/ArchetypeAct1SharpshooterNewAge.java
@@ -115,7 +115,7 @@ public class ArchetypeAct1SharpshooterNewAge extends ArchetypeBaseIronclad {
         } else {
             switch (turn) {
                 case 0:
-                    AbstractCharBoss.boss.getRelic(CBR_Abacus.ID).onTrigger();
+                    AbstractCharBoss.boss.getRelic(CBR_Abacus.ID).onShuffle();
                     addToList(cardsList, new EnHeadshot());
                     addToList(cardsList, new EnDefendHermit());
                     addToList(cardsList, new EnInjury());
