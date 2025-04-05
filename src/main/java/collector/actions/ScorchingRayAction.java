e4a0a3ebd062d869459d405f6da8f98104dfd523
blue-userx <blue63147@gmail.com>
2025-04-05 10:35:08 -0400

    scorching ray

--- src/main/java/collector/actions/ScorchingRayAction.java
+++ src/main/java/collector/actions/ScorchingRayAction.java
@@ -1,62 +1,65 @@
-package collector.actions;
-
-import champ.stances.BerserkerStance;
-import collector.effects.ColoredVerticalAttackEffect;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.math.MathUtils;
-import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.actions.animations.VFXAction;
-import com.megacrit.cardcrawl.actions.common.DamageAction;
-import com.megacrit.cardcrawl.cards.AbstractCard;
-import com.megacrit.cardcrawl.cards.DamageInfo;
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
-import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.PenNibPower;
-import com.megacrit.cardcrawl.powers.VulnerablePower;
-import com.megacrit.cardcrawl.powers.watcher.VigorPower;
-import com.megacrit.cardcrawl.stances.DivinityStance;
-import com.megacrit.cardcrawl.stances.WrathStance;
-import gremlin.actions.PseudoDamageRandomEnemyAction;
-import gremlin.relics.FragmentationGrenade;
-
-import static collector.util.Wiz.att;
-
-public class ScorchingRayAction extends AbstractGameAction {
-    private final AbstractCard card;
-    private static int stuff;
-
-    public ScorchingRayAction(AbstractCard card, int cool) {
-        this.card = card;
-        this.actionType = ActionType.DAMAGE;
-        this.startDuration = 0.1F;
-        this.duration = this.startDuration;
-        stuff = cool;
-    }
-
-    public void update() {
-        isDone = true;
-        AbstractMonster q = AbstractDungeon.getMonsters().getRandomMonster(null, true, AbstractDungeon.cardRandomRng);
-        if (q != null) {
-            card.applyPowers();
-            card.calculateCardDamage(q);
-            int storage = stuff;
-
-            if (q.hasPower(VulnerablePower.POWER_ID)) {
-                stuff = (stuff + (stuff/2));
-            }
-            if (AbstractDungeon.player.hasPower(PenNibPower.POWER_ID)) {
-                stuff = stuff * 2;
-            }
-            if (AbstractDungeon.player.stance.ID.equals(WrathStance.STANCE_ID)) {
-                stuff = stuff * 2;
-            }
-            if (AbstractDungeon.player.stance.ID.equals(DivinityStance.STANCE_ID)) {
-                stuff = stuff * 3;
-            }
-
-            att(new PseudoDamageRandomEnemyAction(q, new DamageInfo(AbstractDungeon.player, card.damage + stuff, DamageInfo.DamageType.NORMAL), AttackEffect.FIRE));
-            att(new VFXAction(new ColoredVerticalAttackEffect(q.hb.x + MathUtils.random(q.hb.width / 3, ((q.hb.width / 3) * 2)), q.hb.cY, true, new Color(MathUtils.random(), MathUtils.random(), MathUtils.random(), 1))));
-            stuff = storage;
-        }
-    }
-}
+//package collector.actions;
+//
+//import champ.stances.BerserkerStance;
+//import collector.effects.ColoredVerticalAttackEffect;
+//import com.badlogic.gdx.graphics.Color;
+//import com.badlogic.gdx.math.MathUtils;
+//import com.megacrit.cardcrawl.actions.AbstractGameAction;
+//import com.megacrit.cardcrawl.actions.animations.VFXAction;
+//import com.megacrit.cardcrawl.actions.common.DamageAction;
+//import com.megacrit.cardcrawl.cards.AbstractCard;
+//import com.megacrit.cardcrawl.cards.DamageInfo;
+//import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+//import com.megacrit.cardcrawl.monsters.AbstractMonster;
+//import com.megacrit.cardcrawl.powers.PenNibPower;
+//import com.megacrit.cardcrawl.powers.VulnerablePower;
+//import com.megacrit.cardcrawl.powers.watcher.VigorPower;
+//import com.megacrit.cardcrawl.stances.DivinityStance;
+//import com.megacrit.cardcrawl.stances.WrathStance;
+//import gremlin.actions.PseudoDamageRandomEnemyAction;
+//import gremlin.relics.FragmentationGrenade;
+//
+//import static collector.util.Wiz.att;
+//
+//public class ScorchingRayAction extends AbstractGameAction {
+//    private final AbstractCard card;
+//    private static int stuff;
+//
+//    public ScorchingRayAction(AbstractCard card, int cool) {
+//        this.card = card;
+//        this.actionType = ActionType.DAMAGE;
+//        this.startDuration = 0.1F;
+//        this.duration = this.startDuration;
+//        stuff = cool;
+//    }
+//
+//    public void update() {
+//        isDone = true;
+//        AbstractMonster q = AbstractDungeon.getMonsters().getRandomMonster(null, true, AbstractDungeon.cardRandomRng);
+//        if (q != null) {
+//            card.applyPowers();
+//            card.calculateCardDamage(q);
+//            int storage = stuff;
+//
+//            if (q.hasPower(VulnerablePower.POWER_ID)) {
+//                stuff = (stuff + (stuff/2));
+//            }
+//
+//            if (AbstractDungeon.player.hasPower(PenNibPower.POWER_ID)) {
+//                stuff = stuff * 2;
+//            }
+//
+//            if (AbstractDungeon.player.stance.ID.equals(WrathStance.STANCE_ID)) {
+//                stuff = stuff * 2;
+//            }
+//
+//            if (AbstractDungeon.player.stance.ID.equals(DivinityStance.STANCE_ID)) {
+//                stuff = stuff * 3;
+//            }
+//
+//            att(new PseudoDamageRandomEnemyAction(q, new DamageInfo(AbstractDungeon.player, card.damage + stuff, DamageInfo.DamageType.NORMAL), AttackEffect.FIRE));
+//            att(new VFXAction(new ColoredVerticalAttackEffect(q.hb.x + MathUtils.random(q.hb.width / 3, ((q.hb.width / 3) * 2)), q.hb.cY, true, new Color(MathUtils.random(), MathUtils.random(), MathUtils.random(), 1))));
+//            stuff = storage;
+//        }
+//    }
+//}
--- src/main/java/collector/cards/ScorchingRay.java
+++ src/main/java/collector/cards/ScorchingRay.java
@@ -1,22 +1,27 @@
 package collector.cards;
 
 import automaton.actions.EasyXCostAction;
-import collector.actions.ScorchingRayAction;
+import collector.effects.ColoredVerticalAttackEffect;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.math.MathUtils;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.animations.VFXAction;
 import com.megacrit.cardcrawl.actions.common.AttackDamageRandomEnemyAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.VulnerablePower;
 import com.megacrit.cardcrawl.powers.watcher.VigorPower;
+import gremlin.actions.PseudoDamageRandomEnemyAction;
 
 import static collector.CollectorMod.makeID;
 import static collector.util.Wiz.atb;
+import static collector.util.Wiz.att;
 
 public class ScorchingRay extends AbstractCollectorCard {
     public final static String ID = makeID(ScorchingRay.class.getSimpleName());
-    // intellij stuff attack, enemy, common, 4, 1, , , 4, 
-    private int cool = 0;
+    // intellij stuff attack, enemy, common, 4, 1, , , 4,
 
     public ScorchingRay() {
         super(ID, -1, CardType.ATTACK, CardRarity.COMMON, CardTarget.ALL_ENEMY);
@@ -25,13 +30,12 @@ public class ScorchingRay extends AbstractCollectorCard {
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
-        cool = 0;
-        if (AbstractDungeon.player.hasPower(VigorPower.POWER_ID)) {
-            cool = AbstractDungeon.player.getPower(VigorPower.POWER_ID).amount;
-        }
-        atb(new EasyXCostAction(this, (effect, params) -> {
+        att(new EasyXCostAction(this, (effect, params) -> {
             for (int i = 0; i < effect; i++) {
-                atb(new ScorchingRayAction(this, cool));
+                //ScorchingRayAction couldn't cut it.
+                AbstractMonster q = AbstractDungeon.getMonsters().getRandomMonster(null, true, AbstractDungeon.cardRandomRng);
+                atb(new VFXAction(new ColoredVerticalAttackEffect(q.hb.x + MathUtils.random(q.hb.width / 3, ((q.hb.width / 3) * 2)), q.hb.cY, true, new Color(MathUtils.random(), MathUtils.random(), MathUtils.random(), 1))));
+                atb(new PseudoDamageRandomEnemyAction(q, new DamageInfo(AbstractDungeon.player, this.damage, DamageInfo.DamageType.NORMAL), AbstractGameAction.AttackEffect.FIRE));
             }
             return true;
         }));
