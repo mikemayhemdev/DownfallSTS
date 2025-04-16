0bec9f72fcb2866953cd7daf7810f523e68524ce
blue-userx <blue63147@gmail.com>
2025-04-16 15:22:13 -0400

    black candle

--- src/main/java/downfall/relics/BlackCandle.java
+++ src/main/java/downfall/relics/BlackCandle.java
@@ -1,27 +1,15 @@
 package downfall.relics;
-
 import basemod.abstracts.CustomRelic;
 import com.badlogic.gdx.graphics.Texture;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.actions.common.EscapeAction;
 import com.megacrit.cardcrawl.actions.common.LoseHPAction;
-import com.megacrit.cardcrawl.actions.common.RelicAboveCreatureAction;
 import com.megacrit.cardcrawl.actions.utility.UseCardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
-import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
-import com.megacrit.cardcrawl.helpers.PowerTip;
-import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.relics.AbstractRelic;
-import com.megacrit.cardcrawl.relics.BlueCandle;
-import downfall.actions.WaitForEscapeAction;
 import downfall.downfallMod;
-import downfall.patches.EvilModeCharacterSelect;
 import hermit.characters.hermit;
-import slimebound.characters.SlimeboundCharacter;
-
 import static downfall.patches.EvilModeCharacterSelect.evilMode;
-import static hermit.characters.hermit.Enums.HERMIT;
 
 
 /// This relic exists to quickgrab its description, images, and texts to replace blue candle while playing evil mode, like hecktoplasm.
@@ -43,13 +31,20 @@ public class BlackCandle extends CustomRelic {
 
     public void onUseCard(AbstractCard card, UseCardAction action) {
         if (card.type == AbstractCard.CardType.CURSE) {
-            this.flash();
             if (card.cost == -2) {
                 this.flash();
                 this.addToBot(new LoseHPAction(AbstractDungeon.player, AbstractDungeon.player, 1, AbstractGameAction.AttackEffect.FIRE));
                 card.exhaust = true;
                 action.exhaustCard = true;
             }
+        }
+    }
+
+    public boolean canPlay(AbstractCard card) {
+        if (card.type == AbstractCard.CardType.CURSE) {
+            return true;
+        }else{
+            return card.canPlay(card);
         }
     }
 
