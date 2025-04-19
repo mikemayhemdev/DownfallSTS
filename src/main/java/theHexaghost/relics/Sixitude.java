9acefd244c23e03af1da99af4afb86175c3af731
blue-userx <blue63147@gmail.com>
2025-04-19 01:42:21 -0400

    quiet six point brand nerf

--- src/main/java/theHexaghost/relics/Sixitude.java
+++ src/main/java/theHexaghost/relics/Sixitude.java
@@ -1,6 +1,7 @@
 package theHexaghost.relics;
 
 import basemod.abstracts.CustomRelic;
+import champ.ChampMod;
 import champ.cards.Strike;
 import com.badlogic.gdx.graphics.Texture;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
@@ -11,6 +12,7 @@ import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import expansioncontent.actions.EchoACardAction;
+import expansioncontent.expansionContentMod;
 import hermit.cards.Strike_Hermit;
 import theHexaghost.HexaMod;
 import downfall.util.TextureLoader;
@@ -31,14 +33,16 @@ public class Sixitude extends CustomRelic {
 
     @Override
     public void onUseCard(AbstractCard targetCard, UseCardAction useCardAction) {
-        ++this.counter;
-        if (this.counter % 6 == 0) {
-            this.flash();
-            this.counter = 0;
-            this.addToBot(new RelicAboveCreatureAction(AbstractDungeon.player, this));
-           // this.addToBot(new DamageRandomEnemyAction(new DamageInfo(null, 6, DamageInfo.DamageType.THORNS), AbstractGameAction.AttackEffect.FIRE));
-            AbstractCard c2 = new Strike_Hermit();
-            addToBot(new EchoACardAction(c2, true));
+        if (targetCard.hasTag(expansionContentMod.ECHO)) {
+            ++this.counter;
+            if (this.counter % 6 == 0) {
+                this.flash();
+                this.counter = 0;
+                this.addToBot(new RelicAboveCreatureAction(AbstractDungeon.player, this));
+                // this.addToBot(new DamageRandomEnemyAction(new DamageInfo(null, 6, DamageInfo.DamageType.THORNS), AbstractGameAction.AttackEffect.FIRE));
+                AbstractCard c2 = new Strike_Hermit();
+                addToBot(new EchoACardAction(c2, true));
+            }
         }
     }
 
