538f605497a1744832ba681fd812e9c5db830abe
blue-userx <blue63147@gmail.com>
2025-04-25 21:36:46 -0400

    Snake Rake rework

--- src/main/java/sneckomod/cards/QuickBite.java
+++ src/main/java/sneckomod/cards/QuickBite.java
@@ -2,11 +2,16 @@ package sneckomod.cards;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.animations.VFXAction;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.vfx.combat.BiteEffect;
 import sneckomod.SneckoMod;
+import sneckomod.actions.MuddleAction;
 import sneckomod.actions.MuddleRandomCardAction;
+
+import java.util.ArrayList;
 
 public class QuickBite extends AbstractSneckoCard {
 
@@ -15,18 +20,42 @@ public class QuickBite extends AbstractSneckoCard {
     //stupid intellij stuff ATTACK, ENEMY, COMMON
 
     private static final int DAMAGE = 9;
-    private static final int UPG_DAMAGE = 3;
+    private static final int UPG_DAMAGE = 1;
 
     public QuickBite() {
         super(ID, 1, CardType.ATTACK, CardRarity.COMMON, CardTarget.ENEMY);
         baseDamage = DAMAGE;
+        baseMagicNumber = magicNumber = 1;
         SneckoMod.loadJokeCardImage(this, "QuickBite.png");
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
         atb(new VFXAction(new BiteEffect(m.hb.cX, m.hb.cY), 0.3F));// 117
         dmg(m, makeInfo(), AbstractGameAction.AttackEffect.NONE);
-        atb(new MuddleRandomCardAction(1, true));
+        //atb(new MuddleRandomCardAction(1, true));
+        // Create a list to track the cards that are drawn
+        ArrayList<AbstractCard> preHand = new ArrayList<>(p.hand.group); // Store current cards in hand before drawing
+
+        // Draw cards based on magicNumber
+        addToBot(new DrawCardAction(magicNumber, new AbstractGameAction() {
+            @Override
+            public void update() {
+                ArrayList<AbstractCard> drawnCards = new ArrayList<>();
+
+                // Identify which cards were added to the hand
+                for (AbstractCard card : p.hand.group) {
+                    if (!preHand.contains(card)) {
+                        drawnCards.add(card); // These are the newly drawn cards
+                    }
+                }
+
+                // Muddle the newly drawn cards
+                for (AbstractCard card : drawnCards) {
+                    addToBot(new MuddleAction(card)); // Automatically Muddle the drawn card
+                }
+                isDone = true;
+            }
+        }));
 
     }
 
@@ -34,6 +63,9 @@ public class QuickBite extends AbstractSneckoCard {
         if (!upgraded) {
             upgradeName();
             upgradeDamage(UPG_DAMAGE);
+            upgradeMagicNumber(1);
+            rawDescription = UPGRADE_DESCRIPTION;
+            initializeDescription();
         }
     }
 }
