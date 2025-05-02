5affcbbf9f830e5d2e1144cab7447c7b1670c438
blue-userx <blue63147@gmail.com>
2025-05-02 09:48:43 -0400

    awakened one prototype

--- src/main/java/awakenedOne/AwakenedOneMod.java
+++ src/main/java/awakenedOne/AwakenedOneMod.java
@@ -10,6 +10,7 @@ import awakenedOne.powers.UltimateHexDebuff;
 import awakenedOne.relics.KTRibbon;
 import awakenedOne.relics.RippedDoll;
 import awakenedOne.relics.ShreddedDoll;
+import awakenedOne.relics.TomeOfPortalmancy;
 import awakenedOne.ui.AwakenedIcon;
 import awakenedOne.ui.OrbitingSpells;
 import awakenedOne.util.CardFilter;
@@ -191,6 +192,7 @@ public class AwakenedOneMod implements
         BaseMod.addRelicToCustomPool(new RippedDoll(), AwakenedOneChar.Enums.AWAKENED_BLUE);
         BaseMod.addRelicToCustomPool(new ShreddedDoll(), AwakenedOneChar.Enums.AWAKENED_BLUE);
         BaseMod.addRelicToCustomPool(new KTRibbon(), AwakenedOneChar.Enums.AWAKENED_BLUE);
+        BaseMod.addRelicToCustomPool(new TomeOfPortalmancy(), AwakenedOneChar.Enums.AWAKENED_BLUE);
 
     }
 
