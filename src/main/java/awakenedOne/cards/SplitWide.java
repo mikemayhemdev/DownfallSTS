07b652835de792a1705ee2b04e9d276eae5d713d
blue-userx <blue63147@gmail.com>
2025-05-08 14:54:31 -0400

    art

--- src/main/java/awakenedOne/cards/SplitWide.java
+++ src/main/java/awakenedOne/cards/SplitWide.java
@@ -1,23 +1,17 @@
 package awakenedOne.cards;
 
 import awakenedOne.powers.StrengthReturnPower;
-import awakenedOne.relics.KTRibbon;
 import com.badlogic.gdx.graphics.Color;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.animations.VFXAction;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
-import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.Settings;
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.AbstractPower;
-import com.megacrit.cardcrawl.powers.watcher.BlockReturnPower;
 import com.megacrit.cardcrawl.vfx.StarBounceEffect;
 import com.megacrit.cardcrawl.vfx.combat.ViolentAttackEffect;
 
 import static awakenedOne.AwakenedOneMod.makeID;
-import static awakenedOne.ui.AwakenButton.awaken;
 
 public class SplitWide extends AbstractAwakenedCard {
     public final static String ID = makeID(SplitWide.class.getSimpleName());
