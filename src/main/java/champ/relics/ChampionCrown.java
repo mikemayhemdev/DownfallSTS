8e964c89de4eab12b20708b1e96c867f7217ac4e
blue-userx <blue63147@gmail.com>
2025-05-08 22:34:05 -0400

    champion crown reversion and ultimate crown change

--- src/main/java/champ/relics/ChampionCrown.java
+++ src/main/java/champ/relics/ChampionCrown.java
@@ -60,7 +60,7 @@ public class ChampionCrown extends CustomRelic  {
     public void atBattleStart() {
         super.atBattleStart();
         if (AbstractDungeon.player.stance.ID.equals(NeutralStance.STANCE_ID)) {
-            int x = AbstractDungeon.cardRandomRng.random(1);
+            int x = AbstractDungeon.relicRng.random(1);
             switch (x) {
                 case 0:
                     //SlimeboundMod.logger.info("Switching to Berserker (Mod Relic)");
