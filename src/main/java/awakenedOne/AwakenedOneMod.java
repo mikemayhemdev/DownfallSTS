b6c898b3b92cdef25d4740932784d6c32dcc3247
blue-userx <blue63147@gmail.com>
2025-05-15 16:42:14 -0400

    potions

--- src/main/java/awakenedOne/AwakenedOneMod.java
+++ src/main/java/awakenedOne/AwakenedOneMod.java
@@ -238,7 +238,7 @@ public class AwakenedOneMod implements
     public void addPotions() {
         BaseMod.addPotion(CultistsDelight.class, Color.BLUE, Color.NAVY, Color.YELLOW, CultistsDelight.POTION_ID, AwakenedOneChar.Enums.AWAKENED_ONE);
         BaseMod.addPotion(SacramentalWine.class, Color.PURPLE, Color.VIOLET, Color.MAROON, SacramentalWine.POTION_ID, AwakenedOneChar.Enums.AWAKENED_ONE);
-        BaseMod.addPotion(SneckoPowersPotion.class, Color.TAN, Color.CYAN, Color.TAN, SneckoPowersPotion.POTION_ID, AwakenedOneChar.Enums.AWAKENED_ONE);
+        BaseMod.addPotion(SneckoPowersPotion.class, Color.CYAN, Color.TAN, Color.BLUE, SneckoPowersPotion.POTION_ID, AwakenedOneChar.Enums.AWAKENED_ONE);
 
 
 
