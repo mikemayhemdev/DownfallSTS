a3501161c13b35699e24de8f60ee114dca820713
blue-userx <blue63147@gmail.com>
2025-05-16 13:31:36 -0400

    crash fix

--- src/main/java/expansioncontent/powers/DarknessFallsPower2.java
+++ src/main/java/expansioncontent/powers/DarknessFallsPower2.java
@@ -1,6 +1,7 @@
-package awakenedOne.powers;
+package expansioncontent.powers;
 
 
+import awakenedOne.powers.OnLoseEnergyPower;
 import com.evacipated.cardcrawl.mod.stslib.powers.abstracts.TwoAmountPower;
 import com.megacrit.cardcrawl.actions.common.GainBlockAction;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
@@ -10,27 +11,26 @@ import com.megacrit.cardcrawl.powers.StrengthPower;
 
 import static awakenedOne.util.Wiz.applyToSelf;
 
-public class DarknessFallsPower extends TwoAmountPower implements OnLoseEnergyPower {
+public class DarknessFallsPower2 extends TwoAmountPower implements OnLoseEnergyPower {
     // intellij stuff buff
-    public static final String POWER_ID = "awakened:DarknessFallsPower";
+    public static final String POWER_ID = "expansioncontent:DarknessFallsPower";
     private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
     public static final String NAME = powerStrings.NAME;
     public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
 
-    public DarknessFallsPower(int amount, int amount2) {
+    public DarknessFallsPower2(int amount, int amount2) {
         this.name = NAME;
         this.ID = POWER_ID;
-        this.owner = AbstractDungeon.player;
         this.amount = amount;
         this.amount2 = amount2;
         this.type = PowerType.BUFF;
-        this.isTurnBased = true;
+        this.loadRegion("hex");
         updateDescription();
     }
 
     @Override
     public void LoseEnergyAction(int gained) {
-        flash();
+        this.flash();
         addToBot(new GainBlockAction(owner, gained*amount));
         applyToSelf(new StrengthPower(AbstractDungeon.player, gained*amount2));
     }
@@ -40,7 +40,3 @@ public class DarknessFallsPower extends TwoAmountPower implements OnLoseEnergyPo
         description = DESCRIPTIONS[0] + amount + DESCRIPTIONS[1] + amount2 + DESCRIPTIONS[2];
     }
 }
-
-
-
-
