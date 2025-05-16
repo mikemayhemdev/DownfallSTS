5e4a4e317939321681a45eacfd9041c7f7b1b184
blue-userx <blue63147@gmail.com>
2025-05-16 12:44:58 -0400

    crash fix

--- src/main/java/awakenedOne/powers/DarknessFallsPower.java
+++ src/main/java/awakenedOne/powers/DarknessFallsPower.java
@@ -3,15 +3,19 @@ package awakenedOne.powers;
 
 import com.evacipated.cardcrawl.mod.stslib.powers.abstracts.TwoAmountPower;
 import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.PowerStrings;
 import com.megacrit.cardcrawl.powers.StrengthPower;
 
 import static awakenedOne.util.Wiz.applyToSelf;
 
 public class DarknessFallsPower extends TwoAmountPower implements OnLoseEnergyPower {
     // intellij stuff buff
-    public static final String NAME = DarknessFallsPower.class.getSimpleName();
     public static final String POWER_ID = "awakened:DarknessFallsPower";
+    private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    public static final String NAME = powerStrings.NAME;
+    public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
 
     public DarknessFallsPower(int amount, int amount2) {
         this.name = NAME;
@@ -33,7 +37,7 @@ public class DarknessFallsPower extends TwoAmountPower implements OnLoseEnergyPo
 
     @Override
     public void updateDescription() {
-        description = DESCRIPTIONS[0] + amount + DESCRIPTIONS[1] + amount2 + "test";
+        description = DESCRIPTIONS[0] + amount + DESCRIPTIONS[1] + amount2 + DESCRIPTIONS[2];
     }
 }
 
