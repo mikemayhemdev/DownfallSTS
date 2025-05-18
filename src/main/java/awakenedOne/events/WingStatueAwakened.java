835e3cee856f76fb4b1c124660379849d31f9ba9
blue-userx <blue63147@gmail.com>
2025-05-18 14:35:27 -0400

    spell upgrade rework

--- src/main/java/awakenedOne/events/WingStatueAwakened.java
+++ src/main/java/awakenedOne/events/WingStatueAwakened.java
@@ -1,25 +1,17 @@
 package awakenedOne.events;
 
 
-import com.badlogic.gdx.math.MathUtils;
-import com.megacrit.cardcrawl.actions.AbstractGameAction.AttackEffect;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.cards.curses.Injury;
-import com.megacrit.cardcrawl.cards.curses.Pain;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.events.AbstractImageEvent;
 import com.megacrit.cardcrawl.localization.EventStrings;
-import com.megacrit.cardcrawl.relics.WarpedTongs;
 import com.megacrit.cardcrawl.vfx.cardManip.ShowCardAndObtainEffect;
-import com.megacrit.cardcrawl.vfx.combat.FlashAtkImgEffect;
-import downfall.cards.curses.Aged;
-import downfall.cards.curses.Haunted;
 import downfall.relics.BrokenWingStatue;
 import downfall.relics.ShatteredFragment;
-import gremlin.patches.GremlinEnum;
 
 public class WingStatueAwakened extends AbstractImageEvent {
     public static final String ID = "awakened:WingStatue";
@@ -37,16 +29,11 @@ public class WingStatueAwakened extends AbstractImageEvent {
     }
 
     private CurScreen screen;
-    private int goldLoss;
 
     public WingStatueAwakened() {
         super(NAME, DESCRIPTIONS[0], "images/events/goldenWing.jpg");
         this.screen = CurScreen.INTRO;
 
-        this.goldLoss = MathUtils.ceil((float) AbstractDungeon.player.maxHealth * 0.25F);
-        if (AbstractDungeon.ascensionLevel >= 15){
-            this.goldLoss = MathUtils.ceil((float) AbstractDungeon.player.maxHealth * 0.35F);
-        }
         if (AbstractDungeon.ascensionLevel >= 15) {
             this.damage = (int)((float)AbstractDungeon.player.maxHealth * 0.35F);
         } else {
@@ -71,10 +58,6 @@ public class WingStatueAwakened extends AbstractImageEvent {
                         this.imageEventText.clearAllDialogs();
                         this.imageEventText.setDialogOption(OPTIONS[3]);
                         AbstractDungeon.getCurrRoom().spawnRelicAndObtain((float) Settings.WIDTH / 2.0F, (float) Settings.HEIGHT / 2.0F, new ShatteredFragment());
-//                        AbstractDungeon.player.decreaseMaxHealth(this.damage);
-                        //  AbstractDungeon.player.damage(new DamageInfo(null, this.damage));
-                        // AbstractDungeon.effectList.add(new FlashAtkImgEffect(AbstractDungeon.player.hb.cX, AbstractDungeon.player.hb.cY, AttackEffect.FIRE));
-                        AbstractDungeon.player.damage(new DamageInfo(null, this.goldLoss));
                         this.screen = CurScreen.RESULT;
                         logMetricObtainCardAndRelic(ID, "Destroyed Statue", curse, new ShatteredFragment());
                         return;
@@ -82,7 +65,7 @@ public class WingStatueAwakened extends AbstractImageEvent {
                         this.imageEventText.updateBodyText(DESCRIPTIONS[1]);
                         this.imageEventText.clearAllDialogs();
                         this.imageEventText.setDialogOption(OPTIONS[3]);
-                        AbstractDungeon.player.damage(new DamageInfo(null, ((AbstractDungeon.ascensionLevel >= 15)?7:5)));
+                        AbstractDungeon.player.damage(new DamageInfo(null, (this.damage)));
                         this.screen = CurScreen.RESULT;
                         AbstractDungeon.getCurrRoom().spawnRelicAndObtain((float) Settings.WIDTH / 2.0F, (float) Settings.HEIGHT / 2.0F, new BrokenWingStatue());
                         logMetricObtainRelicAndDamage(ID, "Collected Statue", new BrokenWingStatue(), ((AbstractDungeon.ascensionLevel >= 15)?7:5));
