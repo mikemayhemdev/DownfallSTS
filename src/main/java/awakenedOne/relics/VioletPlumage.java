b0d64ef35593018d17ea63d73217d3bae64b3e99
blue-userx <blue63147@gmail.com>
2025-06-28 17:17:16 -0400

    rework violet plumage

--- src/main/java/awakenedOne/relics/VioletPlumage.java
+++ src/main/java/awakenedOne/relics/VioletPlumage.java
@@ -9,6 +9,8 @@ import com.megacrit.cardcrawl.actions.utility.UseCardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.status.VoidCard;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.powers.DrawCardNextTurnPower;
+import com.megacrit.cardcrawl.ui.panels.EnergyPanel;
 
 import static awakenedOne.AwakenedOneMod.*;
 
@@ -22,20 +24,33 @@ public class VioletPlumage extends CustomRelic {
 
     //the relic was reworked because mini black hole had to go to shop because its weird rip old effect
 
+    //the relic was reworked again
+
     public VioletPlumage() {
         super(ID, IMG, OUTLINE, RelicTier.COMMON, LandingSound.MAGICAL);
     }
 
     //violet plumage
 
-    @Override
-    public void onUseCard(AbstractCard c, UseCardAction action) {
-        if ((c.type == AbstractCard.CardType.POWER) && !this.grayscale){
-            flash();
-            this.addToBot(new RelicAboveCreatureAction(AbstractDungeon.player, this));
-            this.addToBot(new GainEnergyAction(1));
-            //addToBot(new MakeTempCardInDrawPileAction(new VoidCard(), 1, false, true));
-            this.grayscale = true;
+//    @Override
+//    public void onUseCard(AbstractCard c, UseCardAction action) {
+//        if ((c.type == AbstractCard.CardType.POWER) && !this.grayscale){
+//            flash();
+//            this.addToBot(new RelicAboveCreatureAction(AbstractDungeon.player, this));
+//            this.addToBot(new GainEnergyAction(1));
+//            //addToBot(new MakeTempCardInDrawPileAction(new VoidCard(), 1, false, true));
+//            this.grayscale = true;
+//        }
+//    }
+
+    public void atEndOfTurn(boolean isPlayer) {
+        if (this.grayscale = false) {
+            if (EnergyPanel.totalCount > 0) {
+                this.flash();
+                this.addToBot(new RelicAboveCreatureAction(AbstractDungeon.player, this));
+                this.addToBot(new ApplyPowerAction(AbstractDungeon.player, AbstractDungeon.player, new DrawCardNextTurnPower(AbstractDungeon.player, 3), 3));
+                this.grayscale = true;
+            }
         }
     }
 
@@ -50,7 +65,7 @@ public class VioletPlumage extends CustomRelic {
 
     @Override
     public String getUpdatedDescription() {
-        return DESCRIPTIONS[0];
+        return DESCRIPTIONS[0] + 3 + DESCRIPTIONS[1];
     }
 
 }
