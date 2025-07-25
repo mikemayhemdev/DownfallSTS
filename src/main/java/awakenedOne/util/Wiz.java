b87d1f7064067b85bfdb90b4baddacabc28b0612
blue-userx <blue63147@gmail.com>
2025-07-25 11:58:45 -0400

    awakened 8 power requirement

--- src/main/java/awakenedOne/util/Wiz.java
+++ src/main/java/awakenedOne/util/Wiz.java
@@ -142,7 +142,7 @@ public class Wiz {
             return true;
         }
 
-        if (cool>6) {
+        if (cool>7) {
             OrbitingSpells.upgradeall();
             return true;
         }
