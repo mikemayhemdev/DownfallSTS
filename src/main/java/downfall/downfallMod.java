a732a9734ac33f10b887f7afca2556a86feb51c1
Geronimo <hensongeronimo@gmail.com>
2025-07-26 21:26:51 -0300

    add missing file on Spanish Translation, enable it by default

--- src/main/java/downfall/downfallMod.java
+++ src/main/java/downfall/downfallMod.java
@@ -246,13 +246,15 @@ public class downfallMod implements OnPlayerDamagedSubscriber, OnStartBattleSubs
     public static Settings.GameLanguage[] SupportedLanguages = {
             // Insert other languages here
             // DONT FORGET TO TOGGLE AT reskinContent.getLanguageString() TOO
-            Settings.GameLanguage.ENG, Settings.GameLanguage.ZHS,
-             Settings.GameLanguage.JPN,
+            Settings.GameLanguage.ENG,
+            Settings.GameLanguage.ZHS,
+            Settings.GameLanguage.JPN,
             Settings.GameLanguage.KOR,
-       //     Settings.GameLanguage.FRA,
-//            Settings.GameLanguage.ZHT,
-      //      Settings.GameLanguage.RUS,
-        //    Settings.GameLanguage.PTB
+            Settings.GameLanguage.SPA,
+            // Settings.GameLanguage.FRA,
+            // Settings.GameLanguage.ZHT,
+            // Settings.GameLanguage.RUS,
+            // Settings.GameLanguage.PTB
     };
 
     public static ReplaceData[] wordReplacements;
