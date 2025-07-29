29ded73ba62bb461a068ee1b4792b086c255c624
blue-userx <blue63147@gmail.com>
2025-07-29 19:43:33 -0400

    stuff

--- src/main/java/awakenedOne/cards/tokens/spells/Cryostasis.java
+++ src/main/java/awakenedOne/cards/tokens/spells/Cryostasis.java
@@ -19,7 +19,7 @@ public class Cryostasis extends AbstractSpellCard {
 
     public Cryostasis() {
         super(ID, 1, CardType.SKILL, CardTarget.ENEMY);
-        baseBlock = 8;
+        baseBlock = 9;
         baseMagicNumber = magicNumber = 1;
         loadJokeCardImage(this, makeBetaCardPath(Cryostasis.class.getSimpleName() + ".png"));
     }
@@ -55,7 +55,7 @@ public class Cryostasis extends AbstractSpellCard {
 
 
     public void upp() {
-        upgradeBlock(4);
+        upgradeBlock(3);
         upgradeMagicNumber(1);
     }
 }
