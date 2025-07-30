fd9170711d5909705a0089e45f043f63882e0095
blue-userx <blue63147@gmail.com>
2025-07-30 10:50:30 -0400

    stuff

--- src/main/java/awakenedOne/ui/OrbitingSpells.java
+++ src/main/java/awakenedOne/ui/OrbitingSpells.java
@@ -211,7 +211,7 @@ public class OrbitingSpells {
             float dist = FontHelper.getWidth(FontHelper.tipHeaderFont, s.name, 1.0F);
 
             Color textColor = Color.WHITE.cpy();
-            if (s.upgraded || Wiz.isAwakened()) {
+            if (s.upgraded) {
                 textColor = Color.GREEN.cpy();
             }
             if (s.hasTag(UP_NEXT) && DeterministicConjure) {
