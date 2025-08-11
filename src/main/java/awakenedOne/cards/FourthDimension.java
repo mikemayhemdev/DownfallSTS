dd2e2e1b6234753278203ef3e339c57d382e8afd
blue-userx <blue63147@gmail.com>
2025-08-10 23:37:04 -0400

    4th dimension can't duplicate itself

--- src/main/java/awakenedOne/cards/FourthDimension.java
+++ src/main/java/awakenedOne/cards/FourthDimension.java
@@ -4,6 +4,7 @@ import awakenedOne.powers.FourthDimensionPower;
 import com.evacipated.cardcrawl.mod.stslib.actions.common.SelectCardsInHandAction;
 import com.megacrit.cardcrawl.actions.common.ExhaustSpecificCardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.green.Nightmare;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
@@ -29,7 +30,7 @@ public class FourthDimension extends AbstractAwakenedCard {
     public void use(AbstractPlayer p, AbstractMonster m) {
         atb(new SelectCardsInHandAction(cardStrings.EXTENDED_DESCRIPTION[0], (cards) -> {
             for (AbstractCard c : cards) {
-                if (!(c instanceof FourthDimension)) {
+                if (!(c instanceof FourthDimension) && !(c instanceof Nightmare)) {
                     applyToSelfTop(new FourthDimensionPower(magicNumber, c.makeStatEquivalentCopy()));
                     att(new ExhaustSpecificCardAction(c, AbstractDungeon.player.hand));
                 } else {
