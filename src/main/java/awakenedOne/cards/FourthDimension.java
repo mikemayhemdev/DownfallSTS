6618f14a116376183bfdb8d56db7a9b9166ab594
mikemayhemdev <45570650+mikemayhemdev@users.noreply.github.com>
2025-08-11 02:59:47 -0400

    Pregaming Blue's latest feedback round

--- src/main/java/awakenedOne/cards/FourthDimension.java
+++ src/main/java/awakenedOne/cards/FourthDimension.java
@@ -19,7 +19,7 @@ public class FourthDimension extends AbstractAwakenedCard {
     public FourthDimension() {
         super(ID, 3, CardType.SKILL, CardRarity.RARE, CardTarget.SELF);
         this.exhaust = true;
-        baseMagicNumber = magicNumber = 3;
+        baseMagicNumber = magicNumber = 2;
         //this.tags.add(CardTags.HEALING);
         loadJokeCardImage(this, makeBetaCardPath(FourthDimension.class.getSimpleName() + ".png"));
     }
