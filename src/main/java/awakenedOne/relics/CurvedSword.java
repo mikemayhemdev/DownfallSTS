a25adef8c992f00bb1bbe82fd9050b241cbd4c9c
blue-userx <blue63147@gmail.com>
2025-08-16 10:44:38 -0400

    un-nerf because curiosity nerf

--- src/main/java/awakenedOne/relics/CurvedSword.java
+++ src/main/java/awakenedOne/relics/CurvedSword.java
@@ -38,7 +38,7 @@ public class CurvedSword extends CustomRelic {
     public void onUseCard(AbstractCard card, UseCardAction action) {
         if (card instanceof AbstractSpellCard) {
             ++this.counter;
-            if (this.counter % 5 == 0) {
+            if (this.counter % 4 == 0) {
                 this.flash();
                 this.counter = 0;
                 this.addToBot(new RelicAboveCreatureAction(AbstractDungeon.player, this));
@@ -54,7 +54,7 @@ public class CurvedSword extends CustomRelic {
 
     @Override
     public String getUpdatedDescription() {
-        return DESCRIPTIONS[0] + 5 + DESCRIPTIONS[1];
+        return DESCRIPTIONS[0] + 4 + DESCRIPTIONS[1];
     }
 
 }
