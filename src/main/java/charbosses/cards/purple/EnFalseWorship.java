a5f38f1bac481a061c383e9f82662c9aaac7c1e0
mikemayhemdev <45570650+mikemayhemdev@users.noreply.github.com>
2025-08-17 23:03:33 -0400

    Watcher flips, tweaks to afk watcher

--- src/main/java/charbosses/cards/purple/EnFalseWorship.java
+++ src/main/java/charbosses/cards/purple/EnFalseWorship.java
@@ -2,69 +2,53 @@ package charbosses.cards.purple;
 
 import basemod.ReflectionHacks;
 import charbosses.cards.AbstractBossCard;
-import charbosses.powers.bossmechanicpowers.IroncladStatusPower;
+import charbosses.powers.bossmechanicpowers.CultistRevivePower;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
-import com.megacrit.cardcrawl.actions.unique.IncreaseMaxHpAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.monsters.exordium.Cultist;
 import com.megacrit.cardcrawl.powers.RitualPower;
-import com.megacrit.cardcrawl.powers.VulnerablePower;
-import com.megacrit.cardcrawl.powers.WeakPower;
 import downfall.util.TextureLoader;
 import expansioncontent.expansionContentMod;
 
 import java.util.Iterator;
 
-import static collector.util.Wiz.atb;
 import static collector.util.Wiz.att;
 
-public class EnEndIsNigh extends AbstractBossCard {
-    public static final String ID = "downfall:EndIsNigh";
+public class EnFalseWorship extends AbstractBossCard {
+    public static final String ID = "downfall:FalseWorship";
     private static final CardStrings cardStrings;
 
     static {
         cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
     }
 
-    public EnEndIsNigh() {
-        super(ID, cardStrings.NAME, expansionContentMod.makeCardPath("SummonMushrooms.png"), 1, cardStrings.DESCRIPTION, CardType.POWER, CardColor.PURPLE, CardRarity.RARE, CardTarget.SELF, AbstractMonster.Intent.BUFF);
+    public EnFalseWorship() {
+        super(ID, cardStrings.NAME, expansionContentMod.makeCardPath("SummonMushrooms.png"), 1, cardStrings.DESCRIPTION, CardType.SKILL, CardColor.PURPLE, CardRarity.RARE, CardTarget.SELF, AbstractMonster.Intent.BUFF);
         portrait = TextureLoader.getTextureAsAtlasRegion(expansionContentMod.makeCardPath("SummonMushrooms.png"));
         portraitImg = TextureLoader.getTexture(expansionContentMod.makeCardPath("SummonMushrooms.png"));
         this.loadJokeCardImage();
-        baseMagicNumber = magicNumber = 100;
     }
 
     @Override
     public void use(final AbstractPlayer p, final AbstractMonster m) {
-        atb(new AbstractGameAction() {
-            @Override
-            public void update() {
-                isDone = true;
-                    att(new AbstractGameAction() {
-                        @Override
-                        public void update() {
-                            isDone = true;
-                            m.increaseMaxHp(magicNumber, true);
-                        }
-                    });
-
-            }
-        });
 
         Iterator var4 = AbstractDungeon.getCurrRoom().monsters.monsters.iterator();
 
         while(var4.hasNext()) {
             AbstractMonster mo = (AbstractMonster)var4.next();
-            this.addToBot(new ApplyPowerAction(mo, m, new RitualPower(mo, 1, false), 1, true, AbstractGameAction.AttackEffect.NONE));
+            if (mo instanceof Cultist) {
+                this.addToBot(new ApplyPowerAction(mo, m, new CultistRevivePower(mo, 1), 1, true, AbstractGameAction.AttackEffect.NONE));
+                break;
+            }
         }
-        this.addToBot(new ApplyPowerAction(p, m, new RitualPower(p, 1, false), 1, true, AbstractGameAction.AttackEffect.NONE));
 
     }
 
@@ -77,7 +61,7 @@ public class EnEndIsNigh extends AbstractBossCard {
 
     @Override
     public AbstractCard makeCopy() {
-        return new EnEndIsNigh();
+        return new EnFalseWorship();
     }
 
     public void loadJokeCardImage() {
---
+++ src/main/java/charbosses/powers/bossmechanicpowers/CultistRevivePower.java
@@ -0,0 +1,85 @@
+package charbosses.powers.bossmechanicpowers;
+
+import basemod.interfaces.CloneablePowerInterface;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.megacrit.cardcrawl.actions.animations.TalkAction;
+import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.vfx.combat.HealEffect;
+import downfall.downfallMod;
+import downfall.util.TextureLoader;
+
+public class CultistRevivePower extends AbstractPower implements CloneablePowerInterface {
+
+    public static final String POWER_ID = downfallMod.makeID("CultistRevivePower");
+    public static final String NAME = CardCrawlGame.languagePack.getPowerStrings(POWER_ID).NAME;
+    public static final String DESCRIPTIONS[] = CardCrawlGame.languagePack.getPowerStrings(POWER_ID).DESCRIPTIONS;
+
+    private static final Texture tex84 = TextureLoader.getTexture(downfallMod.assetPath("images/powers/FairyPotion84.png"));
+    private static final Texture tex32 = TextureLoader.getTexture(downfallMod.assetPath("images/powers/FairyPotion32.png"));
+
+    public static boolean CANNOT_END = false;
+
+    public CultistRevivePower(final AbstractCreature owner, final int amount) {
+        this.ID = POWER_ID;
+        this.owner = owner;
+        this.amount = amount;
+        this.type = PowerType.BUFF;
+        this.isTurnBased = true;
+
+        this.region128 = new TextureAtlas.AtlasRegion(tex84, 0, 0, 84, 84);
+        this.region48 = new TextureAtlas.AtlasRegion(tex32, 0, 0, 32, 32);
+
+        this.name = NAME;
+
+        this.updateDescription();
+    }
+
+    private void healUndead(AbstractCreature m, int healAmount) {
+        if (m.isDying) m.isDying = false;
+        for (AbstractPower p : m.powers) {
+            p.onHeal(healAmount);
+        }
+
+        m.currentHealth += healAmount;// 451
+        if (m.currentHealth > m.maxHealth) {// 452
+            m.currentHealth = m.maxHealth;// 453
+        }
+
+
+        if (healAmount > 0) {// 465
+            m.healthBarUpdatedEvent();// 470
+        }
+
+    }
+
+    @Override
+    public void onDeath() {
+        CANNOT_END = true;
+        super.onDeath();
+        healUndead(owner, owner.maxHealth);
+        AbstractDungeon.effectsQueue.add(new HealEffect(owner.hb.cX, owner.hb.cY, owner.maxHealth));
+        addToTop(new RemoveSpecificPowerAction(owner, owner, CultistRevivePower.POWER_ID));
+        addToTop(new TalkAction(owner, DESCRIPTIONS[1]));
+        CANNOT_END = false;
+    }
+
+    /*
+
+
+     */
+
+    @Override
+    public void updateDescription() {
+        this.description = DESCRIPTIONS[0];
+    }
+
+    @Override
+    public AbstractPower makeCopy() {
+        return new CultistRevivePower(owner, amount);
+    }
+}
