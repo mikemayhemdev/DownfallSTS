1b52a83ada8deb47f545f443215c04572dd5ab53
mikemayhemdev <45570650+mikemayhemdev@users.noreply.github.com>
2025-08-26 16:33:27 -0400

    Spells & dejection change

--- src/main/java/awakenedOne/cards/tokens/spells/Darkleech.java
+++ src/main/java/awakenedOne/cards/tokens/spells/Darkleech.java
@@ -11,6 +11,7 @@ import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.VulnerablePower;
+import com.megacrit.cardcrawl.powers.WeakPower;
 import com.megacrit.cardcrawl.vfx.combat.GiantEyeEffect;
 import hermit.actions.SoundAction;
 
@@ -42,14 +43,14 @@ public class Darkleech extends AbstractSpellCard {
         this.addToBot(new SoundAction(makeID("DARKSPELL")));
         if (!AbstractDungeon.player.hasRelic(EyeOfTheOccult.ID)) {
             this.addToBot(new VFXAction(new GiantEyeEffect(m.hb.cX, m.hb.cY + 300.0F * Settings.scale, new Color(1.0F, 0.3F, 1.0F, 0.0F))));
-            this.addToBot(new ApplyPowerAction(m, p, new VulnerablePower(m, this.magicNumber, false), this.magicNumber));
+            this.addToBot(new ApplyPowerAction(m, p, new WeakPower(m, this.magicNumber, false), this.magicNumber));
             this.addToBot(new ApplyPowerAction(m, p, new ManaburnPower(m, this.secondMagic), this.secondMagic));
         } else {
             Iterator var3 = AbstractDungeon.getCurrRoom().monsters.monsters.iterator();
             while (var3.hasNext()) {
                 AbstractMonster mo = (AbstractMonster) var3.next();
                 this.addToBot(new VFXAction(new GiantEyeEffect(mo.hb.cX, mo.hb.cY + 300.0F * Settings.scale, new Color(1.0F, 0.3F, 1.0F, 0.0F))));
-                this.addToBot(new ApplyPowerAction(mo, p, new VulnerablePower(mo, this.magicNumber, false), this.magicNumber, true, AbstractGameAction.AttackEffect.NONE));
+                this.addToBot(new ApplyPowerAction(mo, p, new WeakPower(mo, this.magicNumber, false), this.magicNumber, true, AbstractGameAction.AttackEffect.NONE));
                 this.addToBot(new ApplyPowerAction(mo, p, new ManaburnPower(mo, this.secondMagic), this.secondMagic));
             }
         }
