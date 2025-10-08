4a0dacf3924ae3c2793e02f1736a56bd6e7e9b43
blue-userx <blue63147@gmail.com>
2025-10-07 20:57:10 -0400

    bugfixes

--- src/main/java/champ/powers/IronFortressPower.java
+++ src/main/java/champ/powers/IronFortressPower.java
@@ -28,7 +28,7 @@ public class IronFortressPower extends AbstractPower implements CloneablePowerIn
         this.owner = AbstractDungeon.player;
         this.amount = amount;
         this.type = PowerType.BUFF;
-        this.isTurnBased = true;
+        this.isTurnBased = false;
 
         this.region128 = new TextureAtlas.AtlasRegion(tex84, 0, 0, 84, 84);
         this.region48 = new TextureAtlas.AtlasRegion(tex32, 0, 0, 32, 32);
