918e9d6ce560fb9d09e16d6b81b4e55291c2a1c3
blue-userx <blue63147@gmail.com>
2025-11-21 10:53:02 -0500

    watcher 3 move devotion location

--- src/main/java/theHexaghost/relics/Libra.java
+++ src/main/java/theHexaghost/relics/Libra.java
@@ -5,11 +5,14 @@ import com.badlogic.gdx.graphics.Texture;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.CardGroup;
 import com.megacrit.cardcrawl.cards.curses.AscendersBane;
+import com.megacrit.cardcrawl.cards.curses.Parasite;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.helpers.input.InputHelper;
 import com.megacrit.cardcrawl.relics.AbstractRelic;
 import com.megacrit.cardcrawl.unlock.UnlockTracker;
+import com.megacrit.cardcrawl.vfx.cardManip.ShowCardAndObtainEffect;
 import downfall.util.TextureLoader;
 import guardian.cards.ExploitGems;
 import hermit.relics.Memento;
@@ -66,7 +69,8 @@ public class Libra extends CustomRelic {
                     UnlockTracker.markCardAsSeen(c.cardID);
                     c.isSeen = true;
                 }
-                AbstractDungeon.player.masterDeck.addToTop(c);}
+                AbstractDungeon.effectList.add(new ShowCardAndObtainEffect(c, Settings.WIDTH / 2.0F, Settings.HEIGHT / 2.0F));
+            }
         }
     }
 
