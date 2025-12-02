49e45a76b3b81d960e680ed36c75a7b361976d2e
blue-userx <blue63147@gmail.com>
2025-12-02 09:35:31 -0500

    big text mode fixes

--- src/main/java/theHexaghost/cards/ThermalTransfer.java
+++ src/main/java/theHexaghost/cards/ThermalTransfer.java
@@ -4,6 +4,7 @@ import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.animations.VFXAction;
 import com.megacrit.cardcrawl.actions.common.GainBlockAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import theHexaghost.HexaMod;
 import com.megacrit.cardcrawl.vfx.combat.FireballEffect;
@@ -45,6 +46,15 @@ public class ThermalTransfer extends AbstractHexaCard {
             upgradeName();
             upgradeDamage(UPG_DAMAGE);
             upgradeBlock(UPG_BLOCK);
+        }
+    }
+
+    @Override
+    public float getTitleFontSize() {
+        if(Settings.language != Settings.GameLanguage.ENG) {
+            return 19.0F;
+        }else {
+            return 20.0F;
         }
     }
 }
