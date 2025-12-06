66b9423c6a1cd73df68864cf1d6d27293e5a0720
blue-userx <blue63147@gmail.com>
2025-12-06 11:24:22 -0500

    numbers

--- src/main/java/sneckomod/cards/Medusa.java
+++ src/main/java/sneckomod/cards/Medusa.java
@@ -20,7 +20,7 @@ public class Medusa extends AbstractSneckoCard {
 
     private static final int DAMAGE = 6;
     private static final int UPG_DAMAGE = 2;
-    private static final int MAGIC = 3;
+    private static final int MAGIC = 2;
     private static final int UPG_MAGIC = 1;
     private static int SOFTLOCK = 0;
 
