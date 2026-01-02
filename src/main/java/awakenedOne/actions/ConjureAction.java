366a568378ed0a1429757416e280c4b8ce6063b6
blue-userx <blue63147@gmail.com>
2026-01-02 17:43:03 -0500

    fix no draw bug

--- src/main/java/awakenedOne/actions/ConjureAction.java
+++ src/main/java/awakenedOne/actions/ConjureAction.java
@@ -75,11 +75,11 @@ public class ConjureAction extends AbstractGameAction {
             this.isDone = true;
             return;
         }
-        if (AbstractDungeon.player.hasPower("No Draw")) {
-            AbstractDungeon.player.getPower("No Draw").flash();
-            this.isDone = true;
-            return;
-        }
+//        if (AbstractDungeon.player.hasPower("No Draw")) {
+//            AbstractDungeon.player.getPower("No Draw").flash();
+//            this.isDone = true;
+//            return;
+//        }
         conjuresThisCombat += 1;
         isDone = true;
         conjuredCards.clear();
