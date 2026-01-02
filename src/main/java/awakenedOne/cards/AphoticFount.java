cbafef4ef0c780650e81226cd72da2237e2bc0cd
blue-userx <blue63147@gmail.com>
2026-01-01 23:25:48 -0500

    revert

--- src/main/java/awakenedOne/cards/AphoticFount.java
+++ src/main/java/awakenedOne/cards/AphoticFount.java
@@ -17,8 +17,8 @@ public class AphoticFount extends AbstractAwakenedCard {
     // intellij stuff skill, self, basic, , ,  5, 3, ,
 
     public AphoticFount() {
-        super(ID, 1, CardType.POWER, CardRarity.RARE, CardTarget.SELF);
-        baseMagicNumber = magicNumber = 1;
+        super(ID, 2, CardType.POWER, CardRarity.RARE, CardTarget.SELF);
+        baseMagicNumber = magicNumber = 2;
         this.tags.add(AwakenedOneMod.DELVE);
         loadJokeCardImage(this, makeBetaCardPath(AphoticFount.class.getSimpleName() + ".png"));
     }
@@ -31,7 +31,7 @@ public class AphoticFount extends AbstractAwakenedCard {
 
     @Override
     public void upp() {
-        upgradeMagicNumber(1);
-        //upgradeBaseCost(1);
+        //upgradeMagicNumber(1);
+        upgradeBaseCost(1);
     }
 }
