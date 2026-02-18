cae1f9a2485fabe4f44ebedb7074866b4e00703c
blue-userx <blue63147@gmail.com>
2026-02-18 12:14:16 -0500

    fix icy charm bug

--- src/main/java/theHexaghost/relics/IceCube.java
+++ src/main/java/theHexaghost/relics/IceCube.java
@@ -34,7 +34,7 @@ public class IceCube extends CustomRelic {
     }// 43
 
     @Override
-    public void atBattleStart() {
+    public void atPreBattle() {
             this.addToBot(new ApplyPowerAction(AbstractDungeon.player, AbstractDungeon.player, new EnhancePower(-INTENSITY_LOSS), -INTENSITY_LOSS));
     }
 
